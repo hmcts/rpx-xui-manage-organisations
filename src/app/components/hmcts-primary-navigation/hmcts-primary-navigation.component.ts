8ab67905f97d948f1941abe06c67c59a42180ccb
mas <masahmad7@gmail.com>
2019-01-24 10:45:16 +0000

    minor updates remove debug info

--- src/app/components/hmcts-primary-navigation/hmcts-primary-navigation.component.ts
+++ src/app/components/hmcts-primary-navigation/hmcts-primary-navigation.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-hmcts-primary-navigation',
     templateUrl: './hmcts-primary-navigation.component.html',
     styleUrls: ['./hmcts-primary-navigation.component.scss']
 })
-export class HmctsPrimaryNavigationComponent {
+export class HmctsPrimaryNavigationComponent implements OnInit {
     @Input()
     label = 'Primary navigation'
     @Input()
@@ -25,5 +26,18 @@ export class HmctsPrimaryNavigationComponent {
         }
     ]
 
-    constructor() {}
+
+    constructor(private route: ActivatedRoute) {
+
+        console.log('1', this.route.url);
+    }
+
+
+    ngOnInit() {
+
+        console.log(this.items[1].active = true)
+        console.log(this.items[0].active = false)
+        this.route.url.subscribe(url => console.log('@@@', url));
+        console.log('2', this.route.url)
+    }
 }

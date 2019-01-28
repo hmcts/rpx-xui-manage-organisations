4e5e932a8fda37be1af2d6c2a79c64ff62bf1e8a
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-01-28 11:59:42 +0000

    A more logical identity bar

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import * as fromFeeAccountSummaryStore from '../../../fee-accounts/store';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  identityBar: Observable<any>;
+
+  constructor(
+    private store: Store<fromFeeAccountSummaryStore.FeeAccountsState>
+  ) { }
+
+  ngOnInit() {
+
+    this.identityBar = of(this.store.pipe(select(fromFeeAccountSummaryStore.getFeeAccountSummaryArray)));
+  }
 }

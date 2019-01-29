37e86fc275264ed23e9961c3f46b86178ccbddc3
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-01-29 16:17:24 +0000

    PR requested changes

--- src/app/components/hmcts-identity-bar/hmcts-identity-bar.component.ts
+++ src/app/components/hmcts-identity-bar/hmcts-identity-bar.component.ts
@@ -1,33 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-hmcts-identity-bar',
     templateUrl: './hmcts-identity-bar.component.html',
     styleUrls: ['./hmcts-identity-bar.component.scss']
 })
-export class HmctsIdentityBarComponent implements OnInit {
+export class HmctsIdentityBarComponent {
 
-    @Input() content: Observable<any>;
+    @Input() set content(value) {
+        this.value = value.name;
+    }
 
-    // @Input() set isLoading(value: boolean) {
-    //   this.loading = value;
-    // }
-
-
-  value: string;
+    value: string;
 
     constructor() { }
-    // todo to fix
-    // ngOnChanges(changes: SimpleChanges): void {
-    //   if(changes['routes'] && changes['routes']['currentValue']) {
-    //     this.url = changes.routes.currentValue.state.url;
-    //   }
-    // }
-
-  ngOnInit() {
-        this.content.subscribe(data => {
-            this.value = data.name;
-        });
-    }
 }

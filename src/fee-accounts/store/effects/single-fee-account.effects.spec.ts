76d5052c49798fd4ce3d8a867c313a1732f8d782
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-01-30 16:23:48 +0000

    Added testing for effects

---
+++ src/fee-accounts/store/effects/single-fee-account.effects.spec.ts
@@ -0,0 +1,47 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { hot, cold } from 'jasmine-marbles';
+import { of } from 'rxjs';
+import { provideMockActions } from '@ngrx/effects/testing';
+import * as fromSingleFeeAccountEffects from './single-fee-account.effects';
+import { SingleFeeAccountEffects } from './single-fee-account.effects';
+import { LoadSingleFeeAccount } from '../actions/single-fee-account.actions';
+import { LoadSingleFeeAccountSuccess } from '../actions';
+import { FeeAccountsService } from 'src/fee-accounts/services';
+
+describe('Single fee account Effects', () => {
+  let actions$;
+  let effects: SingleFeeAccountEffects;
+  const SingleFeeAccountServiceMock = jasmine.createSpyObj('SingleFeeAccountService', [
+    'fetchSingleFeeAccount',
+  ]);
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+          {
+            provide: FeeAccountsService,
+            useValue: SingleFeeAccountServiceMock,
+          },
+          fromSingleFeeAccountEffects.SingleFeeAccountEffects,
+          provideMockActions(() => actions$)
+      ]
+    });
+
+    effects = TestBed.get(SingleFeeAccountEffects);
+
+  });
+  describe('loadSingleFeeAccount$', () => {
+    it('should return a collection from loadSingleFeeAccount$ - LoadSingleFeeAccountSuccess', () => {
+      const payload = [{payload: 'something'}];
+      SingleFeeAccountServiceMock.fetchSingleFeeAccount.and.returnValue(of(payload));
+      const action = new LoadSingleFeeAccount({});
+      const completion = new LoadSingleFeeAccountSuccess([{payload: 'something'}]);
+      actions$ = hot('-a', { a: action });
+      const expected = cold('-b', { b: completion });
+      expect(effects.loadSingleFeeAccount$).toBeObservable(expected);
+    });
+  });
+
+
+});

b21f2d53ca31d3fb7ca5d8a4c9e1d46baa519478
Chris Tarasovs <chris@frontr.co.uk>
2019-02-22 09:45:09 +0000

    feat: Profile: Map fields data to state

--- src/users/containers/profile/profile.component.ts
+++ src/users/containers/profile/profile.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { debug } from 'util';
-
+import * as fromAuth from '../../../auth/store';
+import * as fromRoot from '../../../app/store';
 
 
 
@@ -14,11 +15,12 @@ export class ProfileComponent implements OnInit, OnDestroy {
 
   loginSubscription: Subscription;
 
+  user$;
 
-
+  constructor(private store: Store<fromAuth.AuthState>) {}
 
   ngOnInit(): void {
-
+   this.user$ = this.store.pipe(select(fromAuth.getAuthState))
   }
 
   ngOnDestroy() {

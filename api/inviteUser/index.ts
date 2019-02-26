93242645f3a6bf79d11397d77a26e95f9f71787e
fazanki <fazanki@gmail.com>
2019-02-26 16:08:46 +0000

    adding the app-gov-uk-main-warpper component

--- api/inviteUser/index.ts
+++ api/inviteUser/index.ts
@@ -1,14 +1,26 @@
-
+import * as express from 'express'
 import { http } from '../lib/http'
+import {logger} from 'codelyzer/util/logger';
+import config from '../lib/config';
 
-export default async function inviteUser(req, res) {
-  console.log('xxxxxxxxxxxxxxxxxxxxxxxxxx=x=x=x=x=x=x')
+export const router = express.Router({mergeParams: true})
+router.post('/', inviteUserRoute)
+
+async function inviteUserRoute(req, res) {
+
+  const orgId = req.session.auth.orgId
+  const payload = req.body
   try {
-    const response = await http.get('/some/request/')
+    const response = await http.post(`${config.services.rdProfessionalApi}/organisations/${orgId}/users`, payload)
+    logger.info('response::', response.data);
     res.send(response.data)
+
   } catch (error) {
-    const errReport = {  }
-    res.status(500).send(errReport)
+    logger.info('error', error)
+    const errReport = JSON.stringify({ apiError: error, apiStatusCode: error.statusCode, message: ''})
+    res.send(errReport).status(500)
   }
 
 }
+export default router
+
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -5,9 +5,10 @@ import { HmctsPrimaryNavigationComponent } from '../../../projects/gov-ui/src/li
 export const components: any[] = [
   HmctsGlobalHeaderComponent,
   HmctsGlobalFooterComponent,
-  HmctsPrimaryNavigationComponent
+  HmctsPrimaryNavigationComponent,
 ];
 
 export * from './hmcts-global-header/hmcts-global-header.component';
 export * from './hmcts-global-footer/hmcts-global-footer.component';
 export * from '../../../projects/gov-ui/src/lib/components/hmcts-primary-navigation/hmcts-primary-navigation.component';
+export * from '../../../projects/gov-ui/src/lib/components/gov-uk-mian-wrapper/gov-uk-main-wrapper.component';

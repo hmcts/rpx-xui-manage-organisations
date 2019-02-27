0ada23f5e5020a487845ef0581cefe34992cabe2
Chris Tarasovs <chris@frontr.co.uk>
2019-02-27 13:19:57 +0000

    added router

--- api/routes.ts
+++ api/routes.ts
@@ -2,6 +2,7 @@
 import * as express from 'express'
 import accountsRouter from './accounts'
 import * as auth from './auth'
+import userRouter from './user'
 import inviteUser from './inviteUser'
 import organisationRouter from './organisation'
 
@@ -10,6 +11,7 @@ const router = express.Router({ mergeParams: true })
 router.use('/logout', auth.logout)
 router.use('/organisation', organisationRouter)
 router.use('/accounts', accountsRouter)
-router.use('/inviteUser', inviteUser)
+router.use('/user', inviteUser)
+router.use('/inviteUser', userRouter)
 
 export default router

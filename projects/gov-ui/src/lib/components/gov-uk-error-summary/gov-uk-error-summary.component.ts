7d528f2840e4cb6e72f0567159d960713f5d4e1f
fazanki <fazanki@gmail.com>
2019-03-09 08:04:20 +0000

    adding error summary component

---
+++ projects/gov-ui/src/lib/components/gov-uk-error-summary/gov-uk-error-summary.component.ts
@@ -0,0 +1,30 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'lib-gov-uk-error-summary',
+  template: `<div *ngIf="messages" class="govuk-error-summary" aria-labelledby="error-summary-title" role="alert" tabindex="-1" data-module="error-summary">
+              <h2 class="govuk-error-summary__title" id="error-summary-title">
+                There is a problem
+              </h2>
+              <div class="govuk-error-summary__body">
+                <ul class="govuk-list govuk-error-summary__list">
+                  <li *ngFor="let message of messages">
+                    <a href="#{{message}}}">{{message}}}</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+  `
+})
+export class GovUkErrorSummaryComponent  {
+
+  @Input() set errorMessages(value) {
+    this.messages = value;
+  };
+
+  messages: string[];
+
+  constructor() { }
+
+
+}
--- projects/gov-ui/src/lib/gov-ui.module.ts
+++ projects/gov-ui/src/lib/gov-ui.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import {HmctsSubNavigationComponent} from './components/hmcts-sub-navigation/hmcts-sub-navigation.component';
 import {HmctsIdentityBarComponent} from './components/hmcts-identity-bar/hmcts-identity-bar.component';
 import {GovUkMainWrapperComponent} from './components/gov-uk-mian-wrapper/gov-uk-main-wrapper.component';
+import {GovUkErrorSummaryComponent} from './components/gov-uk-error-summary/gov-uk-error-summary.component';
 import {GovukTableComponent} from './components/govuk-table/govuk-table.component';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
@@ -10,7 +11,8 @@ const COMPONENTS = [
   GovukTableComponent,
   HmctsIdentityBarComponent,
   HmctsSubNavigationComponent,
-  GovUkMainWrapperComponent
+  GovUkMainWrapperComponent,
+  GovUkErrorSummaryComponent
 ];
 @NgModule({
   imports: [

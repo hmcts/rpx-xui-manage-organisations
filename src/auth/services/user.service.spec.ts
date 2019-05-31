cf581e7fb532d7b91e984f30a1357716d3d37538
adnan <adnan.akgun@netclipper.co.uk>
2019-05-31 14:08:08 +0100

    EUI-132 some more tests

--- src/auth/services/user.service.spec.ts
+++ src/auth/services/user.service.spec.ts
@@ -1,11 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 import { UserService } from './user.service';
+import { HttpClient } from '@angular/common/http';
+import createSpyObj = jasmine.createSpyObj;
 
-xdescribe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe('UserService', () => {
+  let httpClient: HttpClient;
+  let userService: UserService;
+
+  beforeEach(() => {
+
+    httpClient = createSpyObj<HttpClient>('httpClient', ['get']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        UserService,
+        { provide: HttpClient, useValue: httpClient },
+      ]
+    });
+
+    userService = TestBed.get(UserService);
+  });
 
   it('should be created', () => {
-    const service: UserService = TestBed.get(UserService);
-    expect(service).toBeTruthy();
+    expect(userService).toBeTruthy();
   });
+
 });

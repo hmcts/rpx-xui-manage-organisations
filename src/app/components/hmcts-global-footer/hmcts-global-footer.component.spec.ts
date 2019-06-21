b44cec80984bf863a1adaa6b206e207934a2eaed
Boris <36511748+borislavpetrovhmcts@users.noreply.github.com>
2019-06-21 13:20:32 +0100

    Update footer (#57)
    * Test branch
    
    * Change to chart name
    
    * Update README.md
    
    * Update README.md
    
    * Local test
    
    * Changing local back
    
    * see if slack notifications are breaking build
    
    * update footer
    
    * fixed lint errors, removed unused variables from tests
    
    * export footer content to file
    
    * update footer
    
    * fixed lint errors, removed unused variables from tests
    
    * sonar exlusion, export data from unit test
    
    * EUI-69 Test added
    
    * git ignore
    
    * change auth attachment to api route
    
    * add smoke
    
    * smoke cmds
    
    * tests
    
    * echo
    
    * deleted reports folder added in gitignore
    
    * chore/add pr white list fix
    
    * EUI-157 - DON'T MERGE (#59)
    
    * Test branch
    
    * Change to chart name
    
    * Update README.md
    
    * Update README.md
    
    * Local test
    
    * Changing local back
    
    * Fixes for pipeline
    
    * Updating package json
    
    * Fixing config files
    
    * Updating index file
    
    * Updating preview config
    
    * Updating idam client
    
    * Preview config changes
    
    * see if slack notifications are breaking build
    
    * change auth attachment to api route
    
    * add smoke
    
    * smoke cmds
    
    * tests
    
    * echo
    
    * Updating config for preview
    
    * Feature/fz header eui 49 (#62)
    
    * removing fee accounts
    
    * adding header items nav title and user nav to state
    
    * title name changes
    
    * revming fee accounts and cleaning up
    
    * heaer working version
    
    * adding working version of routing
    
    * fixing some typings
    
    * adding test for selectors
    
    * Updating URL name
    
    * updating url
    
    * bug/correct logger
    
    * added headless on jenkins
    
    * Updating microservice
    
    * update footer
    
    * fixed lint errors, removed unused variables from tests
    
    * update footer
    
    * fixed lint errors, removed unused variables from tests
    
    * export footer content to file
    
    * sonar exlusion, export data from unit test

--- src/app/components/hmcts-global-footer/hmcts-global-footer.component.spec.ts
+++ src/app/components/hmcts-global-footer/hmcts-global-footer.component.spec.ts
@@ -1,9 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HmctsGlobalFooterComponent } from './hmcts-global-footer.component';
-import { helpData, navigationData } from './mock/hmcts-global-footer.mock';
-import { Component, DebugElement, Input, ViewChild } from '@angular/core';
-import { Helper, Navigation } from '../../containers/footer/footer.model';
-import { RouterModule } from '@angular/router';
+import {Component, Input, ViewChild} from '@angular/core';
+import {Helper, Navigation} from '../../containers/footer/footer.model';
+import { AppConstants } from '../../app.constants';
+
 
 describe('HmctsGlobalFooterComponent', () => {
     @Component({
@@ -24,15 +24,14 @@ describe('HmctsGlobalFooterComponent', () => {
     let fixture: ComponentFixture<HmctsGlobalFooterComponent>;
 
 
+    const helpData: Helper = AppConstants.FOOTER_DATA;
+    const navigationData: Navigation = AppConstants.FOOTER_DATA_NAVIGATION;
 
     beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [HmctsGlobalFooterComponent],
-            imports: [
-                RouterModule
-            ]
-        })
-            .compileComponents();
+      TestBed.configureTestingModule({
+        declarations: [ HmctsGlobalFooterComponent ]
+      })
+      .compileComponents();
     }));
 
     beforeEach(() => {
--- src/app/containers/footer/footer.component.ts
+++ src/app/containers/footer/footer.component.ts
@@ -1,30 +1,13 @@
 import { Component } from '@angular/core';
 import { Helper, Navigation } from './footer.model';
-import { environment } from '../../../environments/environment';
+import { AppConstants } from '../../app.constants';
+
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-    public helpData: Helper = {
-        heading: 'Help',
-        email: {
-            address: environment.serviceDeskEmail,
-            text: environment.serviceDeskEmail
-        },
-        phone: {
-            text: environment.serviceDeskTel
-        },
-        opening: {
-            text: 'Monday to Friday, 8am to 6pm'
-        }
-    };
-    public navigationData: Navigation = {
-        items: [
-            { text: 'Terms and conditions', href: 'terms-and-conditions'},
-            { text: 'Cookies', href: 'cookies' },
-            { text: 'Privacy policy', href: 'privacy-policy'}
-        ]
-    };
+    public helpData: Helper = AppConstants.FOOTER_DATA;
+    public navigationData: Navigation = AppConstants.FOOTER_DATA_NAVIGATION;
 }

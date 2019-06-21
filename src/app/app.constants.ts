d3d71ed9652b416c247be747ed0200f0850abab2
Boris <36511748+borislavpetrovhmcts@users.noreply.github.com>
2019-06-21 13:20:32 +0100

    Update footer (#57)
    * Test branch
    
    * Change to chart name
    
    * Update README.md
    
    * Update README.md
    
    * Local test
    
    * Changing local back
    
    * see if slack notifications are breaking build
    
    * update footer
    
    * fixed lint errors, removed unused variables from tests
    
    * export footer content to file
    
    * update footer
    
    * fixed lint errors, removed unused variables from tests
    
    * sonar exlusion, export data from unit test
    
    * EUI-69 Test added
    
    * git ignore
    
    * change auth attachment to api route
    
    * add smoke
    
    * smoke cmds
    
    * tests
    
    * echo
    
    * deleted reports folder added in gitignore
    
    * chore/add pr white list fix
    
    * EUI-157 - DON'T MERGE (#59)
    
    * Test branch
    
    * Change to chart name
    
    * Update README.md
    
    * Update README.md
    
    * Local test
    
    * Changing local back
    
    * Fixes for pipeline
    
    * Updating package json
    
    * Fixing config files
    
    * Updating index file
    
    * Updating preview config
    
    * Updating idam client
    
    * Preview config changes
    
    * see if slack notifications are breaking build
    
    * change auth attachment to api route
    
    * add smoke
    
    * smoke cmds
    
    * tests
    
    * echo
    
    * Updating config for preview
    
    * Feature/fz header eui 49 (#62)
    
    * removing fee accounts
    
    * adding header items nav title and user nav to state
    
    * title name changes
    
    * revming fee accounts and cleaning up
    
    * heaer working version
    
    * adding working version of routing
    
    * fixing some typings
    
    * adding test for selectors
    
    * Updating URL name
    
    * updating url
    
    * bug/correct logger
    
    * added headless on jenkins
    
    * Updating microservice
    
    * update footer
    
    * fixed lint errors, removed unused variables from tests
    
    * update footer
    
    * fixed lint errors, removed unused variables from tests
    
    * export footer content to file
    
    * sonar exlusion, export data from unit test

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -36,9 +36,33 @@ const manageOrgTitle: AppTitlesModel = {
   url: '/'
 };
 
+const FooterData =  {
+  heading: 'Help',
+  email: {
+    address: 'service-desk@hmcts.gov.uk',
+    text: 'service-desk@hmcts.gov.uk'
+  },
+  phone: {
+    text: '0207 633 4140'
+  },
+  opening: {
+    text: 'Monday to Friday, 8am to 6pm (excluding public holidays)'
+  }
+};
+
+const FooterDataNavigation = {
+  items: [
+    { text: 'Terms and conditions', href: 'terms-and-conditions'},
+    { text: 'Cookies', href: 'cookies' },
+    { text: 'Privacy policy', href: 'privacy-policy'}
+  ]
+};
+
 export class AppConstants {
   static NAV_ITEMS = navItems;
   static USER_NAV = userNav;
   static REG_ORG_TITLE = regOrgTitle;
   static MANAGE_ORG_TITLE = manageOrgTitle;
+  static FOOTER_DATA = FooterData;
+  static FOOTER_DATA_NAVIGATION = FooterDataNavigation;
 }

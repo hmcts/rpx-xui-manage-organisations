2284209a7399e507b8d4544b0ad42cf6241a0264
Borislav Petrov <Borislav.Petrov@HMCTS.NET>
2019-06-24 17:43:17 +0100

    updates to payload

--- api/lib/payloadBuilder.ts
+++ api/lib/payloadBuilder.ts
@@ -16,30 +16,19 @@ import {OrganisationPayload} from '../interfaces/organisationPayload'
 
 export function makeOrganisationPayload(stateValues): OrganisationPayload {
     return {
-      // hardcoded value for dev purpose
-      companyNumber: '12345678',
-      // hardcoded value for dev purpose
-      companyUrl: 'comp-url1',
       contactInformation: [
         {
           addressLine1: stateValues.officeAddressOne,
           addressLine2: stateValues.officeAddressTwo,
-          // hardcoded value for dev purpose
-          addressLine3: 'This needs to be clarified',
-          // hardcoded value for dev purpose
-          country: 'Test Country Name - UK',
           county: stateValues.county,
-        postcode: stateValues.postcode,
-        townCity: stateValues.townOrCity,
-          dxAddress: [
+          postcode: stateValues.postcode,
+          townCity: stateValues.townOrCity, dxAddress: [
             {
-              // hardcoded value for dev purpose
-              dxExchange: 'dxexchange1',
-              // hardcoded value for dev purpose
-              dxNumber: 'DX 1234567890',
+              dxExchange: stateValues.DXexchange,
+              dxNumber: stateValues.DXnumber,
             },
           ],
-      }
+        },
       ],
       name: stateValues.orgName,
       pbaAccounts: [
@@ -48,9 +37,6 @@ export function makeOrganisationPayload(stateValues): OrganisationPayload {
           pbaNumber: stateValues.PBAnumber2,
           },
       ],
-      // hardcoded value for dev purpose
-      sraId: 'sraId123',
-      sraRegulated: true,
       superUser: {
           email: stateValues.emailAddress,
           firstName: stateValues.firstName,

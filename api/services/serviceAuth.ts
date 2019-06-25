ff83cb5e8c77a496f1d376ccd8a3c5ea3a2d2c59
Boris <36511748+borislavpetrovhmcts@users.noreply.github.com>
2019-06-25 10:59:28 +0100

    Register Organisation with backend as part of Manage App (#71)
    * update settings
    
    * fixing local docker issues
    
    * end proxy before docker
    
    * update node docker
    
    * updates to payload

--- api/services/serviceAuth.ts
+++ api/services/serviceAuth.ts
@@ -1,10 +1,11 @@
+import { AxiosResponse } from 'axios'
 import * as express from 'express'
 import * as otp from 'otp'
 import { config } from '../lib/config'
 import { http } from '../lib/http'
-import { getHealth, getInfo } from '../lib/util'
-import { AxiosResponse } from 'axios'
 import * as log4jui from '../lib/log4jui'
+import { getHealth, getInfo } from '../lib/util'
+import { tunnel } from '../local'
 
 const url = config.services.s2s
 const microservice = config.microservice
@@ -15,8 +16,8 @@ const logger = log4jui.getLogger('service auth')
 export async function postS2SLease() {
     const configEnv = process ? process.env.PUI_ENV || 'local' : 'local'
     let request: AxiosResponse<any>
-    console.log('test2:', configEnv)
-    if (configEnv !== 'local') {
+    console.log('PUI_ENV is now:', configEnv)
+    if (configEnv !== 'ldocker') {
         const oneTimePassword = otp({ secret: s2sSecret }).totp()
 
         logger.info('generating from secret  :', s2sSecret, microservice, oneTimePassword)
@@ -27,9 +28,10 @@ export async function postS2SLease() {
         })
     } else {
         // this is only for local development against the RD docker image
-        request = await http.get(url)
+        // end tunnel before posting to docker
+        tunnel.end()
+        request = await http.get(`${url}`)
     }
-
     return request.data
 }
 

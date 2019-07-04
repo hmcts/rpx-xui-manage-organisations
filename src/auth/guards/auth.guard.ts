10e099c5d29df7caa2a1f961bf6cb58abfa14937
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-04 14:13:18 +0100

    EUI-358 another trial

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -12,6 +12,7 @@ export class AuthGuard implements CanActivate {
 
   canActivate() {
     if (!this.authService.isAuthenticated()) {
+      console.log('pr trial');
       this.authService.loginRedirect();
       return false;
     }

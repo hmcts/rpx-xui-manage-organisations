39e2045c2041af538b735f77883f26247c05055b
nagendrareddyk <kommurunagendra@gmail.com>
2019-07-16 09:27:25 +0100

    LoginLogout automation test

--- test/e2e/features/step_definitions/loginLogout.steps.js
+++ test/e2e/features/step_definitions/loginLogout.steps.js
@@ -48,7 +48,6 @@ defineSupportCode(function ({And, But, Given, Then, When}) {
   Then(/^I should be redirected to manage organisation page$/, async function () {
     browser.sleep(AMAZING_DELAY);
     await expect(headerPage.moPage.isDisplayed()).to.eventually.be.true;
-    browser.sleep(AMAZING_DELAY);
     await expect(headerPage.moPage.getText()).to.eventually.equal('Manage Organisation details for civil and family law cases');
   });
 
--- test/e2e/support/constants.js
+++ test/e2e/support/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
     LONG_DELAY: 8000,
-    AMAZING_DELAY: 16000,
+    AMAZING_DELAY: 18000,
     MID_DELAY: 6000,
     SHORT_DELAY: 4000
 };

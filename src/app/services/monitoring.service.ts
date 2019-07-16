affa750d44ff355668194c553e1436347d5a77b8
udaydenduluri33 <50869558+udaydenduluri33@users.noreply.github.com>
2019-07-16 14:11:25 +0100

    Appinsights changes (#104)
    * Appinsights changes
    
    * yarn changes
    
    * Adding APPINSIGHTS_INSTRUMENTATIONKEY_MO
    
    * Fix the linter error
    
    * Adding test for Monitoring service
    
    * Adding the tests for the Monitoring service
    
    * Making the tests work
    
    * Increase Monitoring test
    
    * More test cases
    
    * Adding Sonar properties
    
    * Adding tests
    
    * Increasing unit test coverage
    
    * Added new test
    
    * fix for lint error
    
    * rename APPINSIGHTS_INSTRUMENTATIONKEY_MO to APPINSIGHTS-INSTRUMENTATIONKEY-MO

---
+++ src/app/services/monitoring.service.ts
@@ -0,0 +1,93 @@
+import { Injectable, Optional } from '@angular/core';
+import { AppInsights } from 'applicationinsights-js';
+import { HttpClient } from '@angular/common/http';
+
+export interface IMonitoringService {
+  logPageView(name?: string, url?: string, properties?: any,
+              measurements?: any, duration?: number);
+  logEvent(name: string, properties?: any, measurements?: any);
+  logException(exception: Error);
+}
+
+export class MonitorConfig implements Microsoft.ApplicationInsights.IConfig {
+  instrumentationKey?: string;
+  endpointUrl?: string;
+  emitLineDelimitedJson?: boolean;
+  accountId?: string;
+  sessionRenewalMs?: number;
+  sessionExpirationMs?: number;
+  maxBatchSizeInBytes?: number;
+  maxBatchInterval?: number;
+  enableDebug?: boolean;
+  disableExceptionTracking?: boolean;
+  disableTelemetry?: boolean;
+  verboseLogging?: boolean;
+  diagnosticLogInterval?: number;
+  samplingPercentage?: number;
+  autoTrackPageVisitTime?: boolean;
+  disableAjaxTracking?: boolean;
+  overridePageViewDuration?: boolean;
+  maxAjaxCallsPerView?: number;
+  disableDataLossAnalysis?: boolean;
+  disableCorrelationHeaders?: boolean;
+  correlationHeaderExcludedDomains?: string[];
+  disableFlushOnBeforeUnload?: boolean;
+  enableSessionStorageBuffer?: boolean;
+  isCookieUseDisabled?: boolean;
+  cookieDomain?: string;
+  isRetryDisabled?: boolean;
+  url?: string;
+  isStorageUseDisabled?: boolean;
+  isBeaconApiDisabled?: boolean;
+  sdkExtension?: string;
+  isBrowserLinkTrackingEnabled?: boolean;
+  appId?: string;
+  enableCorsCorrelation?: boolean;
+}
+
+@Injectable()
+export class MonitoringService implements IMonitoringService {
+
+  constructor(private http: HttpClient, @Optional() private config?: MonitorConfig,
+              @Optional() private appInsights?: Microsoft.ApplicationInsights.IAppInsights) {
+              if (!appInsights) {
+                appInsights = AppInsights;
+              }
+            }
+
+  logPageView(name?: string, url?: string, properties?: any,
+              measurements?: any, duration?: number) {
+    this.send(() => {
+      this.appInsights.trackPageView(name, url, properties, measurements, duration);
+    });
+  }
+
+  logEvent(name: string, properties?: any, measurements?: any) {
+    this.send(() => {
+      this.appInsights.trackEvent(name, properties, measurements);
+    });
+  }
+
+  logException(exception: Error) {
+    this.send(() => {
+      this.appInsights.trackException(exception);
+    });
+  }
+
+  private send(func: () => any): void {
+    console.log('config is ' + this.config.instrumentationKey);
+    if (this.config && this.config.instrumentationKey) {
+      func();
+    } else {
+      this.http.get('/api/monitoring-tools').subscribe(it => {
+        this.config = {
+          instrumentationKey: it['key']
+        };
+        if (!AppInsights.config) {
+          this.appInsights.downloadAndSetup(this.config);
+        }
+        func();
+      });
+    }
+  }
+}

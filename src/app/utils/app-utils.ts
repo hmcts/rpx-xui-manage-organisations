7f67bbe44a5cf780c2a13ca7b18545c6b87f156c
fazanki <fazanki@gmail.com>
2019-07-16 10:38:21 +0100

    adding comments

--- src/app/utils/app-utils.ts
+++ src/app/utils/app-utils.ts
@@ -3,6 +3,9 @@
  *
  */
 export class AppUtils {
+  /**
+   * it changes active property based on the url
+   */
   static setActiveLink(items, router) {
     const nav = items.map((item: {href}) => {
       return {
@@ -14,6 +17,9 @@ export class AppUtils {
   }
 
   static setSetUserNavItems(state, routes) {
+    /**
+     * it manages user nav array based on the app that is running (register or otherwise)
+     */
     if (state && state.userNav && routes && routes.state.url) {
       const isRegister = routes.state.url.indexOf('register') === -1;
       return isRegister ? state.userNav : [];

c6571ae07269d958cb07784094169496150739cf
Uday Denduluri <uday.denduluri@gmail.com>
2019-07-16 10:37:15 +0100

    Increase Monitoring test

--- src/app/services/monitoring.service.spec.ts
+++ src/app/services/monitoring.service.spec.ts
@@ -1,18 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { TestBed, inject } from '@angular/core/testing';
-import { MonitoringService, IMonitoringService } from './monitoring.service';
+import { MonitoringService, IMonitoringService, MonitorConfig } from './monitoring.service';
 import { AppInsights } from 'applicationinsights-js';
 
 describe('Monitoring service', () => {
     const mockedHttpClient = jasmine.createSpyObj(['mockedHttpClient', 'get']);
     const mockedAppInsights = jasmine.createSpyObj(['mockedAppInsights', 'downloadAndSetup', 'trackException', 'trackEvent',
     'trackPageView']);
+    const mockedConfig = new MonitorConfig();
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [],
             providers: [
                 { provide: HttpClient, useValue: mockedHttpClient },
                 { provide: AppInsights, useValue: mockedAppInsights },
+                { provide: MonitorConfig, useValue: mockedConfig}
             ]
         });
 
@@ -20,29 +22,41 @@ describe('Monitoring service', () => {
             expect(service).toBeTruthy();
         }));
 
-        it('should be able to LogException', inject([MonitoringService], (service: IMonitoringService) => {
+        it('should be able to LogException and Should not call the http service', () => {
+            const service = new MonitoringService(mockedHttpClient, mockedConfig);
+            mockedConfig.instrumentationKey = 'somevalue';
+            expect(service).toBeTruthy();
+            service.logException(new Error('Some ErrorMesssage'));
+            expect(mockedHttpClient.get).not.toHaveBeenCalled();
+            expect(mockedAppInsights.downloadAndSetup).toHaveBeenCalled();
+            expect(mockedAppInsights.trackException).toHaveBeenCalled();
+        });
+
+        it('should be able to LogException', () => {
+            const service = new MonitoringService(mockedHttpClient, mockedConfig);
             expect(service).toBeTruthy();
             service.logException(new Error('Some ErrorMesssage'));
             expect(mockedHttpClient.get).toHaveBeenCalled();
             expect(mockedAppInsights.downloadAndSetup).toHaveBeenCalled();
             expect(mockedAppInsights.trackException).toHaveBeenCalled();
-        }));
+        });
 
-        it('should be able to LogEvent', inject([MonitoringService], (service: IMonitoringService) => {
+        it('should be able to LogEvent', () => {
+            const service = new MonitoringService(mockedHttpClient, mockedConfig);
             expect(service).toBeTruthy();
             service.logEvent('name', [], []);
             expect(mockedHttpClient.get).toHaveBeenCalled();
             expect(mockedAppInsights.downloadAndSetup).toHaveBeenCalled();
             expect(mockedAppInsights.trackEvent).toHaveBeenCalled();
-        }));
+        });
 
-        it('should be able to LogPageview', inject([MonitoringService], (service: IMonitoringService) => {
+        it('should be able to LogPageview', () => {
+            const service = new MonitoringService(mockedHttpClient, mockedConfig);
             expect(service).toBeTruthy();
             service.logPageView('name', null, [], [], 1);
             expect(mockedHttpClient.get).toHaveBeenCalled();
             expect(mockedAppInsights.downloadAndSetup).toHaveBeenCalled();
             expect(mockedAppInsights.trackPageView).toHaveBeenCalled();
-        }));
+        });
     });
-
 });
--- src/app/services/monitoring.service.ts
+++ src/app/services/monitoring.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Optional } from '@angular/core';
 import { AppInsights } from 'applicationinsights-js';
 import { HttpClient } from '@angular/common/http';
 
@@ -9,12 +9,46 @@ export interface IMonitoringService {
   logException(exception: Error);
 }
 
+export class MonitorConfig implements Microsoft.ApplicationInsights.IConfig {
+  instrumentationKey?: string;
+  endpointUrl?: string;
+  emitLineDelimitedJson?: boolean;
+  accountId?: string;
+  sessionRenewalMs?: number;
+  sessionExpirationMs?: number;
+  maxBatchSizeInBytes?: number;
+  maxBatchInterval?: number;
+  enableDebug?: boolean;
+  disableExceptionTracking?: boolean;
+  disableTelemetry?: boolean;
+  verboseLogging?: boolean;
+  diagnosticLogInterval?: number;
+  samplingPercentage?: number;
+  autoTrackPageVisitTime?: boolean;
+  disableAjaxTracking?: boolean;
+  overridePageViewDuration?: boolean;
+  maxAjaxCallsPerView?: number;
+  disableDataLossAnalysis?: boolean;
+  disableCorrelationHeaders?: boolean;
+  correlationHeaderExcludedDomains?: string[];
+  disableFlushOnBeforeUnload?: boolean;
+  enableSessionStorageBuffer?: boolean;
+  isCookieUseDisabled?: boolean;
+  cookieDomain?: string;
+  isRetryDisabled?: boolean;
+  url?: string;
+  isStorageUseDisabled?: boolean;
+  isBeaconApiDisabled?: boolean;
+  sdkExtension?: string;
+  isBrowserLinkTrackingEnabled?: boolean;
+  appId?: string;
+  enableCorsCorrelation?: boolean;
+}
+
 @Injectable()
 export class MonitoringService implements IMonitoringService {
 
-  private config: Microsoft.ApplicationInsights.IConfig;
-
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, @Optional() private config?: MonitorConfig) {
   }
 
   logPageView(name?: string, url?: string, properties?: any,

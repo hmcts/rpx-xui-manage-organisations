0fc84736520f537558b05b59748bf681f3c6327f
Franco Zanki <fazanki@users.noreply.github.com>
2019-07-16 10:38:52 +0100

    Features/fz invite user eui 289 (#98)
    * fixing FE error
    
    * trying to sort logout
    
    * reverting the session
    
    * navigarion fixes
    
    * moving the logic for ative state to state
    
    * cleaning up
    
    * fixing unit tests
    
    * fixing build
    
    * comments as per adnans pr
    
    * fixing unit tests to pass sonar
    
    * bug fix - sign out removed on register or page
    
    * fixing sonar
    
    * adding comments

--- api/routes.ts
+++ api/routes.ts
@@ -10,10 +10,6 @@ import stateRouter from './states'
 const router = express.Router({ mergeParams: true })
 
 router.use(auth.attach) // attach user-profile to all api routes
-
-router.use('/logout', (req, res, next) => {
-    auth.doLogout(req, res)
-})
 router.use('/decisions', stateRouter)
 router.use('/organisation', organisationRouter)
 router.use('/accounts', accountsRouter)
--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -7,7 +7,6 @@ import {NavItemsModel} from '../../models/nav-items.model';
 import {AppTitlesModel} from '../../models/app-titles.model';
 import {UserNavModel} from '../../models/user-nav.model';
 import * as fromActions from '../../store';
-import {tap} from 'rxjs/operators';
 /**
  * Root Component that bootstrap all application.
  * It holds the state for global components (header and footer)
@@ -23,9 +22,9 @@ export class AppComponent implements OnInit {
   identityBar$: Observable<string[]>;
 
   pageTitle$: Observable<string>;
-  navItems$: Observable<NavItemsModel[]> ;
+  navItems$: Observable<{navItems: NavItemsModel[]}> ;
   appHeaderTitle$: Observable<AppTitlesModel>;
-  userNav$: Observable<UserNavModel>;
+  userNav$: Observable<any>;
 
 
   constructor(
@@ -37,13 +36,7 @@ export class AppComponent implements OnInit {
     // this.identityBar$ = this.store.pipe(select(fromSingleFeeAccountStore.getSingleFeeAccountData));
 
     this.pageTitle$ = this.store.pipe(select(fromRoot.getPageTitle));
-    this.navItems$ = this.store.pipe(select(fromRoot.getNavItems),
-      tap(item => {
-        if (item.length) {
-          // always redirect to the first item due to assigned permissions landing pages can change.
-          this.store.dispatch(new fromRoot.Go({path: [item[0].href]}));
-        }
-      }));
+    this.navItems$ = this.store.pipe(select(fromRoot.getNavItems));
     this.appHeaderTitle$ = this.store.pipe(select(fromRoot.getHeaderTitle));
     this.userNav$ = this.store.pipe(select(fromRoot.getUserNav));
 
@@ -54,7 +47,6 @@ export class AppComponent implements OnInit {
       }
     });
   }
-
   onNavigate(event): void {
     if (event === 'sign-out') {
       return this.store.dispatch(new fromActions.Logout());

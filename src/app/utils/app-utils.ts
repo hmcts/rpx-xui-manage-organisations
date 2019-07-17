55661d7dc70fb5c832099edce1d6d629e837759e
fazanki <fazanki@gmail.com>
2019-07-17 10:31:33 +0100

    cleaning up;

--- src/app/utils/app-utils.ts
+++ src/app/utils/app-utils.ts
@@ -29,18 +29,41 @@ export class AppUtils {
 
   static setPageTitle(url): string {
     /**
-     * it sets correct page titles
+     * it sets correct page titles based on the url.
      */
     if (url.indexOf('invite-user') !== -1) {
-      return 'Invite Users - Manage organisation';
-    } else if (url.indexOf('organisation') !== -1) {
-      return 'Organisation - Manage organisation';
+      return 'Invite user - Manage organisation';
     } else if (url.indexOf('profile') !== -1) {
       return 'Profile - Manage organisation';
     } else if (url.indexOf('users') !== -1) {
       return 'Users - Manage organisation';
+    } else if (url.indexOf('organisation-name') !== -1) {
+      return 'Organisation name - Register organisation';
+    } else if (url.indexOf('organisation-address') !== -1) {
+      return 'Organisation address - Register organisation';
+    } else if (url.indexOf('organisation-pba') !== -1) {
+      return 'PBA - Register organisation';
+    } else if (url.indexOf('have-dx') !== -1) {
+      return 'DX - Register organisation';
+    } else if (url.indexOf('organisation-dx') !== -1) {
+      return 'DX reference - Register organisation';
+    } else if (url.indexOf('haveSra') !== -1) {
+      return 'SRA - Register organisation';
+    } else if (url.indexOf('sraNumber') !== -1) {
+      return 'SRA number - Register organisation';
+    } else if (url.indexOf('name') !== -1) {
+      return 'Name - Register organisation';
+    } else if (url.indexOf('email-address') !== -1) {
+      return 'Email - Register organisation';
+    } else if (url.indexOf('check') !== -1) {
+      return 'Check answers - Register organisation';
+    } else if (url.indexOf('check') !== -1) {
+      return 'Check answers - Register organisation';
+    } else if (url.indexOf('organisation') !== -1) {
+      return 'Organisation details - Manage organisation';
+    } else if (url.indexOf('register-org/register') !== -1) {
+      return 'Register - Register organisation';
     }
-
     return 'Manage organisation';
   }
 }

f583d09255cf3358863cc472ffd216f8c7e4fc93
Toireasa Moley <toireasa.moley@hmcts.net>
2019-07-17 12:47:16 +0100

    Updating payload builder

--- api/lib/payloadBuilder.ts
+++ api/lib/payloadBuilder.ts
@@ -12,34 +12,8 @@ import { OrganisationPayload } from '../interfaces/organisationPayload'
  * @return
  */
 
-// should take in a string, or null
-// returns a string
-// if null return empty string
-function notNullOrUndefined(fieldMapping, value) {
-  if (value) {
-    return value;
-  }
-  if (fieldMapping == 'pbaAccounts' && value === null) {
-    console.log('null', value)
-    return 'PBA1234567678';
-  }
-  if (fieldMapping == 'sraId' && value == undefined) {
-    console.log('undefined', value)
-    return 'sraTempNumber';
-  }
-  if (fieldMapping == 'dxExchange' && value == null) {
-    console.log('null', value)
-    return '12345678901234567890';
-  }
-  if (fieldMapping == 'dxNumber' && value == null) {
-    console.log('null', value)
-    return '1234567890123';
-  }
-}
-
 function setPropertyIfNotNull(organisationPayload, propertyName, value) {
   if (value != null) {
-    console.log(value + 'not null, adding')
     organisationPayload[propertyName] =
       value
   }
@@ -47,7 +21,6 @@ function setPropertyIfNotNull(organisationPayload, propertyName, value) {
 
 function setPBAPropertiesIfNotNull(organisationPayload, propertyName1, propertyName2, value1, value2) {
   if (value1 != null || value2 != null) {
-    console.log(value1 + value2 + 'is not null, adding')
     organisationPayload[propertyName1] = [{
       [propertyName1]: value1,
       [propertyName2]: value2
@@ -58,7 +31,6 @@ function setPBAPropertiesIfNotNull(organisationPayload, propertyName1, propertyN
 function setDXPropertiesIfNotNull(organisationPayload, propertyName1, propertyName2, arrayName, value1, value2) {
 
   if (value1 != null || value2 != null) {
-    console.log(value1 + 'not null, adding')
     var [contactInformationArray] = organisationPayload.contactInformation
     contactInformationArray[arrayName] = [{
       [propertyName1]: value1,
@@ -91,8 +63,6 @@ export function makeOrganisationPayload(stateValues): any {
   setPBAPropertiesIfNotNull(organisationPayload, 'pbaAccounts', 'pbaNumber', stateValues.PBAnumber1, stateValues.PBAnumber2)
   setDXPropertiesIfNotNull(organisationPayload, 'dxExchange', 'dxNumber', 'dxAddress',
     stateValues.DXexchange, stateValues.DXnumber)
-
-  console.log('organisation payload is', organisationPayload)
 
   return organisationPayload;
 }

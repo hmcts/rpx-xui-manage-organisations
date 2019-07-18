c2e7b6c05d13a83ea94ec58acd68deaaaf3c0519
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-18 17:55:22 +0100

    EUI-452 unfinished

--- src/app/containers/index.ts
+++ src/app/containers/index.ts
@@ -1,11 +1,14 @@
 
 import {FooterComponent} from './footer/footer.component';
 import {HeaderComponent} from './header/header.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const containers: any[] = [
   HeaderComponent,
   FooterComponent,
+  NotFoundComponent,
 ];
 
 export * from './footer/footer.component';
 export * from './header/header.component';
+export * from './not-found/not-found.component';

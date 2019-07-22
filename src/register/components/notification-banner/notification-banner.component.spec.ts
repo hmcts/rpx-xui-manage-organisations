4238914dec3b6a554287e9065f3594b8eca44a1a
toireasam <toireasa.moley@hmcts.net>
2019-07-22 12:48:20 +0100

    EUI-258-server-errors (#111)
    * Remove null fields on post
    
    * Updating payload
    
    * Bubbling up errors
    
    * Updating reducer
    
    * Updating payload builder
    
    * Updating payload builder
    
    * tidy up functions
    
    * Updating reducer
    
    * Update error messages
    
    * Updating reducer
    
    * Updating error messages
    
    * Updating error messaging
    
    * Updating validation
    
    * Updating errors
    
    * Updating error messages
    
    * Updating description
    
    * Updating error messages
    
    * Updating validation
    
    * Updating reducer
    
    * Updating reducer
    
    * Updating reducer
    
    * Refactor
    
    * Adding component
    
    * First banner attempt
    
    * banner attempt 2
    
    * Updating banner
    
    * Updating errors
    
    * Updating tests
    
    * Updating tests
    
    * Removing bug
    
    * Updating payload builder
    
    * Updating reducer and actions
    
    * Updating conflicts resolved
    
    * Updating conflicts
    
    * Tested
    
    * Tidy up
    
    * Fixing lint errors
    
    * Updating docker config
    
    * Updating reducer
    
    * Updating reducer
    
    * updating code
    
    * Setting old docker config
    
    * Review fixes
    
    * Updating registration component
    
    * old docker config
    
    * Fixing lint errors
    
    * Updating payload builder

---
+++ src/register/components/notification-banner/notification-banner.component.spec.ts
@@ -0,0 +1,8 @@
+import { NotificationBannerComponent } from './notification-banner.component';
+
+describe('Notification Banner Component', () => {
+    it('should have a component', () => {
+        const component = new NotificationBannerComponent();
+        expect(component).toBeTruthy();
+});
+});

c21469ca385d53a7ce24bd40888826b9e785f532
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-23 17:35:42 +0100

    EUI-452 unfinished

--- src/shared/services/health-check.service.ts
+++ src/shared/services/health-check.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subscription } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { forkJoin } from 'rxjs';
 import * as fromRoot from '../../app/store';
 import { Store, select } from '@ngrx/store';
 
 @Injectable()
-export class HealthCheckService {
+export class HealthCheckService implements OnDestroy {
+
+    routeSubscription: Subscription;
+    httpSubscription: Subscription;
 
     constructor(
         private http: HttpClient,
@@ -15,22 +18,36 @@ export class HealthCheckService {
     ) { }
 
     doHealthCheck(): Observable<boolean> {
-        const healthState: boolean = true;
+        const healthState: boolean = false;
         let result: { healthState } = { healthState };
         let path = '';
 
-        this.store.pipe(select(fromRoot.getRouterUrl)).subscribe(value => {
+        this.routeSubscription = this.store.pipe(select(fromRoot.getRouterUrl)).subscribe(value => {
             path = value;
-        });
 
-        if (path !== '') {
-            this.http.get('/api/healthCheck?path=' + encodeURIComponent(path)).subscribe((value: { healthState }) => {
-                result = value;
-            });
-        }
+            if (path !== '') {
+                this.httpSubscription = this.http.get('/api/healthCheck?path=' + encodeURIComponent(path))
+                    .subscribe((res: { healthState }) => {
+                        result = res;
+                        if (!result.healthState) {
+                            this.store.dispatch(new fromRoot.Go({ path: ['/not-found'] }));
+                        }
+                        this.routeSubscription.unsubscribe();
+                    });
+            }
+        });
 
 
         return of(result.healthState);
+    }
+
+    ngOnDestroy() {
+        if (this.routeSubscription) {
+            this.routeSubscription.unsubscribe();
+        }
+        if (this.httpSubscription) {
+            this.httpSubscription.unsubscribe();
+        }
     }
 
 }

32065d05d55bbac9b748fbc9176483583d02f8df
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-24 17:13:25 +0100

    EUI-452 reverting 'v1'

--- api/lib/config/environments/local.config.ts
+++ api/lib/config/environments/local.config.ts
@@ -10,7 +10,7 @@ export default {
         emAnnoApi: 'https://em-anno-aat.service.core-compute-aat.internal',
         emNpaApi: 'https://em-npa-aat.service.core-compute-aat.internal',
         cohCorApi: 'https://coh-cor-aat.service.core-compute-aat.internal',
-        rdProfessionalApi: 'https://rpa-rd-professional-aat.service.core-compute-aat.internal/v1',
+        rdProfessionalApi: 'https://rpa-rd-professional-aat.service.core-compute-aat.internal',
     },
     health: {
         ccdDataApi: 'https://ccd-data-store-api-aat.service.core-compute-aat.internal/health',

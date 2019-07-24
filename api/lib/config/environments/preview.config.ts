e4d30962d4b547990c2fbae3cbe603031b29336a
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-24 16:21:21 +0100

    EUI-452 still unfinished

--- api/lib/config/environments/preview.config.ts
+++ api/lib/config/environments/preview.config.ts
@@ -9,6 +9,13 @@ export default {
         s2s: 'https://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
         rdProfessionalApi: 'https://rd-professional-api-preview.service.core-compute-preview.internal',
     },
+    health: {
+        ccdDataApi: 'https://ccd-data-store-api-aat.service.core-compute-aat.internal/health',
+        ccdComponentApi: 'https://ccd-api-gateway-web-aat.service.core-compute-aat.internal/health',
+        idamWeb: 'https://idam-web-public.aat.platform.hmcts.net/health',
+        idamApi: 'https://idam-api.aat.platform.hmcts.net/health',
+        s2s: 'https://rpe-service-auth-provider-aat.service.core-compute-aat.internal/health',
+    },
     sessionSecret: 'secretSauce',
     useProxy: false,
 }
---
+++ src/app/containers/shutter/shutter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-shutter',
+    templateUrl: './shutter.component.html'
+})
+export class ShutterComponent {
+    constructor() {
+    }
+}

a2a4ed10f962e8cd42829aac742f05f992c36961
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-24 16:46:49 +0100

    EUI-452 removing unused env files and reverting config changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,17 +2,5 @@ export const environment = {
     production: true,
     googleAnalyticsKey: 'UA-124734893-1',
     serviceDeskEmail: 'DCD-ITServiceDesk@hmcts.net',
-    serviceDeskTel: '0300 3030686',
-    services: {
-        ccdDataApi: 'https://ccd-data-store-api-prod.service.core-compute-prod.internal',
-        ccdDefApi: 'https://ccd-definition-store-api-prod.service.core-compute-prod.internal',
-        idamWeb: 'https://idam-web-public-idam-prod.service.core-compute-prod.internal',
-        idamApi: 'https://idam-api-idam-prod.service.core-compute-prod.internal',
-        s2s: 'https://rpe-service-auth-provider-prod.service.core-compute-prod.internal',
-        draftStoreApi: 'https://draft-store-service-prod.service.core-compute-prod.internal',
-        dmStoreApi: 'https://dm-store-prod.service.core-compute-prod.internal',
-        emAnnoApi: 'https://em-anno-prod.service.core-compute-prod.internal',
-        emNpaApi: 'https://em-npa-prod.service.core-compute-prod.internal',
-        rdProfessionalApi: 'https://rpa-rd-professional-aat.service.core-compute-aat.internal',
-    }
+    serviceDeskTel: '0300 3030686'
 };

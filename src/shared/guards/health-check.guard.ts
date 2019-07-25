d556bc3c37898b1be8993e44ed626f99a5c83d9d
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-25 18:02:21 +0100

    EUI-452 placing of guard and testing

--- src/shared/guards/health-check.guard.ts
+++ src/shared/guards/health-check.guard.ts
@@ -3,24 +3,40 @@ import { CanActivate } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { catchError, switchMap } from 'rxjs/operators';
 import { HealthCheckService } from '../services/health-check.service';
+import * as fromRoot from '../../app/store';
+import { Store } from '@ngrx/store';
 
 
 @Injectable()
 export class HealthCheckGuard implements CanActivate {
     constructor(
-        private healthCheck: HealthCheckService
+        private healthCheck: HealthCheckService,
+        private store: Store<fromRoot.State>,
     ) {
     }
 
     canActivate() {
         return this.checkStore().pipe(
-            switchMap((res) => of(res)),
-            catchError(() => of(false))
+            switchMap((res: any) => {
+                const state = res.healthState;
+                if (!state) {
+                    this.redirectToShutter();
+                }
+                return of(res.healthState);
+            }),
+            catchError(() => {
+                this.redirectToShutter();
+                return of(false);
+            })
         );
     }
 
     checkStore(): Observable<boolean> {
         return this.healthCheck.doHealthCheck();
+    }
+
+    redirectToShutter() {
+        this.store.dispatch(new fromRoot.Go({ path: ['/service-down'] }));
     }
 }
 

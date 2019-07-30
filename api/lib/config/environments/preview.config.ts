f7149e0c6bf68988425731a44cf04d4f9779113f
Toireasa Moley <toireasa.moley@hmcts.net>
2019-07-30 15:30:16 +0100

    Updating preview

--- api/lib/config/environments/preview.config.ts
+++ api/lib/config/environments/preview.config.ts
@@ -4,8 +4,8 @@ export default {
     services: {
         ccdDataApi: 'https://ccd-data-store-api-aat.service.core-compute-aat.internal',
         ccdComponentApi: 'https://ccd-api-gateway-web-aat.service.core-compute-aat.internal',
-        idamWeb: 'https://idam-web-public.aat.platform.hmcts.net',
-        idamApi: 'https://idam-api.aat.platform.hmcts.net',
+        idamWeb: 'https://idam-web-public.preview.platform.hmcts.net',
+        idamApi: 'https://idam-api.preview.platform.hmcts.net',
         s2s: 'https://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
         rdProfessionalApi: 'https://rd-professional-api-preview.service.core-compute-preview.internal',
     },

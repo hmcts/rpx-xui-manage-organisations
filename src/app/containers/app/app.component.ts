1e124a3f4740336067ec0b8fd7afea4b733f06df
fazanki <fazanki@gmail.com>
2019-07-30 14:36:23 +0100

    adding scope to redirect url

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -3,7 +3,6 @@ import { Store, select } from '@ngrx/store';
 
 import * as fromRoot from '../../store';
 import { Observable } from 'rxjs';
-import {NavItemsModel} from '../../models/nav-items.model';
 import {AppTitlesModel} from '../../models/app-titles.model';
 import {UserNavModel} from '../../models/user-nav.model';
 import * as fromActions from '../../store';
@@ -23,7 +22,7 @@ export class AppComponent implements OnInit {
   identityBar$: Observable<string[]>;
 
   pageTitle$: Observable<string>;
-  navItems$: Observable<{navItems: NavItemsModel[]}> ;
+  navItems$: Observable<any> ;
   appHeaderTitle$: Observable<AppTitlesModel>;
   userNav$: Observable<UserNavModel>;
 

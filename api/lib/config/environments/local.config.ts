14199252d12d03ef5ef2e01cd63c267bad8f9be5
toireasam <toireasa.moley@hmcts.net>
2019-07-31 12:12:05 +0100

    Update local.config.ts

--- api/lib/config/environments/local.config.ts
+++ api/lib/config/environments/local.config.ts
@@ -2,8 +2,8 @@ export default {
     services: {
         ccdDataApi: 'https://ccd-data-store-api-aat.service.core-compute-aat.internal',
         ccdDefApi: 'https://ccd-definition-store-api-aat.service.core-compute-aat.internal',
-        idamWeb: 'https://idam-web-public.aat.platform.hmcts.net',
-        idamApi: 'https://idam-api.aat.platform.hmcts.net',
+        idamWeb: 'https://idam-web-public.preview.platform.hmcts.net',
+        idamApi: 'https://idam-api.preview.platform.hmcts.net',
         s2s: 'https://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
         draftStoreApi: 'https://draft-store-service-aat.service.core-compute-aat.internal',
         dmStoreApi: 'https://dm-store-aat.service.core-compute-aat.internal',

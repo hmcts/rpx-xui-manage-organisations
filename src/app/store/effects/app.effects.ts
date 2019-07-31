5d6c37f494aafe7453beda2c21addbd6473cbee5
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-07-31 16:06:24 +0100

    EUI-518 service test

--- src/app/store/effects/app.effects.ts
+++ src/app/store/effects/app.effects.ts
@@ -38,7 +38,8 @@ export class AppEffects {
   logout$ = this.actions$.pipe(
     ofType(appActions.LOGOUT),
     map(() => {
-      window.location.href = 'api/logout';
+      // window.location.href = 'api/logout';
+      this.authService.signOut();
     })
   );
 

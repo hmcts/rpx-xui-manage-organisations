ded6a043d7cba546c7f7d69a55ef5b2b2c97c780
fazanki <fazanki@gmail.com>
2019-08-05 16:52:51 +0100

    setting permission routing

---
+++ src/app/containers/redirect/redirect.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Store, select } from '@ngrx/store';
+
+import * as fromRoot from '../../store';
+import * as fromStore from '../../../user-profile/store';
+
+import {Subscription} from 'rxjs';
+/**
+ * Component used for redirection based on permissions.
+ * Please replace this in future by guards
+ */
+@Component({
+  selector: 'app-redirect',
+  template: ``
+})
+export class RedirectComponent implements OnInit, OnDestroy {
+  redirected = false;
+  subscription: Subscription;
+
+  constructor(
+    private store: Store<fromRoot.State>
+  ) {}
+
+  ngOnInit() {
+    this.store.dispatch(new fromStore.GetUserDetails());
+    this.subscription = this.store.pipe(select(fromRoot.getNavItems)).subscribe(nav => {
+      if (nav && nav.navItems.length && !this.redirected) {
+        this.store.dispatch(new fromRoot.Go({path: [nav.navItems[0].href]}));
+        this.redirected = true;
+      }
+    });
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}

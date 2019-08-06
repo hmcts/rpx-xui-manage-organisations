430c4f645540251469996b15e93287a31f168828
Philip <philip.aberneithy@gmail.com>
2019-08-06 16:45:52 +0100

    Working on S2s token generation.

--- api/lib/s2sTokenGeneration.ts
+++ api/lib/s2sTokenGeneration.ts
@@ -12,32 +12,59 @@ const url = config.services.s2s
 const logger = log4jui.getLogger('service user-profile')
 
 /**
- * Return the generated S2s token
+ * generateOneTimePassword
  *
+ * With our S2S Secret we generate a one time password, that is used to get our our S2S Token.
+ *
+ * @param s2sSecret
+ * @returns {any}
+ */
+export function generateOneTimePassword(s2sSecret) {
+
+  return otp({secret: s2sSecret}).totp()
+}
+
+/**
+ * Return the generated S2S token
+ *
+ * Note we do not rely on Idam to generate an S2S Token. Therefore it can be outside of any Idam authentication
+ * layer.
+ *
+ * @microservice xui_webapp
+ * @url https://rpe-service-auth-provider-aat.service.core-compute-aat.internal
  * @returns {string}
  */
 export async function generateS2sToken() {
-  console.log('s2sSecret')
-  console.log(s2sSecret)
-  console.log('microservice')
-  console.log(microservice)
-  console.log('url')
-  console.log(url)
 
-  const oneTimePassword = otp({secret: s2sSecret}).totp()
-
-  logger.info('generating from secret  :', s2sSecret, microservice, oneTimePassword)
+  const oneTimePassword = generateOneTimePassword(s2sSecret)
+  logger.info('Generating the S2S token from secret  :', s2sSecret, microservice, oneTimePassword)
 
   try {
-    const response = await http.post(`${url}/lease`, {
-      microservice,
-      oneTimePassword,
-    })
-    console.log('response')
-    console.log(response)
-    return 'hello s2s token'
+
+    const s2sTokenResponse = await requestS2sToken(url, microservice, oneTimePassword)
+    const s2sToken = s2sTokenResponse.data
+    console.log('s2sToken')
+    console.log(s2sToken)
+    return s2sToken
   } catch (error) {
     console.log('error')
     console.log(error)
   }
+}
+
+/**
+ * Request S2S Token
+ *
+ * @param url - ie. https://rpe-service-auth-provider-aat.service.core-compute-aat.internal
+ * @param microservice - xui_webapp
+ * @param oneTimePassword - Generated with a one time password generator
+ *
+ * @returns {Promise<AxiosPromise<any>>}
+ */
+export async function requestS2sToken(url, microservice, oneTimePassword) {
+
+  return http.post(`${url}/lease`, {
+    microservice,
+    oneTimePassword,
+  })
 }

60f91196c70f433623a36f2befb89983f941baba
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2019-08-06 17:33:02 +0100

    demo changes

--- api/lib/config/environments/demo.config.ts
+++ api/lib/config/environments/demo.config.ts
@@ -1,5 +1,5 @@
 export default {
-<<<<<<< HEAD
+
   services: {
     ccdDataApi: 'https://ccd-data-store-api-demo.service.core-compute-demo.internal',
     ccdDefApi: 'https://ccd-definition-store-api-demo.service.core-compute-demo.internal',
@@ -20,46 +20,5 @@ export default {
   secureCookie: false,
   sessionSecret: 'secretSauce',
   logging: 'debug',
-=======
-    services: {
-        ccdDataApi: 'https://ccd-data-store-api-demo.service.core-compute-demo.internal',
-        ccdDefApi: 'https://ccd-definition-store-api-aat.service.core-compute-aat.internal',
-        idamWeb: 'https://idam-web-public.aat.platform.hmcts.net',
-        idamApi: 'https://idam-api.aat.platform.hmcts.net',
-        s2s: 'https://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
-        draftStoreApi: 'https://draft-store-service-demo.service.core-compute-demo.internal',
-        dmStoreApi: 'https://dm-store-demo.service.core-compute-demo.internal',
-        emAnnoApi: 'https://em-anno-demo.service.core-compute-demo.internal',
-        emNpaApi: 'https://em-npa-demo.service.core-compute-demo.internal',
-        rdProfessionalApi: 'https://rd-professional-api-preview.service.core-compute-preview.internal',
-    },
-    health: {
-        ccdDataApi: 'https://ccd-data-store-api-demo.service.core-compute-demo.internal/health',
-        ccdDefApi: 'https://ccd-definition-store-api-aat.service.core-compute-aat.internal/health',
-        idamWeb: 'https://idam-web-public.aat.platform.hmcts.net/health',
-        idamApi: 'https://idam-api.aat.platform.hmcts.net/health',
-        s2s: 'https://rpe-service-auth-provider-demo.service.core-compute-demo.internal/health',
-        draftStoreApi: 'https://draft-store-service-demo.service.core-compute-demo.internal/health',
-        dmStoreApi: 'https://dm-store-demo.service.core-compute-demo.internal/health',
-        emAnnoApi: 'https://em-anno-demo.service.core-compute-demo.internal/health',
-        emNpaApi: 'https://em-npa-demo.service.core-compute-demo.internal/health',
-        rdProfessionalApi: 'https://rd-professional-api-preview.service.core-compute-preview.internal/health',
-    },
-    useProxy: false,
-    secureCookie: false,
-    sessionSecret: 'secretSauce',
-    logging: 'debug',
-    jurisdictions: [
-        {id: 'SSCS'},
-        {id: 'Divorce'},
-        {id: 'Probate'},
-        {id: 'Public Law'},
-        {id: 'Bulk Scanning'},
-        {id: 'Immigration & Asylum'},
-        {id: 'Civil Money Claims'},
-        {id: 'Employment'},
-        {id: 'Family public law and adoption'},
-        {id: 'Civil enforcement and possession'},
-        ],
->>>>>>> 7d540f7bda48ae7e87391f60172b9071c59c21c3
+
 }

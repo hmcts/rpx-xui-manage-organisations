81b629e5a64440722992bfd8558d1fe16b6d7590
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-08-14 17:23:57 +0100

    EUI-218 done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AuthGuard } from '../user-profile/guards/auth.guard';
 import { ServiceDownComponent } from './containers/service-down/service-down.component';
 import {RedirectComponent} from './containers/redirect/redirect.component';
 import { CookiePolicyComponent } from './containers/cookie-policy/cookie-policy.component';
+import { TermsAndConditionsComponent } from './containers';
 
 export const ROUTES: Routes = [
   {
@@ -41,6 +42,10 @@ export const ROUTES: Routes = [
   {
     path: 'cookies',
     component: CookiePolicyComponent
+  },
+  {
+    path: 'terms-and-conditions',
+    component: TermsAndConditionsComponent
   },
   {
     path: '**',

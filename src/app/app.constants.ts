ddca702bfcf9c0087df7fa0c878ba2a37c4c84dd
Franco Zanki <fazanki@users.noreply.github.com>
2019-08-14 15:53:16 +0100

    removing probates roles (#224)

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -86,8 +86,6 @@ const ccdRoles = [
   'caseworker-divorce-solicitor',
   'caseworker-divorce-financialremedy',
   'caseworker-divorce-financialremedy-solicitor',
-  'caseworker-probate',
-  'caseworker-probate-solicitor'
 ];
 
 /**

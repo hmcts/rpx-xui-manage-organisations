009e9512e48faa53a8a5a72fd6c40378d1cc4f55
Philip <philip.aberneithy@gmail.com>
2019-08-21 15:14:57 +0100

    Point to Preview branch to test https.

--- api/lib/config/environments/preview.config.ts
+++ api/lib/config/environments/preview.config.ts
@@ -4,10 +4,10 @@ export default {
     services: {
         ccdDataApi: 'https://ccd-data-store-api-aat.service.core-compute-aat.internal',
         ccdComponentApi: 'https://ccd-api-gateway-web-aat.service.core-compute-aat.internal',
-        idamWeb: 'https://idam-web-public.preview.platform.hmcts.net',
-        idamApi: 'https://idam-api.preview.platform.hmcts.net',
-        s2s: 'https://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
-        rdProfessionalApi: 'https://rd-professional-api-preview.service.core-compute-preview.internal',
+        idamWeb: 'https://idam-web-public.demo.platform.hmcts.net',
+        idamApi: 'https://idam-api.demo.platform.hmcts.net',
+        s2s: 'https://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
+        rdProfessionalApi: 'https://rd-professional-api-demo.service.core-compute-demo.internal',
     },
     health: {
         ccdDataApi: 'https://ccd-data-store-api-aat.service.core-compute-aat.internal/health',

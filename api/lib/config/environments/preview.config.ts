0b89c7086226ac849c0286ee50242bf88cddaf4d
Uday Denduluri <uday.denduluri@gmail.com>
2019-08-22 14:46:49 +0100

    Change Preview to Demo for testing on the PR

--- api/lib/config/environments/preview.config.ts
+++ api/lib/config/environments/preview.config.ts
@@ -1,24 +1,26 @@
 export default {
-    logging: 'debug',
-    secureCookie: false,
+
     services: {
-        ccdDataApi: 'https://ccd-data-store-api-aat.service.core-compute-aat.internal',
-        ccdComponentApi: 'https://ccd-api-gateway-web-aat.service.core-compute-aat.internal',
-        idamWeb: 'https://idam-web-public.preview.platform.hmcts.net',
-        idamApi: 'https://idam-api.preview.platform.hmcts.net',
-        s2s: 'https://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
-        rdProfessionalApi: 'https://rd-professional-api-preview.service.core-compute-preview.internal',
+      ccdDataApi: 'https://ccd-data-store-api-demo.service.core-compute-demo.internal',
+      ccdDefApi: 'https://ccd-definition-store-api-demo.service.core-compute-demo.internal',
+      idamWeb: 'https://idam-web-public.demo.platform.hmcts.net',
+      idamApi: 'https://idam-api.demo.platform.hmcts.net',
+      s2s: 'https://rpe-service-auth-provider-demo.service.core-compute-demo.internal',
+      rdProfessionalApi: 'https://rd-professional-api-demo.service.core-compute-demo.internal',
     },
     health: {
-        ccdDataApi: 'https://ccd-data-store-api-aat.service.core-compute-aat.internal/health',
-        ccdComponentApi: 'https://ccd-api-gateway-web-aat.service.core-compute-aat.internal/health',
-        idamWeb: 'https://idam-web-public.preview.platform.hmcts.net/health',
-        idamApi: 'https://idam-api.preview.platform.hmcts.net/health',
-        s2s: 'https://rpe-service-auth-provider-aat.service.core-compute-aat.internal/health',
-        rdProfessionalApi: 'https://rd-professional-api-preview.service.core-compute-preview.internal/health',
+      ccdDataApi: 'https://ccd-data-store-api-demo.service.core-compute-demo.internal/health',
+      ccdDefApi: 'https://ccd-definition-store-api-demo.service.core-compute-demo.internal/health',
+      idamWeb: 'https://idam-web-public.demo.platform.hmcts.net/health',
+      idamApi: 'https://idam-api.demo.platform.hmcts.net/health',
+      s2s: 'https://rpe-service-auth-provider-demo.service.core-compute-demo.internal/health',
+      rdProfessionalApi: 'https://rd-professional-api-demo.service.core-compute-demo.internal/health',
+      draftStoreApi: 'https://draft-store-service-demo.service.core-compute-demo.internal/health',
     },
-    sessionSecret: 'secretSauce',
     useProxy: false,
+    secureCookie: false,
+    sessionSecret: 'secretSauce',
+    logging: 'debug',
     jurisdictions: [
       {id: 'SSCS'},
       {id: 'AUTOTEST1'},
@@ -31,4 +33,4 @@ export default {
       {id: 'EMPLOYMENT'},
       {id: 'CMC'}
     ],
-}
+  }

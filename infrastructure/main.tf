8141bce67d63c2972d6cac15ea640975381dc708
Franco Zanki <fazanki@users.noreply.github.com>
2019-08-28 15:14:53 +0100

    Bug/fz configuration fix (#247)
    * Setup aat env eui 673 (#233)
    
    * AAT changes
    
    * small change
    
    * rpe secret values (#234)
    
    * redirect straight to idam after logout (#223)
    
    * redirect straight to idam after logout
    
    * lint fix
    
    * Clean branch test (#240)
    
    * Updating yaml
    
    * Test
    
    * Chart change
    
    * Giving permissions
    
    * permission
    
    * Update change
    
    * Update dockerfile
    
    * Update docker
    
    * changing footer (#242)
    
    * changing footer
    
    * removing commented out code
    
    * changing https_only to true (#239)
    
    * Remove hyperlink for the error control (#241)
    
    * vault changes (#244)
    
    * vault changes
    
    * commented setVault
    
    * jenkins app name
    
    * webapp name for microservicekey
    
    * webapp name for microservicekey
    
    * values.yaml added
    
    * values added back
    
    * yaml values added
    
    * s2s_microservicekey_commented
    
    * enabling prod deployment
    
    * trying configuration fix

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,6 +3,7 @@ locals {
     ase_name = "core-compute-${var.env}"
     local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
     shared_vault_name = "${var.shared_product_name}-${local.local_env}"
+    export PUI_ENV=${var.env}
 }
 
 module "app" {
@@ -15,7 +16,7 @@ module "app" {
     capacity     = "${var.capacity}"
     is_frontend = "${!(var.env == "preview" || var.env == "spreview") ? 1 : 0}"
     additional_host_name = "${var.additional_host_name}"
-    https_only="false"
+    https_only="true"
     common_tags  = "${var.common_tags}"
     asp_rg = "${local.app_full_name}-${var.env}"
     asp_name = "${var.shared_product_name}-${var.env}"

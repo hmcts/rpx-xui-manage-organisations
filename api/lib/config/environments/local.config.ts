80a4f2e46ed38ce15e1fe6d9e6fec50597ab43b4
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2019-08-28 14:45:12 +0100

    typos health check

--- api/lib/config/environments/local.config.ts
+++ api/lib/config/environments/local.config.ts
@@ -10,8 +10,8 @@ export default {
   health: {
     ccdDataApi: 'https://ccd-data-store-api-demo.service.core-compute-demo.internal/health',
     ccdDefApi: 'https://ccd-definition-store-api-demo.service.core-compute-demo.internal/health',
-    idam_web: 'https://hmcts-access.service.gov.uk/health',
-    idam_api: 'https://idam-api.platform.hmcts.net/health',
+    idamWeb: 'https://hmcts-access.service.gov.uk/health',
+    idamApi: 'https://idam-api.platform.hmcts.net/health',
     s2s: 'https://rpe-service-auth-provider-prod.service.core-compute-prod.internal/health',
     rdProfessionalApi: 'https://rd-professional-api-prod.service.core-compute-prod.internal/health',
   },

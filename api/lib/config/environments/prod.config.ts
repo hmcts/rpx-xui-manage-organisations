e7ebb43f152f8d5cde4d98eac48396dc9077ae9f
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2019-08-29 13:17:14 +0100

    rpa config fix

--- api/lib/config/environments/prod.config.ts
+++ api/lib/config/environments/prod.config.ts
@@ -5,7 +5,7 @@ export default {
         idamWeb: 'https://hmcts-access.service.gov.uk',
         idamApi: 'https://idam-api.platform.hmcts.net',
         s2s: 'https://rpe-service-auth-provider-prod.service.core-compute-prod.internal',
-        rdProfessionalApi: 'https://rpa-rd-professional-prod.service.core-compute-prod.internal',
+        rdProfessionalApi: 'https://rd-professional-prod.service.core-compute-prod.internal',
     },
     health: {
         ccdDataApi: 'https://ccd-data-store-api-prod.service.core-compute-prod.internal/health',
@@ -13,7 +13,7 @@ export default {
         idamWeb: 'https://hmcts-access.service.gov.uk/health',
         idamApi: 'https://idam-api.platform.hmcts.net/health',
         s2s: 'https://rpe-service-auth-provider-prod.service.core-compute-prod.internal/health',
-        rdProfessionalApi: 'https://rpa-rd-professional-prod.service.core-compute-prod.internal/health',
+        rdProfessionalApi: 'https://rd-professional-prod.service.core-compute-prod.internal/health',
     },
     useProxy: false,
     secureCookie: true,

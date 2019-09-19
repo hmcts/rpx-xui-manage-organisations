ca5b99a61fe2a307cac4f5064eb9cb9e0228ab63
Elviani <elviani@users.noreply.github.com>
2019-09-19 17:15:28 +0100

    Feature/eui 537 accessibility aa master (#268)
    * EUI-537 Added accessibility and refactored
    
    * EUI-537 fix for anchor
    
    * updating the accessibility content

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -3,13 +3,28 @@ import { HmctsGlobalFooterComponent } from './hmcts-global-footer/hmcts-global-f
 import {
   HmctsPrimaryNavigationComponent
 } from '../../../projects/gov-ui/src/lib/components/hmcts-primary-navigation/hmcts-primary-navigation.component';
+import { CookiePolicyComponent } from './cookie-policy/cookie-policy.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
+import { AccessibilityComponent } from './accessibility/accessibility.component';
+import { ServiceDownComponent } from './service-down/service-down.component';
 
 export const components: any[] = [
   HmctsGlobalHeaderComponent,
   HmctsGlobalFooterComponent,
   HmctsPrimaryNavigationComponent,
+  CookiePolicyComponent,
+  PrivacyPolicyComponent,
+  TermsAndConditionsComponent,
+  AccessibilityComponent,
+  ServiceDownComponent
 ];
 
 export * from './hmcts-global-header/hmcts-global-header.component';
 export * from './hmcts-global-footer/hmcts-global-footer.component';
 export * from '../../../projects/gov-ui/src/lib/components/hmcts-primary-navigation/hmcts-primary-navigation.component';
+export * from './cookie-policy/cookie-policy.component';
+export * from './privacy-policy/privacy-policy.component';
+export * from './terms-and-conditions/terms-and-conditions.component';
+export * from './accessibility/accessibility.component';
+export * from './service-down/service-down.component';

2691840a162d4ec6e1aa5609fea6889e547dead7
Adnan Akgun <adnan.akgun@netclipper.co.uk>
2019-10-01 17:53:23 +0100

    EUI-628 some more on the store logic remains

--- src/users/containers/user-details/user-details.component.ts
+++ src/users/containers/user-details/user-details.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import * as fromStore from '../../store';
-import {Observable} from 'rxjs';
+import * as fromRoot from '../../../app/store';
+import { Observable, Subscription, combineLatest } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
-import {map} from 'rxjs/internal/operators';
+import { map } from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-prd-user-details-component',
@@ -12,20 +13,43 @@ import {map} from 'rxjs/internal/operators';
 })
 export class UserDetailsComponent implements OnInit {
 
-  userData$: Observable<any>; // TODO add type
+  user$: Observable<any>;
+  user: any;
+
+  userSubscription: Subscription;
+  dependancySubscription: Subscription;
 
   constructor(
-    private activeRoute: ActivatedRoute,
-    private store: Store<fromStore.UserState>
+    private userStore: Store<fromStore.UserState>,
+    private routerStore: Store<fromRoot.State>,
   ) { }
 
   ngOnInit(): void {
-    this.activeRoute.parent.params.pipe(
-      map(payload => {
-        this.userData$ = this.store.pipe(select(fromStore.getGetSingleUser, { userIdentifier: payload.userId }));
-      })
-    ).subscribe();
 
+    this.dependancySubscription = combineLatest([
+      this.routerStore.pipe(select(fromRoot.getRouterState)),
+      this.userStore.pipe(select(fromStore.getGetUserList))
+    ]).subscribe(([route, users]) => {
+      if (users.length === 0) {
+        this.userStore.dispatch(new fromStore.LoadUsers());
+      }
+      const userId = route.state.params.userId;
+      this.user$ = this.userStore.pipe(select(fromStore.getGetSingleUser, { userIdentifier: userId }));
+    });
+
+    this.userSubscription = this.user$.subscribe((user) => this.user = user);
+
+  }
+
+  ngOnDestroy() {
+
+    if (this.dependancySubscription) {
+      this.dependancySubscription.unsubscribe();
+    }
+
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
   }
 
 }

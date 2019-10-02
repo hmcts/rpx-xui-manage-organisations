07aca12d9e5bc1c59847672ba2d0e70cbaa786be
Pardeep Singh Basi <paddybasi@gmail.com>
2019-10-02 10:42:21 +0100

    added ability to retrieve bearer token

--- test/integration/tests/utils/generateAPI.ts
+++ test/integration/tests/utils/generateAPI.ts
@@ -1,72 +1,44 @@
 import { generateToken } from '../../../../api/auth/serviceToken';
 import { getOauth2Token } from './getToken';
-import { getTokenFromCode } from '../../../../api/auth/index';
-// import * as http from 'http';
-import { http } from '../../../../api/lib/http';
+import * as http from 'axios';
 
-
-// const should = require('should');
-// const http = require('axios');
-// const getCookie = require('./getToken');
 
 // const mainURL = process.env.TEST_URL || 'https://localhost:3000';
 const mainURL = 'https://rd-professional-api-aat.service.core-compute-aat.internal'
 const LOG_REQUEST_ERROR_DETAILS = false;
 
-
-// const s2sToken = generateToken()
-
 export async function generateAPIRequest(method, subURL, params) {
 
-    // await getCookie.getOauth2Token().then(function (token) {
-    //     cookie = token;
-    // });
-
   let s2sToken;
-  let token;
-
-  // try {
-  //   s2sToken = await generateToken();
-  //   token = await getOauth2Token();
-  // } catch (error) {
-  //   console.log(error);
-  // }
-  // return s2sToken;
+  let authToken;
 
   try {
-    token = await getOauth2Token();
+    s2sToken = await generateToken();
+    authToken = await getOauth2Token();
+
+    const options = {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        ServiceAuthorization: s2sToken,
+        'Content-Type': 'application/json'
+      },
+      json: true,
+      resolveWithFullResponse: true
+    };
+
+    if (params.body) {
+      options.body = params.body;
+    }
+
+    console.log('OPTIONS: ', method, mainURL + subURL, options);
+
+    response = await http(method, mainURL + subURL, options);
+
+    return response;
+
   } catch (error) {
     console.log(error);
   }
-  return token;
-// console.log('cookie Value :' + cookie)
-    /*const options = {
-        headers: {
-            // Cookie: '__auth__=' + cookie,
-            Authorization: `Bearer ${authToken}`,
-            ServiceAuthorization: s2sToken,
-            'Content-Type': 'application/json'
-        },
-        json: true,
-        resolveWithFullResponse: true
-    };
-    console.log(options);*/
-//     if (params.body) options.body = params.body;
-//
-//     let response
-//
-//   console.log(mainURL + subURL)
-//
-//     try {
-//         response = await http(method, mainURL + subURL, options);
-//     } catch (e) {
-//         if (LOG_REQUEST_ERROR_DETAILS) {
-//             console.log(Error.response.body, 'ERROR !');
-//         }
-//     }
-//     if (LOG_REQUEST_ERROR_DETAILS) {
-//         requestPromise.catch(error => console.log(error.response.body, 'ERROR !'));
-//     }
-//     return response;
+
  }
 
--- test/integration/tests/utils/getToken.ts
+++ test/integration/tests/utils/getToken.ts
@@ -1,90 +1,30 @@
 // import {config} from "../../../../api/lib/config";
-import axios, { AxiosResponse } from 'axios'
 const base64 = require('base-64')
-const FormData = require('form-data')
 const fetch = require('node-fetch');
 const url = 'https://idam-api-idam-aat.service.core-compute-idam-aat.internal/oauth2/authorize'
-const idam_api= 'https://idam-web-public.aat.platform.hmcts.net'
+const idamApi = 'https://idam-web-public.aat.platform.hmcts.net'
 const idamSecret = process.env.IDAM_SECRET || 'AAAAAAAAAAAAAAAA'
 const baseUrl = 'https://xui-mo-webapp-aat.service.core-compute-aat.internal'
 const idamClient = 'xuimowebapp'
-// const code = ''
-
-async function generateClientCode() {
-  // const redirectUri = baseUrl + '/oauth2/callback'
-  const redirectUri = 'https://xui-mo-webapp-aat.service.core-compute-aat.internal/oauth2/callback'
-
-  // Build formData object.
-  const formData = new FormData();
-  formData.append('response_type', 'code');
-  formData.append('client_id', idamClient);
-  formData.append('redirect_uri', redirectUri);
-  formData.append('scope', 'profile openid roles manage-user create-user manage-roles');
-
-
-  // let encode = base64.encode((process.env.TEST_EMAIL + ':' + process.env.TEST_PASSWORD))
-  const encode = base64.encode(('lukexuisuperuser@mailnesia.com'+ ':' + 'Monday01'))
-
-  console.log(encode)
-  console.log(url)
-
-  try {
-    return await fetch(url, {
-      method: 'POST',
-      headers: {
-        authorization: 'Basic ' + encode,
-        'Content-Type': 'application/x-www-form-urlencoded'
-
-      },
-       // body: JSON.stringify({formData})
-      body: JSON.stringify({
-        response_type: 'code',
-        client_id: 'xuimowebapp',
-        redirect_uri: 'https://xui-mo-webapp-aat.service.core-compute-aat.internal/oauth2/callback',
-        scope: 'profile openid roles manage-user create-user manage-roles',
-      })
-    });
-  } catch (error) {
-    console.log('Request failure: ', error);
-  }
-
-  // await fetch(url, otherParam).then(data => data.json())
-  //   .then(res => {
-  //     code = res.code
-  //     return res.code
-  //   })
-  //   .catch(error => {
-  //     console.log(error)
-  //   })
-  // return code
-}
 
 export async function getOauth2Token() {
-    const hcode = await generateClientCode()
-    console.log(hcode);
-  //   let data =''
-  //   // const url = 'http://localhost:4501/oauth2/token?code=' + hcode + '&client_id=' + idamClient + '&redirect_uri=' + redirectUri + '&client_secret=' + idamSecret+ '&grant_type=authorization_code'
-  //   const url = idam_api+'/oauth2/authorize?grant_type=authorization_code&code=' + hcode + '&redirect_uri=' + redirectUri
-  // //const url = idam_api+'/oauth2/token?code=' + hcode + '&client_id=' + idamClient + '&redirect_uri=' + redirectUri
-  //   const otherParam = {
-  //       headers: {
-  //           'Content-Type': 'application/x-www-form-urlencoded',
-  //       },
-  //       body: data,
-  //       method: 'POST'
-  //   }
-  //
-  //   await fetch(url, otherParam).then(data => data.json())
-  //       .then(res => {
-  //           token = res.access_token
-  //           console.log(res.access_token)
-  //           return res.code
-  //       })
-  //       .catch(error => {
-  //       })
-  //
-  //   return token;
+  const redirectUri = `${baseUrl}/oauth2/callback`
+  const urlPost = `${url}?response_type=code&client_id=${idamClient}&redirect_uri=${redirectUri}`;
+
+  // let encode = base64.encode((process.env.TEST_EMAIL + ':' + process.env.TEST_PASSWORD))
+  const encode = base64.encode(('lukexuisuperuser@mailnesia.com:Monday01'));
+
+  const otherParam = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: 'Basic ' + encode
+
+    },
+    body: '',
+    method: 'POST'
+  };
+
+  const response = await fetch(urlPost, otherParam);
+  const code = await response.json();
+  return code.code;
 }
-
-module.exports.getOauth2Token = getOauth2Token
-

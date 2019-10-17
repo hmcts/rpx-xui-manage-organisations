793c629c55774f8d377d45049f469586b16c5232
Uday Denduluri <uday.denduluri@gmail.com>
2019-10-17 15:38:33 +0100

    Adding User Permissions

--- src/users/components/index.ts
+++ src/users/components/index.ts
@@ -1,5 +1,7 @@
 import {InviteUserFormComponent} from './invite-user-form/invite-user-form.component';
+import { InviteUserPermissionComponent } from './invite-user-permissions/invite-user-permission.component';
 
-export const components: any[] = [InviteUserFormComponent];
+export const components: any[] = [InviteUserFormComponent, InviteUserPermissionComponent];
 
 export * from './invite-user-form/invite-user-form.component';
+export * from './invite-user-permissions/invite-user-permission.component';
---
+++ src/users/components/invite-user-permissions/invite-user-permission.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+    selector: 'app-invite-permission-form',
+    templateUrl: './invite-user-permission.component.html',
+  })
+
+  export class InviteUserPermissionComponent {
+    @Input() inviteUserForm: FormGroup;
+    // @Input() isInvalid;
+  }

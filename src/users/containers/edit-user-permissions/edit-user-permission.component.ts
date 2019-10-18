200d6dab445d587cdcc5c600978a928de79bcf15
Uday Denduluri <uday.denduluri@gmail.com>
2019-10-18 14:11:45 +0100

    Adding edit user permission and link with router

---
+++ src/users/containers/edit-user-permissions/edit-user-permission.component.ts
@@ -0,0 +1,9 @@
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+
+@Component({
+    selector: 'edit-user-permission',
+    templateUrl: './edit-user-permission.component.html',
+  })
+  export class EditUserPermissionComponent {
+
+  }

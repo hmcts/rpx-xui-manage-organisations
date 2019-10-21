aeae61a5733d0a125ec2933d8140120e9a9aae19
Uday Denduluri <uday.denduluri@gmail.com>
2019-10-21 09:48:38 +0100

    Edit user permissions

--- src/users/containers/edit-user-permissions/edit-user-permission.component.ts
+++ src/users/containers/edit-user-permissions/edit-user-permission.component.ts
@@ -1,9 +1,21 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { checkboxesBeCheckedValidator } from 'src/custom-validators/checkboxes-be-checked.validator';
 
 @Component({
     selector: 'edit-user-permission',
     templateUrl: './edit-user-permission.component.html',
   })
-  export class EditUserPermissionComponent {
-
+  export class EditUserPermissionComponent  implements OnInit {
+    inviteUserForm: FormGroup;
+    isInvalid;
+    ngOnInit(): void {
+      this.inviteUserForm = new FormGroup({
+        roles: new FormGroup({
+          'pui-case-manager': new FormControl(''),
+          'pui-user-manager': new FormControl(''),
+          'pui-organisation-manager': new FormControl('')
+        }, checkboxesBeCheckedValidator())
+      });
+    }
   }

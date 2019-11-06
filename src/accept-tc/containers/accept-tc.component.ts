5b54865ef0c5c5491958cd118624f14423dab855
fazanki <fazanki@gmail.com>
2019-11-06 13:45:23 +0000

    adding html and some accept acctions

--- src/accept-tc/containers/accept-tc.component.ts
+++ src/accept-tc/containers/accept-tc.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import {Store} from '@ngrx/store';
+import * as fromStore from '../store';
 
 @Component({
-  selector: 'app-terms-and-conditions',
+  selector: 'app-acceptterms-and-conditions',
   templateUrl: './accept-tc.component.html'
 })
 export class AcceptTcComponent {
-  constructor() {
+  constructor(private store: Store<fromStore.TCState>) {
+  }
+
+  onAcceptTandC() {
+    this.store.dispatch(new fromStore.AcceptTandC({}));
   }
 }

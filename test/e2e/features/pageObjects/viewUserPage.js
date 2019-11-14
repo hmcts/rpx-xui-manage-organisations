258b9d7b606d0f59db538122ccc5c15b0027258e
kommurunagendra <kommurunagendra@gmail.com>
2019-11-14 15:21:34 +0000

    Mo automations (#145)
    * MO Automation test's
    
    * automation tests
    
    * automation tests
    
    * automation test MO and RO
    
    * automation tests
    
    * Mo automation
    
    * Automation changes
    
    * added the url
    
    * Automation test Register journey
    
    * Register journey Automation tests
    
    * refactor tests
    
    * added main tests

---
+++ test/e2e/features/pageObjects/viewUserPage.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { AMAZING_DELAY, SHORT_DELAY, MID_DELAY, LONG_DELAY } = require('../../support/constants');
+
+class ViewUserPage {
+
+  constructor() {
+    this.header = 'h1';
+    this.inviteUser = element(by.css("body main a"));
+  }
+
+  async getPageHeader() {
+    return await $(this.header).getText();
+  }
+
+  async amOnPage() {
+    let header = await this.getPageHeader();
+    return header === "Users";
+  }
+
+  async clickInviteUser() {
+    await this.inviteUser.click();
+    browser.sleep(AMAZING_DELAY);
+  }
+}
+module.exports = ViewUserPage;
+

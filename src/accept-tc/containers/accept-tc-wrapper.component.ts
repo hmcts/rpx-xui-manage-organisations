ee4f58f4f38594f2688b49576d6ac56b0e8c5bdd
Uday Denduluri <uday.denduluri@gmail.com>
2019-11-18 16:57:23 +0000

    Allow user to proceed after User accept Terms and Conditions

--- src/accept-tc/containers/accept-tc-wrapper.component.ts
+++ src/accept-tc/containers/accept-tc-wrapper.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {select, Store} from '@ngrx/store';
-import * as fromStore from '../store';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {select, Store, Action} from '@ngrx/store';
+import * as fromStore from '../../../src/user-profile/store';
+import * as fromRoot from '../../app/store';
 import * as fromUserProfile from '../../user-profile/store/';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
+import { Actions, ofType } from '@ngrx/effects';
 /**
  * Terms And Condition smart component wrapper
  * absorbs Terms and Condition dumb component
@@ -11,13 +13,36 @@ import {Observable} from 'rxjs';
   selector: 'app-accept-terms-conditions-wrapper',
   templateUrl: './accept-tc-wrapper.component.html'
 })
-export class AcceptTcWrapperComponent implements OnInit {
+export class AcceptTcWrapperComponent implements OnInit, OnDestroy {
   uId: Observable<string>;
-  constructor(private store: Store<fromStore.TCState>) {
+  subscription: Subscription;
+  constructor(private store: Store<fromStore.AuthState>,
+              private actions$: Actions) {
   }
 
   ngOnInit(): void {
     this.uId = this.store.pipe(select(fromUserProfile.getUid));
+    this.subscription = this.getObservable(this.actions$, fromStore.AuthActionTypes.ACCEPT_T_AND_C_SUCCESS).subscribe(() => {
+      this.dispatchAction(this.store, new fromRoot.Go({ path: ['home'] }));
+    });
+  }
+
+  getObservable(actions$: Actions, action: string): Observable<never> {
+    return actions$.pipe(ofType(action));
+  }
+
+  dispatchAction(store: Store<fromStore.AuthState>, action: Action) {
+    store.dispatch(action);
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe(this.subscription);
+  }
+
+  unsubscribe(subscription: Subscription) {
+    if (subscription) {
+      subscription.unsubscribe();
+    }
   }
 
   onAcceptTandC(uid) {
--- src/user-profile/store/actions/user-profile.actions.ts
+++ src/user-profile/store/actions/user-profile.actions.ts
@@ -10,7 +10,10 @@ export enum AuthActionTypes {
   GET_USER_DETAILS_FAIL = '[User] Get User Details Fail',
   LOAD_HAS_ACCEPTED_TC = '[T&C] Load Has Accepted',
   LOAD_HAS_ACCEPTED_TC_SUCCESS = '[T&C] Load Has Accepted Success',
-  LOAD_HAS_ACCEPTED_TC_FAIL = '[T&C] Load Has Accepted Fail'
+  LOAD_HAS_ACCEPTED_TC_FAIL = '[T&C] Load Has Accepted Fail',
+  ACCEPT_T_AND_C = '[T&C] Accept T&C',
+  ACCEPT_T_AND_C_SUCCESS = '[T&C] Accept T&C Success',
+  ACCEPT_T_AND_C_FAIL = '[T&C] Accept T&C Fail'
 }
 
 export class LogIn implements Action {
@@ -52,6 +55,21 @@ export class LoadHasAcceptedTCFail implements Action {
   constructor(public payload: HttpErrorResponse) {}
 }
 
+export class AcceptTandC implements Action {
+  readonly type = AuthActionTypes.ACCEPT_T_AND_C;
+  constructor(public payload: any) {}
+}
+
+export class AcceptTandCSuccess implements Action {
+  readonly type = AuthActionTypes.ACCEPT_T_AND_C_SUCCESS;
+  constructor(public payload: boolean) {}
+}
+
+export class AcceptTandCFail implements Action {
+  readonly type = AuthActionTypes.ACCEPT_T_AND_C_FAIL;
+  constructor(public payload: any) {}
+}
+
 export type UserProfileActions =
   | LogIn
   | LogInFailure
@@ -60,4 +78,7 @@ export type UserProfileActions =
   | GetUserDetailsFailure
   | LoadHasAcceptedTC
   | LoadHasAcceptedTCSuccess
-  | LoadHasAcceptedTCFail;
+  | LoadHasAcceptedTCFail
+  | AcceptTandC
+  | AcceptTandCSuccess
+  | AcceptTandCFail;

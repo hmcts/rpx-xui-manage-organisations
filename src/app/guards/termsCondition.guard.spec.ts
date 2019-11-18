5ba579879ae34932f999b5bf0e3393d8a32161aa
Uday Denduluri <uday.denduluri@gmail.com>
2019-11-18 13:57:15 +0000

    making T&C testable

--- src/app/guards/termsCondition.guard.spec.ts
+++ src/app/guards/termsCondition.guard.spec.ts
@@ -1,12 +1,10 @@
 import { TermsConditionGuard } from './termsCondition.guard';
-import {LoadHasAcceptedTC, LoadHasAcceptedTCSuccess} from '../../user-profile/store/actions';
-import {cold, hot} from 'jasmine-marbles';
+import { of } from 'rxjs';
 
 describe('Accept Tc Component', () => {
   let guard: TermsConditionGuard;
   let mockStore: any;
   let mockService: any;
-  // let actions$;
 
   beforeEach(() => {
     mockStore = jasmine.createSpyObj('mockStore', ['unsubscribe', 'dispatch', 'pipe']);
@@ -18,15 +16,22 @@ describe('Accept Tc Component', () => {
     expect(guard).toBeTruthy();
   });
 
-  it('can checkStore', () => {
-    guard.checkStore();
+  it('can dispatchLoadHasAcceptedTC', () => {
+    guard.dispatchLoadHasAcceptedTC('userId', mockStore);
+    expect(mockStore.dispatch).toHaveBeenCalled();
+  });
+
+  it('can loadTandCIfNotLoaded', () => {
+    const tAndC = { hasUserAccepted: 'false', loaded: false };
+    const observable = of({ hasUserAccepted: 'false', loaded: false });
+    mockStore.pipe.and.returnValue(observable);
+    guard.loadTandCIfNotLoaded(tAndC, mockStore);
     expect(mockStore.pipe).toHaveBeenCalled();
   });
 
-  it('can activate', () => {
-    // const completion = guard.canActivate()
-    // actions$ = hot('-a', { a: action });
-    // const expected = cold('-b', { b: completion });
-    // expect(expected).toBeObservable()
+  it('can dispatchGoIfUserHasNotAccepted', () => {
+    const tAndC = { hasUserAccepted: 'false', loaded: true };
+    guard.dispatchGoIfUserHasNotAccepted(tAndC, mockStore, 'tAndC');
+    expect(mockStore.dispatch).toHaveBeenCalled();
   });
 });

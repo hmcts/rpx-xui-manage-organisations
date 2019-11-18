16028e0d2f9ff298ce1f932c9339b206f8ba82a6
Vamshi-Muniganti <vamshi.muniganti@gmail.com>
2019-11-18 12:27:01 +0000

    Mo automations (#314)
    * MO Automation test's
    
    * automation tests
    
    * automation tests
    
    * automation test MO and RO
    
    * automation tests
    
    * Mo automation
    
    * Automation changes
    
    * added the url
    
    * Automation test Register journey
    
    * Register journey Automation tests
    
    * refactor tests
    
    * added main tests
    
    * added sleep and reduced set of tests

--- test/e2e/features/step_definitions/viewOrganisation.steps.js
+++ test/e2e/features/step_definitions/viewOrganisation.steps.js
@@ -16,8 +16,8 @@ defineSupportCode(function ({And, But, Given, Then, When}) {
   });
 
   Then(/^I should be on display the name and address details of organisation$/, async function () {
+    browser.sleep(LONG_DELAY);
     expect(await viewOrganisationPage.amOnPage()).to.be.true;
-
   });
 
 });

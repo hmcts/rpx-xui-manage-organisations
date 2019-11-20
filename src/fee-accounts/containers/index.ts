82fec7598cdaaeea58c236f9d45d041514decba6
Uday Denduluri <uday.denduluri@gmail.com>
2019-11-20 12:15:24 +0000

    new account wrapper component that navigates the overview component

--- src/fee-accounts/containers/index.ts
+++ src/fee-accounts/containers/index.ts
@@ -1,13 +1,16 @@
 import { OrganisationAccountsComponent } from './overview/account-overview.component';
 import { TransactionsComponent } from '../components/transactions/transactions.component';
 import { SummaryComponent } from '../components/summary/summary.component';
+import { OrganisationAccountsOverviewContainerComponent } from './overview-container/account-overview-container.component';
 
 export const containers: any[] = [
   OrganisationAccountsComponent,
+  OrganisationAccountsOverviewContainerComponent,
   TransactionsComponent,
   SummaryComponent
 ];
 
 export * from './overview/account-overview.component';
+export * from './overview-container/account-overview-container.component';
 export * from '../components/transactions/transactions.component';
 export * from '../components/summary/summary.component';

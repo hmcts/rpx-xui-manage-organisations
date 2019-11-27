117ac7426c4f0b066b207c3cecac9d6be3b4bda3
Ronald Mansveld <ronald.mansveld@atos.net>
2019-11-27 11:59:01 +0000

    Add and update tests

--- src/app/containers/terms-and-conditions/terms-and-conditions.component.ts
+++ src/app/containers/terms-and-conditions/terms-and-conditions.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TCDocument } from '@hmcts/rpx-xui-common-lib';
 import { select, Store } from '@ngrx/store';
 import * as fromRoot from '../../store';
 import { LoadTermsConditions } from '../../store';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-terms-and-conditions',
     templateUrl: './terms-and-conditions.component.html'
 })
-export class TermsAndConditionsComponent implements OnInit {
+export class TermsAndConditionsComponent implements OnInit, OnDestroy {
 
     public document: TCDocument = null;
+
+    private subscriptions: Subscription[] = [];
 
     constructor(private readonly store: Store<fromRoot.State>) {
     }
 
     public ngOnInit() {
-        this.store.pipe(
+        const s = this.store.pipe(
             select(fromRoot.getTermsAndConditions)
         ).subscribe(doc => {
             if (doc) {
@@ -25,5 +28,10 @@ export class TermsAndConditionsComponent implements OnInit {
                 this.store.dispatch(new LoadTermsConditions());
             }
         });
+        this.subscriptions.push(s);
+    }
+
+    public ngOnDestroy() {
+        this.subscriptions.forEach(s => s.unsubscribe());
     }
 }

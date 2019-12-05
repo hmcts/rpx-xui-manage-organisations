803d1b23ad9f0f3df8af24c663a22d024f2a2742
fazanki <fazanki@gmail.com>
2019-12-05 15:59:54 +0000

    adding basic modal

---
+++ projects/gov-ui/src/lib/components/hmcts-dialog/hmcts-dialog-component.ts
@@ -0,0 +1,16 @@
+import {Component, Input} from '@angular/core';
+
+/*
+* Gov UK Dialog Component
+* Responsible for displaying dialog layout
+* using ng-content to display content
+* */
+@Component({
+  selector: 'lib-dialog',
+  templateUrl: './hmcts-dialog.component.html',
+  styleUrls: ['./hmcts-dialog.component.scss']
+})
+export class HmctsDialogComponent {
+  constructor () { }
+
+}

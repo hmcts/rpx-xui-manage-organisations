e1f5bbfd65e43847dde31ade54296ec1c8fe4728
ernestman28 <ernest_man28@hotmail.com>
2019-12-06 13:42:24 +0000

    update ithc config

--- api/lib/config/environments/ithc.config.ts
+++ api/lib/config/environments/ithc.config.ts
@@ -1,16 +1,16 @@
 export default {
 
   services: {
-    ccdDataApi: 'http://ccd-data-store-api-demo.service.core-compute-demo.internal',
-    ccdDefApi: 'http://ccd-definition-store-api-demo.service.core-compute-demo.internal',
+    ccdDataApi: 'http://ccd-data-store-api-ithc.service.core-compute-ithc.internal',
+    ccdDefApi: 'http://ccd-definition-store-api-ithc.service.core-compute-ithc.internal',
     idamWeb: 'https://idam-web-public.ithc.platform.hmcts.net',
     idamApi: 'https://idam-api.ithc.platform.hmcts.net',
     s2s: 'http://rpe-service-auth-provider-ithc.service.core-compute-ithc.internal',
     rdProfessionalApi: 'http://rd-professional-api-ithc.service.core-compute-ithc.internal',
   },
   health: {
-    ccdDataApi: 'http://ccd-data-store-api-demo.service.core-compute-demo.internal/health',
-    ccdDefApi: 'http://ccd-definition-store-api-demo.service.core-compute-demo.internal/health',
+    ccdDataApi: 'http://ccd-data-store-api-ithc.service.core-compute-ithc.internal/health',
+    ccdDefApi: 'http://ccd-definition-store-api-ithc.service.core-compute-ithc.internal/health',
     idamWeb: 'https://idam-web-public.ithc.platform.hmcts.net/health',
     idamApi: 'https://idam-api.ithc.platform.hmcts.net/health',
     s2s: 'http://rpe-service-auth-provider-ithc.service.core-compute-ithc.internal/health',

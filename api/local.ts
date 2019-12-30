9521fefe96d85cda5e85f661851fc34d2869df71
Philip <philip.aberneithy@gmail.com>
2019-12-30 13:01:55 +0000

    Pulling the code across from AO and getting working.

--- api/local.ts
+++ api/local.ts
@@ -2,12 +2,25 @@
  * Common to both server.ts and local.ts files
  */
 
+import * as propertiesVolume from '@hmcts/properties-volume'
 import * as bodyParser from 'body-parser'
 import * as cookieParser from 'cookie-parser'
 import * as express from 'express'
 import * as session from 'express-session'
 import * as sessionFileStore from 'session-file-store'
 import * as auth from './auth'
+import {environmentCheckText, getConfigValue, getEnvironment} from './configuration'
+import {ERROR_NODE_CONFIG_ENV} from './configuration/constants'
+import {
+  COOKIE_TOKEN,
+  COOKIES_USERID,
+  IDAM_CLIENT,
+  MAX_LINES, NOW, SECURE_COOKIE,
+  SERVICES_CCD_DATA_API_PATH,
+  SERVICES_CCD_DEF_API_PATH,
+  SERVICES_IDAM_API_PATH,
+  SESSION_SECRET,
+} from './configuration/references'
 import {appInsights} from './lib/appInsights'
 import {config} from './lib/config'
 import {errorStack} from './lib/errorStack'
@@ -23,6 +36,37 @@ import * as log4js from 'log4js'
 const FileStore = sessionFileStore(session)
 
 const app = express()
+
+/**
+ * Allows us to integrate the Azure key-vault flex volume, so that we are able to access Node configuration values.
+ */
+propertiesVolume.addTo(config)
+
+/**
+ * If there are no configuration properties found we highlight this to the person attempting to initialise
+ * this application.
+ */
+if (!getEnvironment()) {
+  console.log(ERROR_NODE_CONFIG_ENV)
+}
+
+
+/**
+ * TODO: Implement a logger on the Node layer.
+ */
+console.log(environmentCheckText())
+
+// TODO: Testing that we can get the environment variables on AAT from the .yaml file
+console.log('COOKIE_TOKEN')
+console.log(process.env.NODE_CONFIG_ENV)
+console.log(getConfigValue(COOKIE_TOKEN))
+console.log(getConfigValue(COOKIES_USERID))
+console.log(getConfigValue(MAX_LINES))
+console.log(getConfigValue(SERVICES_CCD_DATA_API_PATH))
+console.log(getConfigValue(SERVICES_CCD_DEF_API_PATH))
+console.log(getConfigValue(SERVICES_IDAM_API_PATH))
+console.log(getConfigValue(SESSION_SECRET))
+console.log(getConfigValue(IDAM_CLIENT))
 
 app.use(
   session({

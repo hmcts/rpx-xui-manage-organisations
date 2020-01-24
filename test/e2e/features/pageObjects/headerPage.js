20b0e5478cb4451cbc69aac7af42feb8c65d6004
sreekanthpuligadda <52752138+sreekanthpuligadda@users.noreply.github.com>
2020-01-24 09:21:36 +0000

    Tests/eui 1002 test coverage, fix to handle spinner (#356)
    * Refactor and fix existing functional tests
    
    * New browser to handle user registration email on mailinator domain
    
    * Functional tests invite user and validate after successful registration
    
    * End to end scenarios added
    
    * common run config to separet file
    
    * Fixed config import/require
    
    * minor fix to use chai-promised
    
    * Fixed minor issues. added jenkins nightly file for nightly functional test build.
    
    * Fixed step definition mapping issue.
    
    * Include invite suer scenario to functional test group
    
    * config update
    
    * added retry page load on blank or service error.
    
    * Applied fixes to make tests robust
    
    * locator fix
    
    * Added scenario to validate MC login for invited user
    
    * Fixes to improve robustness of tests.
    
    * merge conflict fixes
    
    * Added retry actions to handle scenario robustness
    
    * Ret login added to get to login page
    
    * fix missing async keyword for function
    
    * Fixes to screenshot util and Invite user retries
    
    * Fixed world param reference in step definition
    
    * Replaced page refresh with get url to overcome service error page.
    
    * Fixed by adding  async function
    
    * Disabled flakey tests on pipeline
    
    * Fix to wait for spinner to disappear
    
    * Handing spinner to improve test stability.
    
    * fix to use screenshot util in  step def.

--- test/e2e/features/pageObjects/headerPage.js
+++ test/e2e/features/pageObjects/headerPage.js
@@ -43,6 +43,7 @@ class HeaderPage {
   }
 
   async clickUser(){
+    await this.waitForSpinnerNotPresent();
     await BrowserWaits.waitForElementNotVisible(this.spinner);
     await BrowserWaits.waitForElement(this.user);
     await BrowserWaits.waitForElementClickable(this.user);
@@ -51,11 +52,17 @@ class HeaderPage {
   }
 
   async clickOrganisation() {
+    await this.waitForSpinnerNotPresent();
     await BrowserWaits.waitForElementNotVisible(this.spinner);
     await BrowserWaits.waitForElement(this.organisation);
     await BrowserWaits.waitForElementClickable(this.organisation);
     await this.organisation.click();
     // browser.sleep(AMAZING_DELAY);
+  }
+
+  async waitForSpinnerNotPresent(){
+    await BrowserWaits.waitForElementNotVisible(element(by.css('div.spinner-wrapper')), 60000);
+
   }
 
 

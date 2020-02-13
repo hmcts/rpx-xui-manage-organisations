3b1bde7bc2a7c9f3dde1b11d4c48b0ed4569c6fb
Uday Denduluri <uday.denduluri@gmail.com>
2020-02-13 15:48:40 +0000

    docker changes

--- api/lib/tunnel.ts
+++ api/lib/tunnel.ts
@@ -5,7 +5,7 @@ import * as log4js from 'log4js'
 import { getConfigValue } from '../configuration'
 import {ENVIRONMENT, LOGGING, PROXY_HOST, PROXY_PORT} from '../configuration/references'
 
-export const globalProxyAgent = createGlobalProxyAgent()
+export const globalProxyAgent = createGlobalProxyAgent({})
 
 const logger = log4js.getLogger('tunnel')
 logger.level = getConfigValue(LOGGING)
@@ -14,15 +14,12 @@ export function init(): void {
   const proxyHost = getConfigValue(PROXY_HOST)
   const environment = getConfigValue(ENVIRONMENT)
   const proxyPort = getConfigValue(PROXY_PORT)
+  logger.info('configuring global-agent: ', proxyHost)
   if (proxyHost && environment === 'DEFAULT ENVIRONMENT') {
     logger.info('initialising tunnel, host: ', PROXY_HOST)
     logger.info('initialising tunnel, port: ', PROXY_PORT)
-    globalProxyAgent.HTTP_PROXY = `http://${proxyHost}:${proxyPort}`
-    globalProxyAgent.NO_PROXY = 'localhost'
+    process.env.GLOBAL_AGENT_HTTP_PROXY = `http://${proxyHost}:${proxyPort}`
+    process.env.GLOBAL_AGENT_NO_PROXY = 'localhost'
+    createGlobalProxyAgent({})
   }
-}
-
-export function end(): void {
-  globalProxyAgent.HTTP_PROXY = ''
-  globalProxyAgent.NO_PROXY = ''
 }

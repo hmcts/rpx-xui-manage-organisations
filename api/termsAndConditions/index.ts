9dfb4056f24c621ed7cbd07ffbe25bfe23af9742
Uday Denduluri <uday.denduluri@gmail.com>
2020-02-18 16:01:10 +0000

    Making the config of T&C work with new way

--- api/termsAndConditions/index.ts
+++ api/termsAndConditions/index.ts
@@ -1,5 +1,6 @@
 import * as express from 'express'
-import { config } from '../lib/config'
+import { getConfigValue } from '../configuration'
+import { SERVICES_TERMS_AND_CONDITIONS_API_PATH } from '../configuration/references'
 import { application } from '../lib/config/application.config'
 import { http } from '../lib/http'
 import { getTermsAndConditionsUrl } from './termsAndConditionsUtil'
@@ -7,7 +8,7 @@ import { getTermsAndConditionsUrl } from './termsAndConditionsUtil'
 async function getTermsAndConditions(req: express.Request, res: express.Response) {
     let errReport: any
     try {
-        const url = getTermsAndConditionsUrl(config.services.termsAndConditions, application.idamClient)
+        const url = getTermsAndConditionsUrl(getConfigValue(SERVICES_TERMS_AND_CONDITIONS_API_PATH), application.idamClient)
         const response = await http.get(url)
         res.send(response.data)
     } catch (error) {

52c1fbf11352710e8106132d8ffce3311ea0e210
Philip a <philip.aberneithy@gmail.com>
2020-03-04 12:15:17 +0000

    Printing out health, removed items that are not currently in production

--- api/local.ts
+++ api/local.ts
@@ -156,7 +156,38 @@ app.use(cookieParser())
 app.get('/oauth2/callback', auth.oauth)
 app.get('/external/ping', (req, res) => {
   console.log('Pong')
-  res.send('Pong')
+  res.send({
+    allowConfigMutations: process.env.ALLOW_CONFIG_MUTATIONS,
+    nodeConfigEnv: process.env.NODE_CONFIG_ENV,
+    // 1st set
+    // tslint:disable-next-line:object-literal-sort-keys
+    idamClient: getConfigValue(IDAM_CLIENT),
+    maxLogLine: getConfigValue(MAX_LOG_LINE),
+    microService: getConfigValue(MICROSERVICE),
+    maxLines: getConfigValue(MAX_LINES),
+    now: getConfigValue(NOW),
+    // 2nd set
+    cookieToken: getConfigValue(COOKIE_TOKEN),
+    cookieUserId: getConfigValue(COOKIES_USERID),
+    oauthCallBack: getConfigValue(OAUTH_CALLBACK_URL),
+    protocol: getConfigValue(PROTOCOL),
+    // 3rd set
+    idamApiPath: getConfigValue(SERVICES_IDAM_API_PATH),
+    idamWeb: getConfigValue(SERVICES_IDAM_WEB),
+    s2sPath: getConfigValue(SERVICE_S2S_PATH),
+    rdProfessionalApi: getConfigValue(SERVICES_RD_PROFESSIONAL_API_PATH),
+    feeAndPayApi: getConfigValue(SERVICES_FEE_AND_PAY_API_PATH),
+    termsAndConditionsApi: getConfigValue(SERVICES_TERMS_AND_CONDITIONS_API_PATH),
+    // 4th set
+    sessionSecret: getConfigValue(SESSION_SECRET),
+    jurisdictions: getConfigValue(JURISDICTIONS),
+    appInsightKey: getConfigValue(APP_INSIGHTS_KEY),
+    // 5th set
+    featureSecureCookieEnabled: showFeature(FEATURE_SECURE_COOKIE_ENABLED),
+    featureAppInsightEnabled: showFeature(FEATURE_APP_INSIGHTS_ENABLED),
+    featureProxyEnabled: showFeature(FEATURE_PROXY_ENABLED),
+    featureTermsAndConditionsEnabled: showFeature(FEATURE_TERMS_AND_CONDITIONS_ENABLED),
+  })
 })
 app.use('/external', openRoutes)
 app.get('/health', (req, res) => {

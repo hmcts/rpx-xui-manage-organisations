0a12950aa5328665c56f04cc1584d6b59875cfc7
Philip a <philip.aberneithy@gmail.com>
2020-03-04 15:10:26 +0000

    Functional tests.

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -109,14 +109,6 @@ variable "allow_config_mutations" {
   default = "1"
 }
 
-variable "ao_http_proxy" {
-  default = "http://172.16.0.7:8080"
-}
-
-variable "ao_no_proxy" {
-  default = "localhost"
-}
-
 variable "app_insights_enabled" {
   default = "1"
 }

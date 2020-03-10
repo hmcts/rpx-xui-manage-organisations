d99b96a5c95bc7da2afd4a13e28bea3c5947a617
Philip a <philip.aberneithy@gmail.com>
2020-03-10 15:10:57 +0000

    UI retrieves Idam Web configuration setting from the node layer.

---
+++ api/configurationUI/index.ts
@@ -0,0 +1,21 @@
+import * as express from 'express'
+import {getConfigValue} from '../configuration'
+import {
+  SERVICES_IDAM_WEB,
+} from '../configuration/references'
+
+export const router = express.Router({mergeParams: true})
+
+router.get('/', configurationUIRoute)
+
+/**
+ * All the following environmental variables are passed to the UI.
+ */
+async function configurationUIRoute(req, res) {
+  console.log('sending configuration')
+  res.status(200).send({
+    idamWeb: getConfigValue(SERVICES_IDAM_WEB),
+  })
+}
+
+export default router

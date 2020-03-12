2398dc7ac2ae0c523fbce4d06a4a64c03b606796
Elviani Huseyin <elviani_s@hotmail.com>
2020-03-12 17:17:51 +0000

    adding unit test

--- src/app/components/terms-and-conditions/terms-and-conditions.component.ts
+++ src/app/components/terms-and-conditions/terms-and-conditions.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TCDocument } from '@hmcts/rpx-xui-common-lib';
 import { select, Store } from '@ngrx/store';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { TermsConditionsService } from 'src/shared/services/termsConditions.service';
 import * as fromRoot from '../../store';
 
@@ -10,29 +10,32 @@ import * as fromRoot from '../../store';
     templateUrl: './terms-and-conditions.component.html'
 })
 export class TermsAndConditionsComponent implements OnInit, OnDestroy {
-
     public document: TCDocument = null;
 
     private readonly subscriptions: Subscription[] = [];
 
-    public isTandCEnabled$: Observable<boolean>;
+    public isTandCEnabled: boolean = false;
 
     constructor(private readonly store: Store<fromRoot.State>,
                 private readonly termsAndConditionsService: TermsConditionsService) {
     }
 
     public ngOnInit() {
-      this.isTandCEnabled$ = this.termsAndConditionsService.isTermsConditionsFeatureEnabled();
-      const s = this.store.pipe(
-          select(fromRoot.getTermsAndConditions)
-      ).subscribe(doc => {
-          if (doc) {
-              this.document = doc;
-          } else {
-              this.store.dispatch(new fromRoot.LoadTermsConditions());
+      this.termsAndConditionsService.isTermsConditionsFeatureEnabled().subscribe(enabled => {
+        if (enabled) {
+          this.isTandCEnabled = true;
+          const s = this.store.pipe(
+            select(fromRoot.getTermsAndConditions)
+        ).subscribe(doc => {
+            if (doc) {
+                this.document = doc;
+            } else {
+                this.store.dispatch(new fromRoot.LoadTermsConditions());
+            }
+          });
+          this.subscriptions.push(s);
           }
       });
-      this.subscriptions.push(s);
     }
 
     public ngOnDestroy() {

402570c655ef5be54dd2a187862e090e6b10b9d2
Pardeep Singh Basi <paddybasi@gmail.com>
2020-03-27 17:37:13 +0000

    incorrect tags being picked up in functional tests

--- test/e2e/config/common.conf.js
+++ test/e2e/config/common.conf.js
@@ -15,7 +15,7 @@ const localConfig = [
         browserName: 'chrome',
         acceptInsecureCerts: true,
         unexpectedAlertBehaviour: 'accept',
-        chromeOptions: { args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-zygote ', '--disable-notifications'] },
+        chromeOptions: { args: ['--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-zygote ', '--disable-notifications'] },
         proxy: {
             proxyType: 'manual',
             httpProxy: 'proxyout.reform.hmcts.net:8080',
@@ -32,7 +32,7 @@ const config = {
         password: process.env.TEST_PASSWORD || 'Monday01'
     },
     twoFactorAuthEnabled: false
-    
+
 };
 
 const cucumberOpts = [

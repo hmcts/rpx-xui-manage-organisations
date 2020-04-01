d1cdd81bc707ae143ff065a62ec18f0ed4fa69f8
Ronald Mansveld <ronald.mansveld@atos.net>
2020-04-01 15:07:29 +0100

    Load config from new endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { ENVIRONMENT_CONFIG } from './models/environmentConfig.model';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+fetch('/api/configuration-ui/').then(async response => {
+  let config = await response.json();
+  config = config || {};
+  platformBrowserDynamic([{ provide: ENVIRONMENT_CONFIG, useValue: config }])
+    .bootstrapModule(AppModule)
+    .catch(err => console.log(err));
+});

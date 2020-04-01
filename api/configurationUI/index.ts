62b892c43a6915cabb67d2807a8c1901b7ba7435
Ronald Mansveld <ronald.mansveld@atos.net>
2020-04-01 15:53:32 +0100

    Add launch darkly client id to api config

--- api/configurationUI/index.ts
+++ api/configurationUI/index.ts
@@ -1,10 +1,6 @@
 import * as express from 'express'
-import {getConfigValue} from '../configuration'
-import {
-  PROTOCOL,
-  GOOGLE_ANALYTICS_KEY,
-  SERVICES_IDAM_WEB,
-} from '../configuration/references'
+import { getConfigValue } from '../configuration'
+import { GOOGLE_ANALYTICS_KEY, LAUNCH_DARKLY_CLIENT_ID, PROTOCOL, SERVICES_IDAM_WEB } from '../configuration/references'
 
 export const router = express.Router({mergeParams: true})
 
@@ -15,9 +11,10 @@ router.get('/', configurationUIRoute)
  */
 async function configurationUIRoute(req, res) {
   res.status(200).send({
+    googleAnalyticsKey: getConfigValue(GOOGLE_ANALYTICS_KEY),
     idamWeb: getConfigValue(SERVICES_IDAM_WEB),
+    launchDarklyClientId: getConfigValue(LAUNCH_DARKLY_CLIENT_ID),
     protocol: getConfigValue(PROTOCOL),
-    googleAnalyticsKey: getConfigValue(GOOGLE_ANALYTICS_KEY)
   })
 }
 

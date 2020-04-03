91a9c1bc64fa07d39a64c3e673f3632ecc811bca
Uday Denduluri <uday.denduluri@gmail.com>
2020-04-03 17:24:28 +0100

    re-invite unhappy path

--- src/app/components/service-down/service-down.component.ts
+++ src/app/components/service-down/service-down.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { GlobalError } from 'src/app/store/reducers/app.reducer';
+import * as fromAppStore from '../../../app/store';
 
 @Component({
     selector: 'app-service-down',
     templateUrl: './service-down.component.html'
 })
-export class ServiceDownComponent {
-    constructor() {
+export class ServiceDownComponent implements OnInit, OnDestroy {
+    public currentError: GlobalError;
+    constructor(private readonly store: Store<fromAppStore.State>) {
+    }
+    public ngOnDestroy(): void {
+        this.store.dispatch(new fromAppStore.ClearGlobalError());
+    }
+    public ngOnInit(): void {
+        this.currentError = {
+            errors: [{bodyText: 'Try again later.', urlText: null, url: null}],
+            header: 'Sorry, there is a problem with the service'
+        };
+        this.store.pipe(select(fromAppStore.getCurrentError))
+        .subscribe(error => {
+            if (error) {
+                this.currentError = error;
+            }
+        });
     }
 }

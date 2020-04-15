b24b50e86f03d424d68f46f31d92e968d009de44
Uday Denduluri <uday.denduluri@gmail.com>
2020-04-15 14:51:43 +0100

    fix spell check

--- api/inviteUser/inviteUserUtil.spec.ts
+++ api/inviteUser/inviteUserUtil.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import { getInviteUserUrl } from './inviteUserUtil'
 
-describe('terms And Conditions ', () => {
+describe('inviteuser util', () => {
     it('should getInviteUserUrl', () => {
         let url = getInviteUserUrl('http://base')
         expect(url).to.equal('http://base/refdata/external/v1/organisations/users/')

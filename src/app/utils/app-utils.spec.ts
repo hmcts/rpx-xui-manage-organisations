182fca005af8e78e74d1ae99ee3d25e623a8c362
Philip <philip.aberneithy@gmail.com>
2020-07-15 09:36:45 +0100

    Cherry Pick EUI-1690

--- src/app/utils/app-utils.spec.ts
+++ src/app/utils/app-utils.spec.ts
@@ -2,6 +2,7 @@
 import {AppConstants} from '../app.constants';
 import { AppFeatureFlag } from '../store/reducers/app.reducer';
 import {AppUtils} from './app-utils';
+import { propsExist } from '../../../api/lib/objectUtilities';
 const router = {
   state: {
     url: 'register'
@@ -216,4 +217,35 @@ describe('AppUtils', () => {
     const dateString = AppUtils.formatDateAtTime(dateTime, true);
     expect(dateString).toEqual('01 Jan 2020 at 21:12');
   });
+
+  describe('propsExist()', () => {
+
+    it('Should return true if all the properties exist on an object.', () => {
+
+      const object = {level1: {level2: {level3: 'level3'}}}
+
+      expect(propsExist(object, ['level1', 'level2', 'level3'])).toEqual(true);
+    })
+
+    it('Should return false if a property does not exist on an object.', () => {
+
+      const object = {level1: {level2: {level3: 'level3'}}}
+
+      expect(propsExist(object, ['level1', 'breakingProperty', 'level3'])).toEqual(false);
+    })
+
+    it('Should return false if the object is undefined.', () => {
+
+      const object = undefined
+
+      expect(propsExist(object, ['level1', 'level2', 'level3'])).toEqual(false);
+    })
+
+    it('Should return false if the object is null.', () => {
+
+      const object = null
+
+      expect(propsExist(object, ['level1', 'level2', 'level3'])).toEqual(false);
+    })
+  })
 });

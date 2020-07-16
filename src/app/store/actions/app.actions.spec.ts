df1f8676ed8418b9bf629fd313e25ff4efb43663
Uday Denduluri <uday.denduluri@gmail.com>
2020-07-16 12:24:22 +0100

    adding unit tests for app.actions

--- src/app/store/actions/app.actions.spec.ts
+++ src/app/store/actions/app.actions.spec.ts
@@ -39,4 +39,55 @@ describe('App actions', () => {
       });
     });
   });
+
+  describe('LoadFeatureToggleConfig', () => {
+    it('should create an action', () => {
+      const action = new fromAppActions.LoadFeatureToggleConfig([{featureName: 'feature1', isEnabled: true}]);
+      const payload = [{featureName: 'feature1', isEnabled: true}];
+      expect({ ...action }).toEqual({
+        type: fromAppActions.LOAD_FEATURE_TOGGLE_CONFIG,
+        payload
+      });
+    });
+  });
+
+  describe('LoadFeatureToggleConfigSuccess', () => {
+    it('should create an action', () => {
+      const action = new fromAppActions.LoadFeatureToggleConfigSuccess([{featureName: 'feature1', isEnabled: true}]);
+      const payload = [{featureName: 'feature1', isEnabled: true}];
+      expect({ ...action }).toEqual({
+        type: fromAppActions.LOAD_FEATURE_TOGGLE_CONFIG_SUCCESS,
+        payload
+      });
+    });
+  });
+
+  describe('LoadFeatureToggleConfigFail', () => {
+    it('should create an action', () => {
+      const action = new fromAppActions.LoadFeatureToggleConfigFail({errorMessage: 'some error'});
+      const payload = {errorMessage: 'some error'};
+      expect({ ...action }).toEqual({
+        type: fromAppActions.LOAD_FEATURE_TOGGLE_CONFIG_FAIL,
+        payload
+      });
+    });
+  });
+
+  describe('StartAppInitilizer', () => {
+    it('should create an action', () => {
+      const action = new fromAppActions.StartAppInitilizer();
+      expect({ ...action }).toEqual({
+        type: fromAppActions.START_APP_INITIALIZER
+      });
+    });
+  });
+
+  describe('FinishAppInitilizer', () => {
+    it('should create an action', () => {
+      const action = new fromAppActions.FinishAppInitilizer();
+      expect({ ...action }).toEqual({
+        type: fromAppActions.FINISH_APP_INITIALIZER
+      });
+    });
+  });
 });

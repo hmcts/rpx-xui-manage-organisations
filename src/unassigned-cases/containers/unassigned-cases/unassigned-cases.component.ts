46bd16f1586877b2019ef14b566855fa557e1667
Lam <daniel.2.lam@atos.net>
2020-07-21 17:10:04 +0100

    EUI-2115: Update to use simplified Case List component

--- src/unassigned-cases/containers/unassigned-cases/unassigned-cases.component.ts
+++ src/unassigned-cases/containers/unassigned-cases/unassigned-cases.component.ts
@@ -1,83 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { SearchResultView } from '@hmcts/ccd-case-ui-toolkit';
 import { select, Store } from '@ngrx/store';
 import * as fromStore from '../../store';
+
 @Component({
     selector: 'app-unassigned-cases-component',
     templateUrl: './unassigned-cases.component.html',
   })
-
 export class UnassignedCasesComponent implements OnInit {
 
   private readonly resultsArr: any[] = new Array();
-  private resultView: SearchResultView;
+  private caseListItems: any;
+  private columnNames: string[];
 
   constructor(private readonly store: Store<fromStore.UnassignedCasesState>) {}
   public ngOnInit(): void {
     this.store.dispatch(new fromStore.LoadUnassignedCases());
+    this.columnNames = [
+      'Case created date',
+      'Case due date',
+      'Case reference',
+      'Pet. First name',
+      'Pet. Last name',
+      'Resp. First name',
+      'Resp. Last name',
+      'Solicitor reference'
+    ];
     this.store.pipe(select(fromStore.getUnassignedCasesState)).subscribe((data) => {
       console.log(data);
+      this.caseListItems = data;
     });
-
-    // Do a fake population of resultsArr and resultView just to get some data showing
-    this.resultsArr.push({
-      case_id: 'abc123',
-      case_fields: {
-          TextField1: '',
-          TextField2: '4748-2828-7712-8233',
-          TextField3: '16 Jun 2020'
-      }
-    });
-    this.resultView = {
-      hasDrafts: () => false,
-      columns: [{
-        label: 'Solicitor reference',
-        order: 1,
-        case_field_id: 'TextField1',
-        case_field_type: {
-          id: 'Text',
-          type: 'Text',
-          min: null,
-          max: null,
-          regular_expression: null,
-          fixed_list_items: [],
-          complex_fields: [],
-          collection_field_type: null
-        },
-      },
-      {
-        label: 'Case reference',
-        order: 2,
-        case_field_id: 'TextField2',
-        case_field_type: {
-          id: 'Text',
-          type: 'Text',
-          min: null,
-          max: null,
-          regular_expression: null,
-          fixed_list_items: [],
-          complex_fields: [],
-          collection_field_type: null
-        }
-      },
-      {
-        label: 'Case created date',
-        order: 3,
-        case_field_id: 'TextField3',
-        case_field_type: {
-          id: 'Text',
-          type: 'Text',
-          min: null,
-          max: null,
-          regular_expression: null,
-          fixed_list_items: [],
-          complex_fields: [],
-          collection_field_type: null
-        }
-      }],
-      results: this.resultsArr,
-      result_error: null
-    };
   }
-
 }

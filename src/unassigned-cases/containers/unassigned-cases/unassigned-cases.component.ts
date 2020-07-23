001e99b94b368558707c4f51e4e20ef7e0cef818
Lam <daniel.2.lam@atos.net>
2020-07-23 05:31:01 +0100

    Update to use modified Case List component

--- src/unassigned-cases/containers/unassigned-cases/unassigned-cases.component.ts
+++ src/unassigned-cases/containers/unassigned-cases/unassigned-cases.component.ts
@@ -8,27 +8,26 @@ import * as fromStore from '../../store';
   })
 export class UnassignedCasesComponent implements OnInit {
 
-  private caseListItems: any[];
-  private columnNames: string[];
+  private rows: any[];
+  private columnConfig: any[];
 
   constructor(private readonly store: Store<fromStore.UnassignedCasesState>) {}
   public ngOnInit(): void {
     this.store.dispatch(new fromStore.LoadUnassignedCases());
     // Static column names for now
-    this.columnNames = [
-      'Case created date',
-      'Case due date',
-      'Case reference',
-      'Pet. First name',
-      'Pet. Last name',
-      'Resp. First name',
-      'Resp. Last name',
-      'Solicitor reference'
+    this.columnConfig = [
+      { header: 'Case created date', key: 'caseCreatedDate', type: 'date' },
+      { header: 'Case due date', key: 'caseDueDate', type: 'date' },
+      { header: 'Case reference', key: 'caseRef' },
+      { header: 'Pet. First name', key: 'petFirstName' },
+      { header: 'Pet. Last name', key: 'petLastName' },
+      { header: 'Resp. First name', key: 'respFirstName' },
+      { header: 'Resp. Last name', key: 'respLastName' },
+      { header: 'Solicitor reference', key: 'sRef' }
     ];
     this.store.pipe(select(fromStore.getUnassignedCasesState)).subscribe((data) => {
       console.log(data);
-      // Problem is this is asynchronous and will be populated too late for Case List component
-      this.caseListItems = data as unknown as any[];
+      this.rows = data.unassignedCases;
     });
   }
 }

aabae798ee7b08cea1d4a25a763afac7ed11ff35
Uday Denduluri <uday.denduluri@gmail.com>
2020-07-23 15:01:23 +0100

    unassigned Case Service unit tests

---
+++ src/unassigned-cases/services/unassinged-cases.service.spec.ts
@@ -0,0 +1,12 @@
+import { of } from 'rxjs';
+import {UnassignedCasesService} from './unassigned-cases.service';
+
+describe('UnassignedCasesService', () => {
+    it('fetchUnassignedCases', () => {
+        const mockHttp = jasmine.createSpyObj('http', ['get']);
+        mockHttp.get.and.returnValue(of({}));
+        const service = new UnassignedCasesService(mockHttp);
+        service.fetchUnassignedCases();
+        expect(mockHttp.get).toHaveBeenCalledWith(UnassignedCasesService.url);
+    });
+});

0a644016be76e0e89296963c0057b34239dad936
Uday Denduluri <uday.denduluri@gmail.com>
2020-08-27 13:21:48 +0100

    integrating with real caseTypes

--- api/unassignedCaseTypes/index.ts
+++ api/unassignedCaseTypes/index.ts
@@ -1,17 +1,27 @@
 import { Request, Response, Router } from 'express'
 import { getConfigValue } from '../configuration'
 import { SERVICES_CCD_DATA_STORE_API_PATH } from '../configuration/references'
-import { createCaseTypeResponse, getApiPath } from './unassignedCases.util'
+import * as log4jui from '../lib/log4jui'
+import { getApiPath } from './unassignedCases.util'
 
 export async function handleUnassignedCaseTypes(req: Request, res: Response) {
+    const logger = log4jui.getLogger('caseTypes')
     console.log(req.body)
     const payload = req.body
 
     const rdProfessionalApiPath = getApiPath(getConfigValue(SERVICES_CCD_DATA_STORE_API_PATH))
-    //const response = await req.http.post(rdProfessionalApiPath, payload)
-    const response = createCaseTypeResponse()
-    // console.log(response.data)
-    res.send(response)
+    try {
+        const response = await req.http.post(rdProfessionalApiPath, payload)
+        res.send(response.data)
+    } catch (error) {
+        logger.info(error)
+        const errReport = JSON.stringify({
+            apiError: error,
+            apiStatusCode: error.statusCode ? error.statusCode : 500,
+            message: error.message ? error.message : '',
+        })
+        res.status(500).send(errReport)
+    }
 }
 
 export const router = Router({ mergeParams: true })

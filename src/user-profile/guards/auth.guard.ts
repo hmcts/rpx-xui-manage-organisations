8f8f6a46894036cc3b35a68d42588b0d8edfe911
Uday Denduluri <uday.denduluri@gmail.com>
2020-09-08 14:29:22 +0100

    Make both calls in an effective way

--- src/user-profile/guards/auth.guard.ts
+++ src/user-profile/guards/auth.guard.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { select, Store } from '@ngrx/store';
+import { combineLatest, Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
+import { filter, map, take, tap } from 'rxjs/operators';
 import {AuthService} from '../services/auth.service';
 import * as fromStore from '../store';
 
@@ -14,15 +15,32 @@ export class AuthGuard implements CanActivate {
   ) {
   }
 
-  public canActivate(): Observable<boolean> {
-    return this.authService.isAuthenticated().map( isAuth => {
+  public canActivate() {
+    return combineLatest([this.checkUserStore(), this.isAuthenticated()]).pipe(
+      map(([isUserLoadedInStore, isAuthenticated]) => {
+        return isUserLoadedInStore && isAuthenticated;
+      }),
+    );
+  }
+
+  public isAuthenticated(): Observable<boolean> {
+    return this.authService.isAuthenticated().pipe(map(isAuth => {
       if (!isAuth) {
         this.authService.loginRedirect();
         return false;
       }
-      this.store.dispatch(new fromStore.GetUserDetails());
-      return true;
-    });
+    }));
   }
 
+  public checkUserStore(): Observable<boolean> {
+    return this.store.pipe(select(fromStore.userLoaded),
+      tap(loaded => {
+        if (!loaded) {
+          this.store.dispatch(new fromStore.GetUserDetails());
+        }
+      }),
+      filter(loaded => loaded),
+      take(1)
+    );
+  }
 }

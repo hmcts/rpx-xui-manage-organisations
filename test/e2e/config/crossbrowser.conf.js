74d9d04383ce7db2d7011098a9a322f326768a9e
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2020-10-19 14:43:08 +0100

    EUI2780_CB_SecurityFix

--- test/e2e/config/crossbrowser.conf.js
+++ test/e2e/config/crossbrowser.conf.js
@@ -1,156 +1,84 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
-
 chai.use(chaiAsPromised);
 const minimist = require('minimist');
-
 const argv = minimist(process.argv.slice(2));
 
-
 const config = {
-    framework: 'custom',
-    frameworkPath: require.resolve('protractor-cucumber-framework'),
+  framework: 'custom',
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
 
-   sauceSeleniumAddress: 'ondemand.eu-central-1.saucelabs.com:443/wd/hub',
+  sauceSeleniumAddress: 'ondemand.eu-central-1.saucelabs.com:443/wd/hub',
 
- // sauceSeleniumAddress: 'https://vmuniganti:***REMOVED***@ondemand.eu-central-1.saucelabs.com:443/wd/hub',
+  host: 'ondemand.eu-central-1.saucelabs.com',
+  sauceRegion: 'eu',
+  port: 80,
+  sauceConnect: true,
+  specs: ['../features/**/*.feature'],
 
-   host: 'ondemand.eu-central-1.saucelabs.com',
-   sauceregion: 'eu',
-   port: 80,
-    specs: ['../features/**/*.feature'],
+  baseUrl: (process.env.TEST_URL || 'http://localhost:3000/').replace('https', 'http'),
 
-    baseUrl: (process.env.TEST_URL || 'http://localhost:3000/').replace('https', 'http'),
+  params: {
+    serverUrls: process.env.TEST_URL || 'http://localhost:3000/',
+    targetEnv: argv.env || 'local',
+    //username: process.env.TEST_EMAIL,
+    //password: process.env.TEST_PASSWORD,
+  },
 
-    params: {
-        serverUrls: process.env.TEST_URL || 'http://localhost:3000/',
-        targetEnv: argv.env || 'local',
-        username: process.env.TEST_EMAIL,
-        password: process.env.TEST_PASSWORD,
-        fr_judge_username: process.env.FR_EMAIL,
-        fr_judge_password: process.env.FR_PASSWORD
+
+  sauceProxy: 'http://proxyout.reform.hmcts.net:8080',  // Proxy for the REST API
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  SAUCE_REST_ENDPOINT: 'https://eu-central-1.saucelabs.com/rest/v1/',
+  allScriptsTimeout: 111000,
+
+  useAllAngular2AppRoots: true,
+  multiCapabilities: [
+    {
+      browserName: 'chrome',
+      version: 'latest',
+      platform: 'Windows 10',
+      name: 'chrome-tests',
+      tunnelIdentifier: 'reformtunnel',
+      extendedDebugging: true,
+      sharedTestFiles: false,
+      maxInstances: 1
     },
+  ],
 
-    // sauceProxy: 'https://proxyout.reform.hmcts.net:8080',  // Proxy for the REST API
-  //  sauceUser: process.env.SAUCE_USERNAME,
- //   sauceKey: process.env.SAUCE_ACCESS_KEY,
-   sauceUser: 'vmuniganti',
-   sauceKey: '***REMOVED***',
-    SAUCE_REST_ENDPOINT: 'https://eu-central-1.saucelabs.com/',
-    allScriptsTimeout: 111000,
+  exclude: [],
 
-    useAllAngular2AppRoots: true,
-    multiCapabilities: [
-        {
-            browserName: 'chrome',
-            version: 'latest',
-            platform: 'Windows 10',
-            name: 'chrome-tests',
-            tunnelIdentifier: 'reformtunnel',
-            extendedDebugging: true,
-            sharedTestFiles: false,
-            maxInstances: 1
-
-        },
-
-        // {
-        //     browserName: 'chrome',
-        //     name: 'MAC_CHROME_LATEST',
-        //     platform: 'macOS 10.13',
-        //     version: 'latest',
-        //     'tunnel-identifier': 'reformtunnel'
-        //         // shardTestFiles: true,
-        //         // maxInstances: 1
-        //
-        // },
-
-        //
-        // {
-        //     browserName: 'firefox',
-        //     name: 'WIN_FIREFOX_LATEST',
-        //     platform: 'Windows 10',
-        //     version: 'latest',
-        //     'tunnel-identifier': 'reformtunnel',
-        //     shardTestFiles: false,
-        //     maxInstances: 1
-        //
-        //  }
-        //
-        // {
-        //     browserName: 'firefox',
-        //     name: 'MAC_FIREFOX_LATEST',
-        //     platform: 'macOS 10.13',
-        //     version: 'latest',
-        //     'tunnel-identifier': 'reformtunne',
-        //     shardTestFiles: true,
-        //     maxInstances: 1
-        // }
-        //
-
-        //
-        // {
-        //     'browserName': 'internet explorer',
-        //     'platform': 'Windows 10',
-        //     'version': '11.103',
-        //     'name': 'XUI-MO-IE11-TEST',
-        //     'tunnelIdentifier': 'reformtunnel',
-        //     'extendedDebugging': true,
-        //     'shardTestFiles': true,
-        //     'maxInstances': 1
-        //
-        // }
-        //
+  cucumberOpts: {
+    strict: true,
+    format: ['node_modules/cucumber-pretty', 'json:cb_reports/saucelab_results.json'],
+    require: ['../support/world.js', '../support/*.js', '../features/step_definitions/**/*.steps.js'],
+    tags: ['@crossbrowser']
+  },
 
 
-    ],
+  plugins: [
+    {
+      package: 'protractor-multiple-cucumber-html-reporter-plugin',
+      options: {
+        automaticallyGenerateReport: true,
+        removeExistingJsonReportFile: true,
+        reportName: 'XUI MO CrossBrowser Tests',
+        jsonDir: 'reports/tests/crossbrowser',
+        reportPath: 'reports/tests/crossbrowser'
 
-
-    exclude: [],
-
-    cucumberOpts: {
-        strict: true,
-        format: ['node_modules/cucumber-pretty', 'json:cb_reports/saucelab_results.json' ],
-        require: ['../support/world.js', '../support/*.js', '../features/step_definitions/**/*.steps.js'],
-        tags: ['@crossbrowser']
-    },
-
-    //
-    // onComplete() {
-    //     const printSessionId = function(jobName) {
-    //         browser.getSession()
-    //             .then(session => {
-    //             });
-    //     };
-    //     printSessionId('JUI CB Tests');
-    // },
-
-
-
-    plugins: [
-        {
-            package: 'protractor-multiple-cucumber-html-reporter-plugin',
-            options: {
-                automaticallyGenerateReport: true,
-                removeExistingJsonReportFile: true,
-                reportName: 'XUI CrossBrowser Tests',
-                jsonDir: 'reports/tests/crossbrowser',
-                reportPath: 'reports/tests/crossbrowser'
-
-            }
-        }
-    ],
-    onPrepare() {
-        const caps = browser.getCapabilities();
-        browser.manage()
-            .window()
-            .maximize();
-        browser.waitForAngularEnabled(false);
-        global.expect = chai.expect;
-        global.assert = chai.assert;
-        global.should = chai.should;
+      }
     }
-
-
+  ],
+  onPrepare() {
+    const caps = browser.getCapabilities();
+    browser.manage()
+      .window()
+      .maximize();
+    browser.waitForAngularEnabled(false);
+    global.expect = chai.expect;
+    global.assert = chai.assert;
+    global.should = chai.should;
+  }
 };
 
 

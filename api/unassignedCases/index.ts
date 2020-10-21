b3aa40e495c5b3274208581bc46f1e9704e0d44a
Uday Denduluri <uday.denduluri@gmail.com>
2020-10-21 12:18:06 +0100

    explicitly casted it to string

--- api/unassignedCases/index.ts
+++ api/unassignedCases/index.ts
@@ -4,7 +4,7 @@ import { SERVICES_MCA_PROXY_API_PATH } from '../configuration/references'
 import { getApiPath, getRequestBody, mapCcdCases } from './unassingedCases-util'
 
 export async function handleUnassignedCases(req: Request, res: Response, next: NextFunction) {
-  const caseTypeId = req.query.caseTypeId.toString()
+  const caseTypeId = req.query.caseTypeId as string
   const path = getApiPath(getConfigValue(SERVICES_MCA_PROXY_API_PATH), caseTypeId)
   const payload = getRequestBody(req.session.auth.orgId)
   try {

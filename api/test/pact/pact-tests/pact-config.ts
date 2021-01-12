fdda4e86f72e7b691e81c75c3a70c5bdc6cb27e2
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2021-01-12 17:27:27 +0000

    pact-config.ts not required as it is not used.

--- api/test/pact/pact-tests/pact-config.ts
+++ api/test/pact/pact-tests/pact-config.ts
@@ -1,15 +1,16 @@
-import nodeConfig from 'config';
-
-interface Config {
-  pactRdProfessionalApi:string;
-  pactFeeAndPayApi:string;
-}
-
-const config: Config = {
-  pactRdProfessionalApi: nodeConfig.get<string>('services.rdProfessionalApi'),
-  pactFeeAndPayApi: nodeConfig.get<string>('services.feeAndPayApi'),
-};
-
-// export the config so that it can be used elsewhere.
-export default config;
-
+// import nodeConfig from 'config';
+//
+// interface Config {
+//   pactRdProfessionalApi:string;
+//   pactFeeAndPayApi:string;
+// }
+//
+// const config: Config = {
+//   pactRdProfessionalApi: nodeConfig.get<string>('services.rdProfessionalApi'),
+//   // tslint:disable-next-line:object-literal-sort-keys
+//   pactFeeAndPayApi: nodeConfig.get<string>('services.feeAndPayApi'),
+// };
+//
+// // export the config so that it can be used elsewhere.
+// export default config;
+//

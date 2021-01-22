4472cd08aec6bc7c5b5d71711477f8bc338e9927
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2021-01-22 12:31:42 +0000

    crossbrowser test

--- test/e2e/config/crossbrowser.conf.js
+++ test/e2e/config/crossbrowser.conf.js
@@ -46,28 +46,40 @@ const config = {
     },
 
     {
-      browserName: 'internet explorer',
-      platform: 'Windows 10',
+      browserName: 'firefox',
       version: 'latest',
-      name: 'IE-TEST',
+      platform: 'Windows 10',
+      name: 'firefox-tests',
       tunnelIdentifier: 'reformtunnel',
       extendedDebugging: true,
       sharedTestFiles: false,
       maxInstances: 1
-
     },
 
-    {
-      browserName: 'safari',
-      platform: 'macOS 10.13',
-      version: '11',
-      name: 'Safari-TEST',
-      tunnelIdentifier: 'reformtunnel',
-      extendedDebugging: true,
-      sharedTestFiles: false,
-      maxInstances: 1
+    //
+    // {
+    //   browserName: 'internet explorer',
+    //   platform: 'Windows 10',
+    //   version: 'latest',
+    //   name: 'IE-TEST',
+    //   tunnelIdentifier: 'reformtunnel',
+    //   extendedDebugging: true,
+    //   sharedTestFiles: false,
+    //   maxInstances: 1
+    //
+    // },
 
-    },
+    // {
+    //   browserName: 'safari',
+    //   platform: 'macOS 10.13',
+    //   version: '11',
+    //   name: 'Safari-TEST',
+    //   tunnelIdentifier: 'reformtunnel',
+    //   extendedDebugging: true,
+    //   sharedTestFiles: false,
+    //   maxInstances: 1
+    //
+    // },
 
     {
       browserName: 'MicrosoftEdge',

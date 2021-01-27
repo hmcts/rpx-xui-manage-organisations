b7114f862af25fb4d3c4e741959d821089758522
sreekanthpuligadda <52752138+sreekanthpuligadda@users.noreply.github.com>
2021-01-27 10:02:23 +0000

    Tests/mutation nightly build fixes (#610)
    * mutation test fix to resolve compile error caused customTyping in stryker.
    
    * jenkins nightly fix
    
    * crossbrowser test
    
    Co-authored-by: VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>

--- test/integration/tests/utils/getCookie.ts
+++ test/integration/tests/utils/getCookie.ts
@@ -1,29 +1,65 @@
 import * as puppeteer from 'puppeteer';
+import {Error} from 'tslint/lib/error';
 
 const username = process.env.TEST_API_EMAIL;
 const password = process.env.TEST_API_PASSWORD;
 let xsrfCookie = '';
 let xxsrfCookie = '';
 
+const userNameCSSSelector = '#username';
+const passwordCSSSelector = '#password';
+
+let userCookie = '';
+
 export async function  authenticateAndGetcookies(url)  {
   console.log( 'Getting Cookie details...');
+  if (userCookie !== '')
+  {
+    return userCookie;
+  }
   const browser = await puppeteer.launch(getPuppeteerLaunchOptions(url));
 
   const page = await browser.newPage();
   await page.goto(url);
   console.log( 'Loading...');
 
-  try {
-    await page.waitForSelector('#username', { visible: true });
-
-    await page.type('#username', username);
-    await page.type('#password', password);
-
-    await page.click('.button');
-    await page.waitForSelector('.hmcts-primary-navigation', { visible: true });
-  } catch (error) {
-    await browser.close();
-    throw error;
+  // try {
+  //   await page.waitForSelector('#username', { visible: true });
+  //
+  //   await page.type('#username', username);
+  //   await page.type('#password', password);
+  //
+  //   await page.click('.button');
+  //   await page.waitForSelector('.hmcts-primary-navigation', { visible: true });
+  // } catch (error) {
+  //   await browser.close();
+  //   throw error;
+  // }
+  let loginRetryCounter = 0;
+  let isLoginSuccess = false;
+  while (loginRetryCounter < 3 && !isLoginSuccess) {
+    try {
+      console.log(`login retry attempt : ' + ${loginRetryCounter}`);
+      await page.waitForSelector(userNameCSSSelector, { visible: true, timeout: 20000 });
+      await page.type(userNameCSSSelector, username);
+      await page.type(passwordCSSSelector, password);
+      await page.click('.button');
+      // browser.sleep(10000);
+      await page.waitForSelector('.hmcts-header__navigation', { visible: true, timeout: 30000 });
+      isLoginSuccess = true;
+    } catch (error) {
+      const usernameInput = await page.$eval(userNameCSSSelector , element => element.value);
+      if (usernameInput === '') {
+        loginRetryCounter++;
+        console.log(`Login error :  ${error.message}`);
+      } else {
+        await browser.close();
+        throw error;
+      }
+    }
+  }
+  if (!isLoginSuccess) {
+    throw new Error('Login not successful...');
   }
   const cookies: [] = await page.cookies();
   // let xsrfCookie = '';
@@ -45,6 +81,7 @@ export async function  authenticateAndGetcookies(url)  {
   const finalCookie = `${webappCookie};${authCookie};${xsrfCookie}`;
 
   await browser.close();
+  userCookie = finalCookie;
   return finalCookie;
 }
 

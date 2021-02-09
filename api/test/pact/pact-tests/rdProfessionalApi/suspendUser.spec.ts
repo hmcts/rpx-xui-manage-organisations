7767667a4f079f477fa4ae946bfbea9f554b6bd2
satya chundur <>
2021-02-09 13:02:41 +0000

    enabled pact tests in jenkins and refactored tests

--- api/test/pact/pact-tests/rdProfessionalApi/suspendUser.spec.ts
+++ api/test/pact/pact-tests/rdProfessionalApi/suspendUser.spec.ts
@@ -1,39 +1,16 @@
-import { Pact } from '@pact-foundation/pact';
 import { expect } from 'chai';
-import * as getPort from 'get-port';
-import * as path from 'path';
 import { SuspendUserReponseDto } from '../pactFixtures';
 import { suspendUser } from '../pactUtil';
-const {Matchers} = require('@pact-foundation/pact');
-const {somethingLike, like, eachLike} = Matchers;
+import { PactTestSetup } from '../settings/provider.mock';
+
+const { Matchers } = require('@pact-foundation/pact');
+const { somethingLike, like, eachLike } = Matchers;
+const pactSetUp = new PactTestSetup({ provider: 'referenceData_professionalExternalUsers', port: 8000 });
+
 
 describe("RD Professional API", () => {
-  let mockServerPort: number
-  let provider: Pact
 
-  // Setup the provider
-  before(async() => {
-    mockServerPort = await getPort()
-    provider = new Pact({
-      consumer: "xui_manageOrg",
-      provider: "referenceData_professionalExternalUsers",
-      log: path.resolve(process.cwd(), "api/test/pact/logs", "mockserver-integration.log"),
-      dir: path.resolve(process.cwd(), "api/test/pact/pacts"),
-      logLevel: 'info',
-      port: mockServerPort,
-      spec: 2,
-      pactfileWriteMode: "merge"
-    })
-    return provider.setup()
-  })
-
-  // Write Pact when all tests done
-  after(() => provider.finalize())
-
-  // verify with Pact, and reset expectations
-  afterEach(() => provider.verify())
-
-  describe("Suspend A User", () => {
+  describe("Suspend A User", async () => {
 
     const userId = '123456';
 
@@ -50,23 +27,25 @@ describe("RD Professional API", () => {
     }
 
     const mockResponse = {
-        "roleAdditionResponse": {
-         "idamMessage": somethingLike("Role successfully Updated"),
-         "idamStatusCode": somethingLike("200")
-        }
+      "roleAdditionResponse": {
+        "idamMessage": somethingLike("Role successfully Updated"),
+        "idamStatusCode": somethingLike("200")
+      }
     }
 
     const requestPath = "/refdata/external/v1/organisations/users/123456";
 
-    before(done => {
+    before(async () => {
+      const jwt = 'some-access-token'
+      await pactSetUp.provider.setup()
       const interaction = {
-        state: "Professional User exists for modification with identifier 123456" ,
+        state: "Professional User exists for modification with identifier 123456",
         uponReceiving: "a request to update the roles of that user",
         withRequest: {
           method: "PUT",
           headers: {
-            "Content-Type":  "application/json;charset=utf-8",
-            "Authorization":  "Bearer some-access-token",
+            "Content-Type": "application/json;charset=utf-8",
+            "Authorization": "Bearer some-access-token",
             "ServiceAuthorization": "serviceAuthToken"
           },
           path: requestPath,
@@ -77,31 +56,32 @@ describe("RD Professional API", () => {
             "Content-Type": "application/json",
           },
           status: 200,
-          body:mockResponse
+          body: mockResponse
         },
       }
       // @ts-ignore
-      provider.addInteraction(interaction).then(() => {
-        done()
-      })
+      pactSetUp.provider.addInteraction(interaction)
     })
 
-    it("returns the correct response", done => {
+    it("returns the correct response", async () => {
       // call the pactUtil's method which Calls The Downstream API directly without going through the Service Class.
       const userId = '123456';
-      const taskUrl: string = `${provider.mockService.baseUrl}/refdata/external/v1/organisations/users/`+userId;
+      const taskUrl: string = `${pactSetUp.provider.mockService.baseUrl}/refdata/external/v1/organisations/users/` + userId;
 
-      const resp =  suspendUser(taskUrl,mockRequest as any);
+      const resp = suspendUser(taskUrl, mockRequest as any);
 
       resp.then((response) => {
-        const responseDto: SuspendUserReponseDto  = <SuspendUserReponseDto> response.data
+        const responseDto: SuspendUserReponseDto = <SuspendUserReponseDto>response.data
         assertResponse(responseDto);
-      }).then(done,done)
+      }).then(() => {
+        pactSetUp.provider.verify()
+        pactSetUp.provider.finalize()
+      })
     })
   })
 })
 
-function assertResponse(dto:SuspendUserReponseDto):void{
+function assertResponse(dto: SuspendUserReponseDto): void {
   expect(dto.roleAdditionResponse.idamMessage).to.equal('Role successfully Updated');
   expect(dto.roleAdditionResponse.idamStatusCode).to.equal("200");
 }

a1dc135e6fd92b31a1eb1e099967d91ac4d90a6d
satya chundur <>
2021-02-09 13:02:41 +0000

    enabled pact tests in jenkins and refactored tests

--- api/test/pact/pact-tests/feePayApi/getAccounts.spec.ts
+++ api/test/pact/pact-tests/feePayApi/getAccounts.spec.ts
@@ -1,100 +1,75 @@
-import {Pact} from '@pact-foundation/pact'
-import {expect} from 'chai'
-import * as getPort from 'get-port';
-import * as path from 'path'
-import {FeeAccount} from '../../../../../src/fee-accounts/models/pba-accounts';
-import {getAccountFeeAndPayApi} from '../pactUtil';
+import { expect } from 'chai';
+import { FeeAccount } from '../../../../../src/fee-accounts/models/pba-accounts';
+import { getAccountFeeAndPayApi } from '../pactUtil';
+import { PactTestSetup } from '../settings/provider.mock';
 
-const {Matchers} = require('@pact-foundation/pact');
-const {somethingLike, like, eachLike} = Matchers;
+
+const { Matchers } = require('@pact-foundation/pact');
+const { somethingLike, like, eachLike } = Matchers;
+const pactSetUp = new PactTestSetup({ provider: 'payment_accounts', port: 8000 });
+
 
 const responsePaymentAccountDto =
-    {
-        account_number: somethingLike("PBA1234"),
-        account_name: somethingLike("account name"),
-        credit_limit: like(20000.00),
-        available_balance: like(20000.00),
-        status: somethingLike("Active"),
-        effective_date: somethingLike("2021-01-20T12:56:47.576Z")
-    }
+{
+  account_number: somethingLike("PBA1234"),
+  account_name: somethingLike("account name"),
+  credit_limit: like(20000.00),
+  available_balance: like(20000.00),
+  status: somethingLike("Active"),
+  effective_date: somethingLike("2021-01-20T12:56:47.576Z")
+}
 
 describe("Payment API interaction for get account", () => {
-    let mockServerPort: number
-    let provider: Pact
+  const accountId = 'PBA1234'
 
-    const accountId = 'PBA1234'
+  describe("Get Organisation", () => {
 
-    // Setup the provider
+    const jwt = 'some-access-token'
+    const details = ''; // ATM this is not being used in the Service.
+
     before(async () => {
-        mockServerPort = await getPort()
-        provider = new Pact({
-            consumer: "xui_manageOrg",
-            provider: "payment_accounts",
-            log: path.resolve(process.cwd(), "api/test/pact/logs", "mockserver-integration.log"),
-            dir: path.resolve(process.cwd(), "api/test/pact/pacts"),
-            logLevel: 'info',
-            port: mockServerPort,
-            spec: 2,
-            pactfileWriteMode: "merge"
-        })
-        return provider.setup()
+      await pactSetUp.provider.setup()
+      const interaction = {
+        state: "An account exists with identifier PBA1234",
+        uponReceiving: "A request for that account",
+        withRequest: {
+          method: "GET",
+          path: "/accounts/" + accountId,
+          headers: {
+            "Content-Type": "application/json",
+          }
+        },
+        willRespondWith: {
+          status: 200,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: responsePaymentAccountDto,
+        },
+      }
+      // @ts-ignore
+      pactSetUp.provider.addInteraction(interaction)
     })
 
-    // Write Pact when all tests done
-    after(() => provider.finalize())
+    it("returns the correct response", async () => {
+      // call the pactUtil's method which Calls The Downstream FeeAndPay API directly without going through the Service Class.
 
-    // verify with Pact, and reset expectations
-    afterEach(() => provider.verify())
+      const taskUrl: string = `${pactSetUp.provider.mockService.baseUrl}/accounts/` + accountId;
 
-    describe("Get Organisation", () => {
-
-        const jwt = 'some-access-token'
-        const details = ''; // ATM this is not being used in the Service.
-
-        before(done => {
-            const interaction = {
-                state: "An account exists with identifier PBA1234",
-                uponReceiving: "A request for that account",
-                withRequest: {
-                    method: "GET",
-                    path: "/accounts/" + accountId,
-                    headers: {
-                        "Content-Type": "application/json",
-                    }
-                },
-                willRespondWith: {
-                    status: 200,
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: responsePaymentAccountDto,
-                },
-            }
-            // @ts-ignore
-            provider.addInteraction(interaction).then(() => {
-                done()
-            })
-        })
-
-        it("returns the correct response", done => {
-            // call the pactUtil's method which Calls The Downstream FeeAndPay API directly without going through the Service Class.
-
-            const taskUrl: string = `${provider.mockService.baseUrl}/accounts/` + accountId;
-
-            const resp = getAccountFeeAndPayApi(taskUrl);
-            resp.then((response) => {
-                console.log(' back in the TEST ....  assertion call.....');
-                const responseDto: FeeAccount = <FeeAccount>response.data
-                assertResponse(responseDto);
-            }).then(done, done)
-        })
+      const resp = getAccountFeeAndPayApi(taskUrl);
+      resp.then((response) => {
+        const responseDto: FeeAccount = <FeeAccount>response.data
+        assertResponse(responseDto);
+      }).then(() => {
+        pactSetUp.provider.verify()
+        pactSetUp.provider.finalize()
+      })
     })
+  })
 })
 
 function assertResponse(dto: FeeAccount) {
-    expect(dto.account_name).to.equal('account name');
-    expect(dto.account_number).to.equal('PBA1234');
+  expect(dto.account_name).to.equal('account name');
+  expect(dto.account_number).to.equal('PBA1234');
 
 }
-
-
--- api/test/pact/pact-tests/idamApi/getUserDetails.spec.ts
+++ api/test/pact/pact-tests/idamApi/getUserDetails.spec.ts
@@ -1,26 +1,15 @@
-import {Pact} from '@pact-foundation/pact'
-import {expect} from 'chai'
-import * as getPort from "get-port";
-import * as path from 'path'
+import { expect } from 'chai';
 import { getDetails } from '../pactUtil';
-import {IdamGetDetailsResponseDto} from "./getUserDetails.spec";
-const {Matchers} = require('@pact-foundation/pact');
-const {somethingLike} = Matchers;
+import { PactTestSetup } from '../settings/provider.mock';
+import { IdamGetDetailsResponseDto } from "./getUserDetails.spec";
 
-let mockServerPort: number;
-let provider: Pact;
+const { Matchers } = require('@pact-foundation/pact');
+const { somethingLike } = Matchers;
+const pactSetUp = new PactTestSetup({ provider: 'idamApi_users', port: 8000 });
 
-describe("Idam API user details", async() => {
-    mockServerPort = await getPort()
-    provider = new Pact({
-    port: mockServerPort,
-    log: path.resolve(process.cwd(), "api/test/pact/logs", "mockserver-integration.log"),
-    dir: path.resolve(process.cwd(), "api/test/pact/pacts"),
-    spec: 2,
-    consumer: "xui_manageOrg",
-    provider: "idamApi_users",
-    pactfileWriteMode: "merge",
-  })
+
+
+describe("Idam API user details", async () => {
 
   const RESPONSE_BODY = {
     "id": somethingLike("abc123"),
@@ -28,57 +17,54 @@ describe("Idam API user details", async() => {
     "surname": somethingLike("Bloggs"),
     "email": somethingLike("joe.bloggs@hmcts.net"),
     "active": somethingLike(true),
-    "roles":somethingLike([
-      somethingLike("solicitor"),somethingLike("caseworker")
+    "roles": somethingLike([
+      somethingLike("solicitor"), somethingLike("caseworker")
     ])
   }
-  // Setup the provider
-  before(() => provider.setup())
-  // Write Pact when all tests done
-  after(() => provider.finalize())
-  // verify with Pact, and reset expectations
-  afterEach(() => provider.verify())
+
   describe("get /details", () => {
 
-        const jwt = 'some-access-token'
-        before(done => {
-            const interaction = {
-                state: "a valid user exists",
-                uponReceiving: "a request for that user:",
-                withRequest: {
-                    method: "GET",
-                    path: "/details",
-                    headers: {
-                        Authorization: "Bearer some-access-token"
-                    },
-                },
-                willRespondWith: {
-                    status: 200,
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: RESPONSE_BODY,
-                },
-            }
-            // @ts-ignore
-            provider.addInteraction(interaction).then(() => {
-                done()
-            })
-        })
-        it("returns the correct response", (done) => {
-            const taskUrl = `${provider.mockService.baseUrl}`;
-
-            const response:Promise<any>  = getDetails(taskUrl,jwt);
-
-            response.then((axiosResponse) => {
-                const dto:IdamGetDetailsResponseDto = <IdamGetDetailsResponseDto> axiosResponse;
-                assertResponses(dto);
-            }).then(done,done)
-        })
+    const jwt = 'some-access-token'
+    before(async () => {
+      await pactSetUp.provider.setup()
+      const interaction = {
+        state: "a valid user exists",
+        uponReceiving: "a request for that user:",
+        withRequest: {
+          method: "GET",
+          path: "/details",
+          headers: {
+            Authorization: "Bearer some-access-token"
+          },
+        },
+        willRespondWith: {
+          status: 200,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: RESPONSE_BODY,
+        },
+      }
+      // @ts-ignore
+      pactSetUp.provider.addInteraction(interaction)
     })
+    it("returns the correct response", async () => {
+      const taskUrl = `${pactSetUp.provider.mockService.baseUrl}`;
+
+      const response: Promise<any> = getDetails(taskUrl, jwt);
+
+      response.then((axiosResponse) => {
+        const dto: IdamGetDetailsResponseDto = <IdamGetDetailsResponseDto>axiosResponse;
+        assertResponses(dto);
+      }).then(() => {
+        pactSetUp.provider.verify()
+        pactSetUp.provider.finalize()
+      })
+    })
+  })
 })
 
-function assertResponses(dto:IdamGetDetailsResponseDto){
+function assertResponses(dto: IdamGetDetailsResponseDto) {
   expect(dto.active).to.be.equal(true);
   expect(dto.email).to.be.equal('joe.bloggs@hmcts.net');
   expect(dto.forename).to.be.equal('Joe');
@@ -87,11 +73,11 @@ function assertResponses(dto:IdamGetDetailsResponseDto){
   expect(dto.roles[1]).to.be.equal('caseworker');
 }
 
-export interface IdamGetDetailsResponseDto{
-  id:string,
-  forename:string,
-  surname:string,
-  email:string,
-  active:boolean
-  roles:string[]
+export interface IdamGetDetailsResponseDto {
+  id: string,
+  forename: string,
+  surname: string,
+  email: string,
+  active: boolean
+  roles: string[]
 }

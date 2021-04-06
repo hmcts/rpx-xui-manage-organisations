a910e535be1de53272b626a36e8d00cec7f5921d
unknown <phillip.whitaker@hmcts.net>
2021-04-06 14:08:09 +0100

    added base function for add/remove page

---
+++ src/organisation/containers/update-pba-numbers/update-pba-numbers.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { OrganisationDetails } from '../../../models/organisation.model';
+import * as fromStore from '../../store';
+
+@Component({
+  selector: 'app-prd-update-pba-numbers-component',
+  templateUrl: './update-pba-numbers.component.html',
+})
+export class UpdatePbaNumbersComponent implements OnInit {
+
+  public readonly title = 'Add or remove PBA accounts';
+
+  public organisationPaymentAccount: string[];
+
+  constructor(private orgStore: Store<fromStore.OrganisationState>) {}
+
+  ngOnInit() {
+    this.getOrganisationDetailsFromStore();
+  }
+
+  /**
+* Get Payment Account
+*
+* Get the PBA numbers for the Organisation.
+*
+* @param organisationDetails - See unit test.
+* @return ['PBA3344542','PBA7843342']
+*/
+  public getPaymentAccount(organisationDetails: Partial<OrganisationDetails>): string[] {
+    return (!organisationDetails.hasOwnProperty('paymentAccount') || !organisationDetails.paymentAccount.length) ?
+      null : organisationDetails.paymentAccount;
+  }
+
+ /**
+ * Get Organisation Details from Store.
+ *
+ * Once we have the Organisation Details, we display them on the page.
+ */
+  public getOrganisationDetailsFromStore(): void {
+    this.orgStore.pipe(select(fromStore.getOrganisationSel)).subscribe(organisationDetails => {
+      console.log(organisationDetails);
+
+      this.organisationPaymentAccount = this.getPaymentAccount(organisationDetails);
+    });
+  }
+}

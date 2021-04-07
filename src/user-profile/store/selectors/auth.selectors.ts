cecdff4f9041b8ad852dba31c3d37396a1611390
unknown <phillip.whitaker@hmcts.net>
2021-04-07 16:09:06 +0100

    fixed issue with scenario 3

--- src/user-profile/store/selectors/auth.selectors.ts
+++ src/user-profile/store/selectors/auth.selectors.ts
@@ -33,7 +33,7 @@ export const getIsUserCaaAdmin = createSelector(
 
 export const getIsUserPuiFinanceManager = createSelector(
   getUser,
-  (user) => user.roles.includes('pui-finance-manager')
+  (user) => user.roles.indexOf('pui-finance-manager') !== -1
 );
 
 export const getUid = createSelector(

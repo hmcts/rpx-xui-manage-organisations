8a3b0f546daef0c72aa97b2654d24b48dea47bf9
unknown <phillip.whitaker@hmcts.net>
2021-04-07 10:03:13 +0100

    fixed linting erros

--- src/organisation/containers/update-pba-numbers/update-pba-numbers.component.ts
+++ src/organisation/containers/update-pba-numbers/update-pba-numbers.component.ts
@@ -13,30 +13,30 @@ export class UpdatePbaNumbersComponent implements OnInit {
 
   public organisationPaymentAccount: string[];
 
-  constructor(private orgStore: Store<fromStore.OrganisationState>) {}
+  constructor(private orgStore: Store<fromStore.OrganisationState>) { }
 
   ngOnInit() {
     this.getOrganisationDetailsFromStore();
   }
 
   /**
-  * Get Payment Account
-  *
-  * Get the PBA numbers for the Organisation.
-  *
-  * @param organisationDetails - See unit test.
-  * @return ['PBA3344542','PBA7843342']
-  */
+   * Get Payment Account
+   *
+   * Get the PBA numbers for the Organisation.
+   *
+   * @param organisationDetails - See unit test.
+   * @return ['PBA3344542','PBA7843342']
+   */
   public getPaymentAccount(organisationDetails: Partial<OrganisationDetails>): string[] {
     return (!organisationDetails.hasOwnProperty('paymentAccount') || !organisationDetails.paymentAccount.length) ?
       null : organisationDetails.paymentAccount;
   }
 
   /**
-  * Get Organisation Details from Store.
-  *
-  * Once we have the Organisation Details, we display them on the page.
-  */
+   * Get Organisation Details from Store.
+   *
+   * Once we have the Organisation Details, we display them on the page.
+   */
   private getOrganisationDetailsFromStore(): void {
     this.orgStore.pipe(select(fromStore.getOrganisationSel)).subscribe(organisationDetails => {
       this.organisationPaymentAccount = this.getPaymentAccount(organisationDetails);

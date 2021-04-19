13f6e33237b2525c5634a00d0549d0ce306fe685
unknown <phillip.whitaker@hmcts.net>
2021-04-19 17:10:48 +0100

    resolved formatting and added more tests for PBA input

--- src/organisation/components/pba-number-input/pba-number-input.component.ts
+++ src/organisation/components/pba-number-input/pba-number-input.component.ts
@@ -8,9 +8,9 @@ import { ErrorMessagesModel } from "@hmcts/rpx-xui-common-lib/lib/gov-ui/models"
 })
 export class PbaNumberInputComponent implements OnInit {
 
-    static PBA_GENERIC_ERROR_MESSAGE = ['There is a problem. Enter a PBA number, for example PBA1234567.'];
-    static PBA_UNIQUE_ERROR_MESSAGE = ['You have entered this PBA number more than once.'];
-    static PBA_EXISTING_ERROR_MESSAGE = ['This PBA number is already associated with your organisation.'];
+    static PBA_GENERIC_ERROR_MESSAGE = ['There is a problem. Enter a PBA number, for example PBA1234567'];
+    static PBA_UNIQUE_ERROR_MESSAGE = ['You have entered this PBA number more than once'];
+    static PBA_EXISTING_ERROR_MESSAGE = ['This PBA number is already associated with your organisation'];
 
     @Input() public id: string;
     @Input() public group: FormGroup;

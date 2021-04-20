c11a368af0faad92cf17db303f136b663e800de2
unknown <phillip.whitaker@hmcts.net>
2021-04-20 10:48:12 +0100

    added additional tests

--- src/organisation/components/pba-numbers-form/pba-numbers-form.component.spec.ts
+++ src/organisation/components/pba-numbers-form/pba-numbers-form.component.spec.ts
@@ -37,4 +37,52 @@ fdescribe('PbaNumbersFormComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    describe('onAddNewBtnClicked()', () => {
+        it('should add a new form control to the pba form', () => {
+            const currentFormArrayCount = component.pbaNumbers.length;
+            component.onAddNewBtnClicked();
+
+            const newFormArrayCount = component.pbaNumbers.length;
+            expect(newFormArrayCount).toEqual(currentFormArrayCount + 1);
+        });
+    });
+
+    describe('onRemoveNewPbaNumberClicked()', () => {
+        it('should remove a new form control to the pba form', () => {
+            component.onAddNewBtnClicked();
+
+            const currentFormArrayCount = component.pbaNumbers.length;
+
+            component.onRemoveNewPbaNumberClicked(0);
+
+            const newFormArrayCount = component.pbaNumbers.length;
+            expect(newFormArrayCount).toEqual(currentFormArrayCount - 1);
+        });
+    });
+
+    describe('onRemoveExistingPaymentByAccountNumberClicked()', () => {
+        it('should remove a new form control to the pba form', () => {
+            component.updatePbaNumbers.existingPbaNumbers = ['PBA1234567'];
+
+            component.onRemoveExistingPaymentByAccountNumberClicked('PBA1234567');
+
+            expect(component.updatePbaNumbers.pendingRemovePbaNumbers).toEqual(['PBA1234567']);
+        });
+    });
+
+    describe('generateSummaryErrorMessage()', () => {
+
+        it('should display the existing organisation error when user tries to add an existing PBA', () => {
+            component.updatePbaNumbers.existingPbaNumbers = ['PBA1234567'];
+
+            component.onAddNewBtnClicked();
+            component.pbaNumbers.controls[0].setValue({pbaNumber: 'PBA1234567'});
+
+            expect(component.summaryErrors.isFromValid).toBeFalsy();
+            expect(component.summaryErrors.header).toEqual('There is a problem');
+            expect(component.summaryErrors.items[0].message).toEqual('This PBA number is already associated to your organisation');
+        });
+    });
+    
 });

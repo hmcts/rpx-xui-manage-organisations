5be7ce4abbe6e06aa362251593b0216deab13ce2
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2021-04-26 17:52:49 +0100

    Changes in the texts

--- projects/gov-ui/src/lib/components/gov-uk-label/gov-uk-label.component.ts
+++ projects/gov-ui/src/lib/components/gov-uk-label/gov-uk-label.component.ts
@@ -9,19 +9,21 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'lib-gov-label',
   template: `<h1 *ngIf="config.isPageHeading else noHeading">
-          <label *ngIf="config.label" [class]="config.classes + ' govuk-label'"
-                 [for]="config.id" [innerHTML]="config.label">
+          <label *ngIf="config.label" [class]="config.classes + ' govuk-label'" [for]="config.id">
+            <span *ngIf="config.span" class="govuk-visually-hidden">{{config.span}}</span>
+            {{config.label}}
           </label>
         </h1>
         <ng-template #noHeading>
-          <label *ngIf="config.label" [class]="config.classes + ' govuk-label'"
-                 [for]="config.id" [innerHTML]="config.label">
+          <label *ngIf="config.label" [class]="config.classes + ' govuk-label'" [for]="config.id">
+            <span *ngIf="config.span" class="govuk-visually-hidden">{{config.span}}</span>
+            {{config.label}}
           </label>
         </ng-template>
   `
 })
 export class GovUkLabelComponent {
   constructor () { }
-  @Input() config: { label: string, name: string; id: string, isPageHeading, classes: string };
+  @Input() config: { label: string, name: string; id: string, isPageHeading, classes: string, span: string };
 
 }

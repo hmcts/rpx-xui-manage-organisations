2e5c03d56367a7b2f0c812108a73815ec24ca6e7
VAMSHI MUNIGANTI <vamshi.muniganti@gmail.com>
2021-05-04 12:06:24 +0100

    editpermissionstests_EUI-1622

--- test/e2e/features/pageObjects/inviteUserPage.js
+++ test/e2e/features/pageObjects/inviteUserPage.js
@@ -20,6 +20,12 @@ class InviteUserPage{
 
     this.spinner = element(by.css(".spinner-wrapper"));
 
+    this.activeUser = element(by.xpath("//a[contains(text(),'Townley MCUser')]"));
+    this.changeLink = element(by.linkText("Change"));
+    this.suspendButton = element(by.css(".hmcts-button--secondary"));
+    this.editUserText = element(by.css(".govuk-heading-xl"));
+    this.suspendUserText = element(by.css(".govuk-heading-xl"));
+
   }
 
   /**
@@ -32,7 +38,7 @@ class InviteUserPage{
 
   async selectPermission(permission,isSelect){
     const permisssionCheckboxXpath = by.xpath('//div[@class = "govuk-checkboxes"]//div[contains(@class,"govuk-checkboxes__item")]/label[contains(text(),"' + permission+'")]/../input');
-    
+
     let isSelected = await element(permisssionCheckboxXpath).isSelected();
     console.log(isSelected);
 
@@ -41,7 +47,7 @@ class InviteUserPage{
 
     }
     isSelected = await element(permisssionCheckboxXpath).isSelected();
-    assert(isSelected === isSelect, permission + " selection status is not  " + isSelect );  
+    assert(isSelected === isSelect, permission + " selection status is not  " + isSelect );
   }
   /**
    * Enter random text into the Text field

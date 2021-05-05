784228de06afcb564edf80386cd9fad85427587d
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2021-05-05 15:10:01 +0100

    Resolved spinner issue in unassigned cases

--- src/shared/modules/loader/services/loader-interceptor.service.ts
+++ src/shared/modules/loader/services/loader-interceptor.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, Injector } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
-import { Observable, pipe } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 import { LoaderService } from './loader.service';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,14 +11,7 @@ export class LoaderInterceptorService implements HttpInterceptor {
   constructor(private loaderService: LoaderService) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.showLoader();
-    return next.handle(req).pipe(tap((event: HttpEvent<any>) => {
-      if (event instanceof HttpResponse) {
-        this.onEnd();
-      }
-    },
-      (err: any) => {
-        this.onEnd();
-    }));
+    return next.handle(req).pipe(finalize(() => this.onEnd()));
   }
   private onEnd(): void {
     this.hideLoader();

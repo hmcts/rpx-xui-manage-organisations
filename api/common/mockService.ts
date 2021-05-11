d7c6d056464c04753fa395c2f0babec366b19314
Raymond Liang <ray.liang@atos.net>
2021-05-11 15:57:45 +0100

    EUI-3449-submit-pbas-happy-path:change to generic method name

--- api/common/mockService.ts
+++ api/common/mockService.ts
@@ -5,7 +5,7 @@ import { setHeaders } from '../lib/proxy'
 import { exists } from '../lib/util'
 import { httpMock } from './httpMock'
 
-const logger: JUILogger = log4jui.getLogger('pba-service')
+const logger: JUILogger = log4jui.getLogger('mock-service')
 
 /**
  * Handle GET method

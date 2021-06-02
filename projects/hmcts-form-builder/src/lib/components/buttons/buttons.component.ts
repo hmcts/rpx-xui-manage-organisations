f6ea0f4ac6cfc86a03fc819b49ee8bc9cc3a649a
paul-graham <73112256+paul-graham@users.noreply.github.com>
2021-06-02 14:03:57 +0100

    Lifecycle methods (#678)
    All lifecycle methods should now be public with a void return type.
    
    In the midst of this, also tidied up the affected components further, including fixing a number of problematic imports, sorting out the public/private status of properties and methods, and fixing some indenting issues.

--- projects/hmcts-form-builder/src/lib/components/buttons/buttons.component.ts
+++ projects/hmcts-form-builder/src/lib/components/buttons/buttons.component.ts
@@ -1,28 +1,25 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-buttons',
   templateUrl: './buttons.component.html'
 })
 export class ButtonsComponent implements OnInit {
-    @Input() idPrefix = 'btn';
-    @Input() name = 'btn';
-    @Input() group: FormGroup;
-    @Input() classes;
-    @Input() typeBtn;
-    @Input() control;
-    @Input() value;
+    @Input() public idPrefix = 'btn';
+    @Input() public name = 'btn';
+    @Input() public group: FormGroup;
+    @Input() public classes; // TODO: Add type.
+    @Input() public typeBtn; // TODO: Add type.
+    @Input() public control; // TODO: Add type.
+    @Input() public value; // TODO: Add type.
 
-    constructor() {
-    }
-    isValid: boolean;
-    ngOnInit(): void {
+    public isValid: boolean;
 
+    public ngOnInit(): void {
       this.isValid = this.group.status === 'INVALID';
       this.group.valueChanges.subscribe(value => {
         this.isValid = this.group.status === 'INVALID';
       });
-
     }
 }

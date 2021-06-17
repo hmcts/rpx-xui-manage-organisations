ead427c2617a373b79473e06681cd129723626c9
Paul Graham <paul.graham@hmcts.net>
2021-06-17 09:46:28 +0100

    Unit tests
    Added a unit test for the Update PBA numbers component.

--- src/organisation/containers/update-pba-check/update-pba-numbers-check.component.ts
+++ src/organisation/containers/update-pba-check/update-pba-numbers-check.component.ts
@@ -16,7 +16,8 @@ export class UpdatePbaNumbersCheckComponent implements OnInit {
 
   constructor(
     private readonly router: Router,
-    private readonly orgStore: Store<fromStore.OrganisationState>) { }
+    private readonly orgStore: Store<fromStore.OrganisationState>
+  ) {}
 
   public ngOnInit(): void {
     this.getOrganisationDetailsFromStore();
@@ -42,7 +43,7 @@ export class UpdatePbaNumbersCheckComponent implements OnInit {
   public onSubmitClicked(): void {
     const pendingPaymentAccount: PendingPaymentAccount = {
       pendingAddPaymentAccount: this.organisationDetails.pendingAddPaymentAccount.map(pba => pba.pbaNumber),
-      pendingRemovePaymentAccount: this.organisationDetails.pendingRemovePaymentAccount.map(pba => pba.pbaNumber),
+      pendingRemovePaymentAccount: this.organisationDetails.pendingRemovePaymentAccount.map(pba => pba.pbaNumber)
     };
     this.orgStore.dispatch(new fromStore.OrganisationUpdatePBAs(pendingPaymentAccount));
   }

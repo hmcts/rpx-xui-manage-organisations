7c1fd8a30aaa4d4995b13ed7072d50ff18498446
RiteshHMCTS <74713687+RiteshHMCTS@users.noreply.github.com>
2021-07-12 14:02:43 +0100

    Release-EUI-2439-3787-3843 (#704)
    * EUI-2439-PBA-field
    
    * EUI-2439-PBA-field
    
    * EUI-2439-PBA-field
    
    * EUI-2439-PBA-field
    
    * EUI-3787 functionality done
    
    * EUI-3787 policy page
    
    * EUI-3787 confirmed used cookies and tests
    
    * EUI-3787 lint
    
    * EUI-3787 further lint
    
    * EUI-3787 refactored test file
    
    * EUI-3787 linting
    
    * EUI-3843 copied stuff over from MC and adapted
    
    * EUI-3843 fixed tests
    
    * EUI-3843 semi-colon
    
    * MO CVE test
    
    * MO CVE fix
    
    * disabled t&c's
    
    * removed unwanted tests
    
    Co-authored-by: issshiO <64783858+issshiO@users.noreply.github.com>
    Co-authored-by: Adnan Akgun <adnan.akgun@netclipper.co.uk>
    Co-authored-by: ernestman28 <ernest_man28@hotmail.com>

--- src/app/containers/app/app.component.spec.ts
+++ src/app/containers/app/app.component.spec.ts
@@ -8,19 +8,20 @@ import { cold } from 'jasmine-marbles';
 
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { FeatureToggleService, ManageSessionServices, windowToken} from '@hmcts/rpx-xui-common-lib';
+import { CookieService, FeatureToggleService, ManageSessionServices, windowToken} from '@hmcts/rpx-xui-common-lib';
 
 import * as fromAuth from '../../../user-profile/store';
 import { AppConstants } from '../../app.constants';
 import { ENVIRONMENT_CONFIG } from 'src/models/environmentConfig.model';
 import { of } from 'rxjs';
-import { CookieModule, CookieService } from 'ngx-cookie';
+import { CookieModule } from 'ngx-cookie';
+import { LoggerService } from 'src/shared/services/logger.service';
 
 const windowMock: Window = { gtag: () => {}} as any;
 
 const featureMock: FeatureToggleService = {
   initialize: () => {},
-  isEnabled: () => of(false),
+  isEnabled: () => of(true),
   getValue: () => of(),
   getValueOnce: () => of()
 };
@@ -35,7 +36,13 @@ const idleServiceMock = {
 
 describe('AppComponent', () => {
   let store: Store<fromAuth.AuthState>;
+  let fixture;
+  let app;
+  let loggerService: any;
+  let cookieService: any;
   beforeEach(async(() => {
+    cookieService = jasmine.createSpyObj('CookieService', ['deleteCookieByPartialMatch']);
+    loggerService = jasmine.createSpyObj('LoggerService', ['enableCookies']);
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
@@ -69,26 +76,26 @@ describe('AppComponent', () => {
           provide: ManageSessionServices,
           useValue: idleServiceMock
         },
+        {
+          provide: CookieService,
+          useValue: cookieService
+        },
+        {
+          provide: LoggerService,
+          useValue: loggerService
+        }
       ],
     }).compileComponents();
     store = TestBed.get(Store);
 
     spyOn(store, 'dispatch').and.callThrough();
-  }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-
-    const app = fixture.debugElement.componentInstance;
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
     fixture.detectChanges();
-    expect(app).toBeTruthy();
   }));
 
   it('should have pageTitle$ Observable the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    fixture.detectChanges();
-
     const expected = cold('a', { a: '' });
     expect(app.pageTitle$).toBeObservable(expected);
 
@@ -96,20 +103,12 @@ describe('AppComponent', () => {
 
 
   it('should have appHeaderTitle$ Observable the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    fixture.detectChanges();
-
     const expected = cold('a', { a: undefined });
     expect(app.appHeaderTitle$).toBeObservable(expected);
 
   }));
 
   it('should have userNav$ Observable the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    fixture.detectChanges();
-
     const expected = cold('a', { a: [] });
     expect(app.userNav$).toBeObservable(expected);
 
@@ -117,9 +116,6 @@ describe('AppComponent', () => {
 
 
   it('should have navItems$ Observable the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    fixture.detectChanges();
     const navItems = [
       {
         text: 'Organisation',
@@ -137,13 +133,68 @@ describe('AppComponent', () => {
   }));
 
   it('should dispatch a logout action', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     app.onNavigate('sign-out');
     fixture.detectChanges();
 
     expect(store.dispatch).toHaveBeenCalledWith(new Logout());
 
   }));
+
+
+  describe('cookie actions', () => {
+
+    describe('setCookieBannerVisibility()', () => {
+        it('should set isCookieBannerVisible true when there is no cookie and there is a user and cookie banner is feature toggled on', () => {
+
+          app.handleCookieBannerFeatureToggle();
+          app.setUserAndCheckCookie('dummy');
+          expect(app.isCookieBannerVisible).toBeTruthy();
+        });
+
+        it('should set isCookieBannerVisible false when there is no cookie and there is no user and cookie banner is feature toggled on', () => {
+            app.handleCookieBannerFeatureToggle();
+            expect(app.isCookieBannerVisible).toBeFalsy();
+        });
+    });
+
+    describe('setUserAndCheckCookie()', () => {
+
+        it('should call setCookieBannerVisibility', () => {
+            const spy = spyOn(app, 'setCookieBannerVisibility');
+            app.setUserAndCheckCookie('dummy');
+            expect(spy).toHaveBeenCalled();
+        });
+
+    });
+
+    describe('handleCookieBannerFeatureToggle()', () => {
+
+        it('should make a call to setCookieBannerVisibility', () => {
+            const spy = spyOn(app, 'setCookieBannerVisibility');
+            app.handleCookieBannerFeatureToggle();
+            expect(spy).toHaveBeenCalled();
+        });
+
+    });
+
+    describe('notifyAcceptance()', () => {
+
+        it('should make a call to loggerService', () => {
+            app.notifyAcceptance();
+            expect(loggerService.enableCookies).toHaveBeenCalled();
+        });
+
+    });
+
+    describe('notifyRejection()', () => {
+
+        it('should make a call to cookieService', () => {
+            app.notifyRejection();
+            expect(cookieService.deleteCookieByPartialMatch).toHaveBeenCalled();
+        });
+
+    });
+
+});
 
 });

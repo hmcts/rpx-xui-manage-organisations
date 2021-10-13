6f1ae66c428fcf6b767c557e18727de079bef6ec
RiteshHMCTS <ritesh.dsouza@HMCTS.net>
2021-10-13 15:33:48 +0100

    FT update for PBA details entry

--- test/e2e/features/step_definitions/createOrganisation.steps.js
+++ test/e2e/features/step_definitions/createOrganisation.steps.js
@@ -76,8 +76,8 @@ defineSupportCode(function ({ Given, When, Then }) {
     browser.sleep(MID_DELAY);
     await createOrganisationObject.PBANumber1.isDisplayed();
     await createOrganisationObject.enterPBANumber();
-    await createOrganisationObject.PBANumber2.isDisplayed();
-    await createOrganisationObject.enterPBA2Number();
+    //await createOrganisationObject.PBANumber2.isDisplayed();
+    //await createOrganisationObject.enterPBA2Number();
     await createOrganisationObject.continue_button.click();
     browser.sleep(MID_DELAY);
   });
@@ -223,7 +223,7 @@ defineSupportCode(function ({ Given, When, Then }) {
   When(/^I Enter the invalid PBA1 and PBA2 details$/,  async function () {
     await expect(createOrganisationObject.PBANumber1.isDisplayed()).to.eventually.be.true;
     await createOrganisationObject.PBANumber1.sendKeys(1234455558);
-    await createOrganisationObject.PBANumber2.sendKeys(1233334988);
+    //await createOrganisationObject.PBANumber2.sendKeys(1233334988);
     await createOrganisationObject.continue_button.click();
     // browser.sleep(LONG_DELAY);
   });

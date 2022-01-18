66a4bfd56a46bbacb20643a78f99bd914673ae79
Paul Howes <paul.howes@HMCTS.NET>
2022-01-18 18:23:54 +0000

    EUI-3126 PR639

--- src/register/constants/pbaNumber.ts
+++ src/register/constants/pbaNumber.ts
@@ -3,69 +3,74 @@ export const pbaNumber = {
   meta: {
     idPrefix: 'tbc',
     name: 'organisation-pba',
-    header: 'What are the Payment by Account (PBA) numbers for your organisation?',
-    formGroupValidators: [],
+    header: 'What payment by account (PBA) numbers does your organisation have?',
+    formGroupValidators: [
+      {
+        validatorFunc: 'duplicatedPBACheck',
+        validationErrorId: 'duplicatedPBAError',
+        validationLevel: 'formGroup',
+        controls: 'PBANumber',
+        text: 'You have entered this PBA number more than once',
+      },
+      {
+        validatorFunc: 'invalidPBANumberCheck',
+        validationErrorId: 'invalidPBANumberError',
+        validationLevel: 'formGroup',
+        controls: 'PBANumber',
+        text: 'Enter a valid PBA number',
+      }],
     validationHeaderErrorMessages: [
       {
         validationLevel: 'formControl',
         controlId: 'PBANumber1',
         text: 'Enter a valid PBA number',
       },
-      {
-        validationLevel: 'formControl',
-        controlId: 'PBANumber2',
-        text: 'Enter a valid PBA number',
-      },
     ],
     groups: [
       {
-        fieldset: [
-          {
-            legend: {
-              text: 'What are the Payment by Account (PBA) numbers for your organisation?',
-              classes: 'govuk-fieldset__legend--xl'
+        inputButton: {
+          label: {
+            text: 'PBA number (optional)',
+            classes: 'govuk-label--m',
+          },
+          control: 'PBANumber1',
+          type: 'inputButton',
+          validators: ['pbaNumberPattern', 'pbaNumberMaxLength', 'pbaNumberMinLength'],
+          validationErrors: [
+            {
+              validationErrorId: 'duplicatedPBAError',
+              validationLevel: 'formControl',
+              controls: 'PBANumber',
+              text: 'You have entered this PBA number more than once',
+            },
+            {
+              validationErrorId: 'invalidPBANumberError',
+              validationLevel: 'formControl',
+              controls: 'PBANumber',
+              text: 'Enter a valid PBA number',
             }
-          },
+          ],
+          classes: 'govuk-width-input-button',
+        },
+      },
+      {
+        button: {
+          control: 'addAnotherPBANumber',
+          value: 'Add another PBA number',
+          type: 'button',
+          classes: 'hmcts-button--secondary  hmcts-add-another__add-button',
+          onEvent: 'addAnotherPBANumber',
+        },
+      },
+      {
+        extension:
           {
-            input: {
-              label: {
-                text: 'PBA number 1 (optional)',
-                classes: 'govuk-label--m',
-              },
-              control: 'PBANumber1',
-              validators: ['pbaNumberPattern', 'pbaNumberMaxLength', 'pbaNumberMinLength'],
-              validationError: {
-                value: 'Enter a valid PBA number',
-                controlId: 'PBANumber1',
-              },
-              classes: 'govuk-!-width-two-thirds',
-            },
-          },
-          {
-            input: {
-              label: {
-                text: 'PBA number 2 (optional)',
-                classes: 'govuk-label--m',
-              },
-              control: 'PBANumber2',
-              validators: ['pbaNumberPattern', 'pbaNumberMaxLength', 'pbaNumberMinLength'],
-              validationError: {
-                value: 'Enter a valid PBA number',
-                controlId: 'PBANumber2',
-              },
-              classes: 'govuk-!-width-two-thirds',
-            },
-          },
-          {
-            extension:
-              { componentDetails: {
-                title: 'Why add a PBA number?',
-                  text: 'Adding a PBA number for your organisation will allow you to view your:',
-                  ul: ['account balance', 'available credit', 'transactions'],
-                }
-              }
+            componentDetails: {
+              title: 'Why add a PBA number?',
+              text: 'Adding a PBA number for your organisation will allow you to view your:',
+              ul: ['account balance', 'available credit', 'transactions'],
+            }
           }
-        ]
       },
       {
         button: {
@@ -80,3 +85,4 @@ export const pbaNumber = {
   },
   newRoute: null
 };
+

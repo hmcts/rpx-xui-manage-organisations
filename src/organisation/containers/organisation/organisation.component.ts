92d6f4495c19eeda8557f66e559607ac66177822
Paul Howes <paul.howes@HMCTS.NET>
2022-01-19 09:39:00 +0000

    EUI-3378 PR664

--- src/organisation/containers/organisation/organisation.component.ts
+++ src/organisation/containers/organisation/organisation.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 
 import { DxAddress, OrganisationContactInformation, OrganisationDetails } from '../../../models/organisation.model';
+import * as fromAuthStore from '../../../user-profile/store/index';
 import * as fromStore from '../../store';
 
 @Component({
   selector: 'app-prd-organisation-component',
   templateUrl: './organisation.component.html',
 })
+
 export class OrganisationComponent implements OnInit {
 
   public organisationDetails: Partial<OrganisationDetails>;
@@ -15,7 +17,11 @@ export class OrganisationComponent implements OnInit {
   public organisationDxAddress: DxAddress;
   public organisationPaymentAccount: string[];
 
-  constructor(private readonly store: Store<fromStore.OrganisationState>) {
+  public showChangePbaNumberLink: boolean;
+
+  constructor(
+    private readonly orgStore: Store<fromStore.OrganisationState>,
+    private readonly authStore: Store<fromAuthStore.AuthState>) {
   }
 
   public ngOnInit(): void {
@@ -78,13 +84,20 @@ export class OrganisationComponent implements OnInit {
    * Once we have the Organisation Details, we display them on the page.
    */
   public getOrganisationDetailsFromStore(): void {
-    this.store.pipe(select(fromStore.getOrganisationSel)).subscribe(organisationDetails => {
+    this.orgStore.pipe(select(fromStore.getOrganisationSel)).subscribe(organisationDetails => {
 
       this.organisationContactInformation = this.getContactInformation(organisationDetails);
       this.organisationPaymentAccount = this.getPaymentAccount(organisationDetails);
       this.organisationDxAddress = this.getDxAddress(this.organisationContactInformation);
 
       this.organisationDetails = organisationDetails;
+      this.canShowChangePbaNumbersLink();
+    });
+  }
+
+  public canShowChangePbaNumbersLink(): void {
+    this.authStore.pipe(select(fromAuthStore.getIsUserPuiFinanceManager)).subscribe((userIsPuiFinanceManager: boolean) => {
+      this.showChangePbaNumberLink = userIsPuiFinanceManager;
     });
   }
 

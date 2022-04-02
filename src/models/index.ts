692494e1dcf6763345ca179fa3e9b3629f08b4b2
waynepHMCTS <90253865+waynepHMCTS@users.noreply.github.com>
2022-04-02 12:03:31 +0100

    src folder

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,44 +1,4 @@
-
-export interface DxAddress {
-  dxNumber: string;
-  dxExchange: string;
-}
-
-export interface PBANumberModel {
-  pbaNumber: string;
-  status?: string;
-  statusMessage?: string;
-  dateCreated?: string;
-  dateAccepted?: string;
-}
-
-
-export interface OrganisationContactInformation {
-  addressLine1: string;
-  addressLine2: string;
-  addressLine3: string;
-  townCity: string;
-  county: string;
-  country: string;
-  postCode: string;
-  dxAddress: DxAddress[];
-}
-
-export interface OrganisationDetails {
-  name: string;
-  organisationIdentifier: string;
-  contactInformation: OrganisationContactInformation[];
-  status: string;
-  sraId: string;
-  sraRegulated: boolean;
-  superUser: {
-    firstName: string;
-    lastName: string;
-    email: string;
-  };
-  paymentAccount: PBANumberModel[];
-  pendingPaymentAccount?: PBANumberModel[];
-  pendingAddPaymentAccount: PBANumberModel[];
-  pendingRemovePaymentAccount: PBANumberModel[];
-  response?: any;
-}
+export * from './environmentConfig.model';
+export * from './organisation.model';
+export * from './pbaNumber.model';
+export * from './pendingPaymentAccount.model';
--- src/register/containers/hmcts-form-builder/src/lib/components/legend/legend.component.ts
+++ src/register/containers/hmcts-form-builder/src/lib/components/legend/legend.component.ts
@@ -5,5 +5,5 @@ import {Component, Input} from '@angular/core';
   templateUrl: './legend.component.html'
 })
 export class LegendComponent {
-    @Input() classes:  string;
+    @Input() public classes: string;
 }

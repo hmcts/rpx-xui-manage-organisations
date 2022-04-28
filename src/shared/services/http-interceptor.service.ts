37754eadf960052671efb1b9eaa16da4852748d8
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2022-04-28 07:07:17 +0100

    before upgrade check

--- src/shared/services/http-interceptor.service.ts
+++ src/shared/services/http-interceptor.service.ts
@@ -1,19 +1,10 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import 'rxjs/add/operator/do';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
+  HttpEvent, HttpHandler, HttpInterceptor, HttpRequest
 } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { HeadersService } from './headers.service';
-import { PLATFORM_ID, Inject } from '@angular/core';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/observable/fromPromise';
-
 
 @Injectable({
   providedIn: 'root'

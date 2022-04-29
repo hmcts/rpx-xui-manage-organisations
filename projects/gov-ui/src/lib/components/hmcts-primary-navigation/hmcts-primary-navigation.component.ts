d93fb827240a8ba46707ef82eac0d30208d7218a
Kartheek-S <92437222+Kartheek-S@users.noreply.github.com>
2022-04-29 11:25:47 +0100

    fixed styling

--- projects/gov-ui/src/lib/components/hmcts-primary-navigation/hmcts-primary-navigation.component.ts
+++ projects/gov-ui/src/lib/components/hmcts-primary-navigation/hmcts-primary-navigation.component.ts
@@ -2,23 +2,21 @@ import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-    selector: 'app-hmcts-primary-navigation',
-    templateUrl: './hmcts-primary-navigation.component.html',
-    styleUrls: ['./hmcts-primary-navigation.component.scss']
+  selector: 'app-hmcts-primary-navigation',
+  templateUrl: './hmcts-primary-navigation.component.html',
+  styleUrls: ['./hmcts-primary-navigation.component.scss']
 })
 export class HmctsPrimaryNavigationComponent {
 
-    @Input() set userLoggedIn(value) {
-        this.isUserLoggedIn = value;
-    }
+  @Input() public set userLoggedIn(value) {
+    this.isUserLoggedIn = value;
+  }
 
-    @Input() label: string;
-    @Input() items: object[];
-    @Input() isBrandedHeader: boolean;
+  @Input() public label: string;
+  @Input() public items: object[];
+  @Input() public isBrandedHeader: boolean;
 
-    isUserLoggedIn: boolean;
-    constructor(private route: ActivatedRoute) {
-
-    }
+  public isUserLoggedIn: boolean;
+  constructor(private readonly route: ActivatedRoute) {}
 
 }
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,6 +1,4 @@
-import {
-  HmctsPrimaryNavigationComponent
-} from '../../../projects/gov-ui/src/lib/components/hmcts-primary-navigation/hmcts-primary-navigation.component';
+import { HmctsPrimaryNavigationComponent } from '../../../projects/gov-ui/src/lib/components/hmcts-primary-navigation/hmcts-primary-navigation.component';
 import { AccessibilityComponent } from './accessibility/accessibility.component';
 import { CookiePolicyComponent } from './cookie-policy/cookie-policy.component';
 import { GetHelpComponent } from './get-help/get-help.component';
@@ -8,8 +6,8 @@ import { HmctsGlobalFooterComponent } from './hmcts-global-footer/hmcts-global-f
 import { HmctsGlobalHeaderComponent } from './hmcts-global-header/hmcts-global-header.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { ServiceDownComponent } from './service-down/service-down.component';
-import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
 import { SignedOutComponent } from './signed-out/signed-out.component';
+import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
 
 export const components: any[] = [
   HmctsGlobalHeaderComponent,
@@ -24,13 +22,14 @@ export const components: any[] = [
   SignedOutComponent
 ];
 
-export * from './hmcts-global-header/hmcts-global-header.component';
-export * from './hmcts-global-footer/hmcts-global-footer.component';
 export * from '../../../projects/gov-ui/src/lib/components/hmcts-primary-navigation/hmcts-primary-navigation.component';
-export * from './cookie-policy/cookie-policy.component';
-export * from './privacy-policy/privacy-policy.component';
 export * from './accessibility/accessibility.component';
-export * from './service-down/service-down.component';
+export * from './cookie-policy/cookie-policy.component';
 export * from './get-help/get-help.component';
-export * from './terms-and-conditions/terms-and-conditions.component';
+export * from './hmcts-global-footer/hmcts-global-footer.component';
+export * from './hmcts-global-header/hmcts-global-header.component';
+export * from './privacy-policy/privacy-policy.component';
+export * from './service-down/service-down.component';
 export * from './signed-out/signed-out.component';
+export * from './terms-and-conditions/terms-and-conditions.component';
+

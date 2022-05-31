760c7423ccbffedb6e2b13c272982eff7fa07b09
RiteshHMCTS <74713687+RiteshHMCTS@users.noreply.github.com>
2022-05-31 10:03:36 +0100

    EUI-5736-pagination (#845)
    * pagination
    
    * Unassigned cases pagination
    
    * resolved lint error
    
    * removed unwanted code
    
    * common lib & toolkit version updated
    
    * added pagination

--- api/unassignedCases/index.ts
+++ api/unassignedCases/index.ts
@@ -6,7 +6,11 @@ import { getApiPath, getRequestBody, mapCcdCases } from './unassingedCases-util'
 export async function handleUnassignedCases(req: Request, res: Response, next: NextFunction) {
   const caseTypeId = req.query.caseTypeId as string
   const path = getApiPath(getConfigValue(SERVICES_MCA_PROXY_API_PATH), caseTypeId)
-  const payload = getRequestBody(req.session.auth.orgId)
+  const page: number = (+req.query.pageNo || 1) - 1;
+  const size: number = (+req.query.pageSize);
+  const fromNo: number = page * size;
+  const payload = getRequestBody(req.session.auth.orgId, fromNo, size)
+
   try {
     const response = await req.http.post(path, payload)
     const unassignedCases = mapCcdCases(caseTypeId, response.data)

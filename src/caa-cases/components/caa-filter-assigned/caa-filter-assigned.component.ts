84efc1514e285049f45d9c11589235cad0ad68e1
John Benjamin <john.benjamin@hmcts.net>
2022-07-28 13:30:13 +0100

    EUI-6345 CAA unassigned cases filter

--- src/caa-cases/components/caa-filter-assigned/caa-filter-assigned.component.ts
+++ src/caa-cases/components/caa-filter-assigned/caa-filter-assigned.component.ts
@@ -12,6 +12,7 @@ export class CaaFilterAssignedComponent implements OnInit {
   @Input() public selectedFilterType: string;
 
   @Output() public emitSelectedFilterType = new EventEmitter<string>();
+  @Output() public emitSelectedFilterValue = new EventEmitter<string>();
 
   public caaFormGroup: FormGroup;
   public caaCasesPageType = CaaCasesPageType;
@@ -32,5 +33,21 @@ export class CaaFilterAssignedComponent implements OnInit {
   public selectFilterOption(caaCasesFilterType: string): void {
     this.selectedFilterType = caaCasesFilterType;
     this.emitSelectedFilterType.emit(this.selectedFilterType);
+  }
+
+  public search(): void {
+    let selectedFilterValue: string;
+    switch (this.selectedFilterType) {
+      case CaaCasesFilterType.assigneeName:
+        selectedFilterValue = this.caaFormGroup.get('assignee-person').value;
+        break;
+      case CaaCasesFilterType.caseReferenceNumber:
+        selectedFilterValue = this.caaFormGroup.get('case-reference-number').value;
+        break;
+      default:
+        selectedFilterValue = null;
+        break;
+    }
+    this.emitSelectedFilterValue.emit(selectedFilterValue);
   }
 }

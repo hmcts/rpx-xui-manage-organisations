fc55ffde8c42945d47bcbdfbcec790d190d14b05
jdHMCTS <105708661+jdHMCTS@users.noreply.github.com>
2022-07-29 10:04:01 +0100

    remove telemetry api call for requests (#882)
    * remove telemetry api call for requests
    
    * CVE
    
    Co-authored-by: jDman <davis.jaym@gmail.com>

--- api/application.ts
+++ api/application.ts
@@ -1,26 +1,23 @@
 import * as healthcheck from '@hmcts/nodejs-healthcheck'
-import {SESSION, xuiNode} from '@hmcts/rpx-xui-node-lib'
+import { SESSION, xuiNode } from '@hmcts/rpx-xui-node-lib'
 import * as bodyParser from 'body-parser'
 import * as cookieParser from 'cookie-parser'
 import * as express from 'express'
 import * as helmet from 'helmet'
-import {attach, getXuiNodeMiddleware} from './auth'
-import {environmentCheckText, getConfigValue, getEnvironment, showFeature} from './configuration'
-import {ERROR_NODE_CONFIG_ENV} from './configuration/constants'
+import { attach, getXuiNodeMiddleware } from './auth'
+import { environmentCheckText, getConfigValue, getEnvironment, showFeature } from './configuration'
+import { ERROR_NODE_CONFIG_ENV } from './configuration/constants'
 import {
   CASE_TYPES,
   FEATURE_HELMET_ENABLED,
   FEATURE_REDIS_ENABLED,
   FEATURE_TERMS_AND_CONDITIONS_ENABLED,
-  HELMET,
-  SERVICE_S2S_PATH,
-  SERVICES_CCD_DATA_STORE_API_PATH,
+  HELMET, SERVICES_CCD_DATA_STORE_API_PATH,
   SERVICES_FEE_AND_PAY_API_PATH,
   SERVICES_MCA_PROXY_API_PATH,
   SERVICES_RD_PROFESSIONAL_API_PATH,
-  SERVICES_TERMS_AND_CONDITIONS_API_PATH, SESSION_SECRET,
+  SERVICES_TERMS_AND_CONDITIONS_API_PATH, SESSION_SECRET
 } from './configuration/references'
-import {appInsights} from './lib/appInsights'
 import * as log4jui from './lib/log4jui'
 import * as tunnel from './lib/tunnel'
 import openRoutes from './openRoutes'
@@ -48,7 +45,6 @@ if (showFeature(FEATURE_HELMET_ENABLED)) {
   app.use(helmet(getConfigValue(HELMET)))
 }
 
-app.use(appInsights)
 app.use(bodyParser.json({limit: '5mb'}))
 app.use(bodyParser.urlencoded({limit: '5mb', extended: true}))
 app.use(cookieParser(getConfigValue(SESSION_SECRET)))

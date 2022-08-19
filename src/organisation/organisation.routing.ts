50bc315bf7b51918595037cd2a085e2d5a234046
jDman <davis.jaym@gmail.com>
2022-08-19 16:49:17 +0100

    updated some imports

--- src/organisation/organisation.routing.ts
+++ src/organisation/organisation.routing.ts
@@ -1,8 +1,8 @@
 // routes
 import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { OrganisationGuard } from 'src/organisation/guards/organisation.guard';
-import { HealthCheckGuard } from 'src/shared/guards/health-check.guard';
+import { OrganisationGuard } from './guards/organisation.guard';
+import { HealthCheckGuard } from '../shared/guards/health-check.guard';
 import { OrganisationComponent } from './containers';
 import { OrganisationModule } from './organisation.module';
 

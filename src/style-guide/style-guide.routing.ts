2d4bcc433f0f54e3b4c942f4c296ce9875f214e4
jDman <davis.jaym@gmail.com>
2022-08-22 09:50:06 +0100

    WIP

--- src/style-guide/style-guide.routing.ts
+++ src/style-guide/style-guide.routing.ts
@@ -2,7 +2,6 @@
 import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StyleGuideComponent } from './containers/style-guide/style-guide.component';
-import {AuthGuard} from '../user-profile/guards/auth.guard';
 import { StyleGuideModule } from './style-guide.module';
 
 export const ROUTES: Routes = [

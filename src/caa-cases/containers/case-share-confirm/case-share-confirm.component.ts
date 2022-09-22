40c15163d8d25981f6e664799dd7bc3ad46bc0e2
John Benjamin <john.benjamin@hmcts.net>
2022-09-22 15:59:51 +0100

    EUI-5559 CAA case sharing deselect case

--- src/caa-cases/containers/case-share-confirm/case-share-confirm.component.ts
+++ src/caa-cases/containers/case-share-confirm/case-share-confirm.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { SharedCase } from '@hmcts/rpx-xui-common-lib/lib/models/case-share.model';
-import { RouterReducerState } from '@ngrx/router-store';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { getRouterState, RouterStateUrl } from '../../../app/store/reducers';
 import * as fromCasesFeature from '../../store';
 import * as fromCaseList from '../../store/reducers';
 
@@ -14,40 +13,36 @@ import * as fromCaseList from '../../store/reducers';
 })
 export class CaseShareConfirmComponent implements OnInit {
 
-  public routerState$: Observable<RouterReducerState<RouterStateUrl>>;
   public shareCases$: Observable<SharedCase[]>;
   public shareCases: SharedCase[];
+  public url: string;
   public fnTitle: string;
   public backLink: string;
   public changeLink: string;
   public completeLink: string;
 
-  constructor(public store: Store<fromCaseList.CaaCasesState>) {}
+  constructor(private readonly store: Store<fromCaseList.CaaCasesState>,
+              private readonly router: Router) {
+    this.url = this.router && this.router.url && this.router.url;
+  }
 
   public ngOnInit(): void {
-    this.routerState$ = this.store.pipe(select(getRouterState));
-    this.routerState$.subscribe(router => {
-      // Set backLink and fnTitle depending on whether navigation
-      // is via the Unassigned Cases or Assigned Cases page
-      const url = router.state.url.substring(0, router.state.url.indexOf('/', 1));
-      // Set backLink only if the URL is either "/unassigned-cases" or "/assigned-cases"
-      if (url === '/unassigned-cases' || url === '/assigned-cases') {
-        this.backLink = `${url}`;
-      }
-      if (url === '/unassigned-cases') {
-        this.fnTitle = 'Share a case';
-        this.changeLink = '/unassigned-cases/case-share';
-        this.completeLink = '/unassigned-cases/case-share-complete';
-      } else if (url === '/assigned-cases') {
-        this.fnTitle = 'Manage case sharing';
-        this.changeLink = '/assigned-cases/case-share';
-        this.completeLink = '/assigned-cases/case-share-complete';
-      }
-    });
+    // Set fnTitle, backLink, changeLink and confirmLink depending on whether navigation
+    // is via the Unassigned Cases or Assigned Cases page
+    if (this.url.startsWith('/unassigned-cases')) {
+      this.fnTitle = 'Share a case';
+      this.backLink = '/unassigned-cases';
+      this.changeLink = '/unassigned-cases/case-share';
+      this.completeLink = '/unassigned-cases/case-share-complete';
+    } else {
+      this.fnTitle = 'Manage case sharing';
+      this.backLink = '/assigned-cases';
+      this.changeLink = '/assigned-cases/case-share';
+      this.completeLink = '/assigned-cases/case-share-complete';
+    }
     this.shareCases$ = this.store.pipe(select(fromCasesFeature.getShareCaseListState));
     this.shareCases$.subscribe(shareCases => {
       this.shareCases = shareCases;
     });
   }
-
 }

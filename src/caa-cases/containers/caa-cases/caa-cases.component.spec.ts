bfe49e6006a560a866da189223c5c34dbf9f01b6
John Benjamin <john.benjamin@hmcts.net>
2022-09-26 14:48:29 +0100

    EUI-6813 CAA filter no cases found message

--- src/caa-cases/containers/caa-cases/caa-cases.component.spec.ts
+++ src/caa-cases/containers/caa-cases/caa-cases.component.spec.ts
@@ -1,8 +1,10 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TableConfig } from '@hmcts/ccd-case-ui-toolkit';
 import { Store, StoreModule } from '@ngrx/store';
+import { of } from 'rxjs';
 import * as fromOrganisationStore from '../../../organisation/store';
 import { CaaCasesFilterType, CaaCasesNoDataMessage, CaaCasesPageTitle, CaaCasesPageType, CaaShowHideFilterButtonText } from '../../models/caa-cases.enum';
 import * as fromStore from '../../store';
@@ -16,7 +18,7 @@ describe('CaaCasesComponent', () => {
   let organisationStore: Store<fromOrganisationStore.OrganisationState>;
   let router: Router;
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         StoreModule.forRoot({}),
@@ -26,11 +28,11 @@ describe('CaaCasesComponent', () => {
       declarations: [ CaaCasesComponent ]
     })
     .compileComponents();
+  }));
+
+  beforeEach(() => {
     store = TestBed.get(Store);
     organisationStore = TestBed.get(Store);
-    spyOn(store, 'pipe').and.callThrough();
-    spyOn(store, 'dispatch').and.callThrough();
-    // spyOn(ngrxStore, 'select').and.callThrough();
     fixture = TestBed.createComponent(CaaCasesComponent);
     component = fixture.componentInstance;
     router = TestBed.get(Router);
@@ -45,40 +47,8 @@ describe('CaaCasesComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should set the page title and show/hide filter button text correctly', () => {
-    // The fake url needs to be set before the component constructor is called
-    const routerUrlPropertySpy = spyOnProperty(router, 'url', 'get').and.returnValue('/unassigned-cases');
-    fixture = TestBed.createComponent(CaaCasesComponent);
-    component = fixture.componentInstance;
-    expect(component.caaCasesPageType).toBe(CaaCasesPageType.UnassignedCases);
-    expect(component.pageTitle).toEqual(CaaCasesPageTitle.UnassignedCases);
-    expect(component.caaShowHideFilterButtonText).toEqual(CaaShowHideFilterButtonText.UnassignedCasesShow);
-    routerUrlPropertySpy.and.returnValue('/assigned-cases');
-    fixture = TestBed.createComponent(CaaCasesComponent);
-    component = fixture.componentInstance;
-    expect(component.caaCasesPageType).toBe(CaaCasesPageType.AssignedCases);
-    expect(component.pageTitle).toEqual(CaaCasesPageTitle.AssignedCases);
-    expect(component.caaShowHideFilterButtonText).toEqual(CaaShowHideFilterButtonText.AssignedCasesShow);
-  });
-
-  it('should return correct no cases found message', () => {
-    let cases = [];
-    component.totalCases = 0;
-    component.caaCasesPageType = CaaCasesPageType.AssignedCases;
-    expect(component.getNoCasesFoundMessage(cases)).toEqual(CaaCasesNoDataMessage.NoAssignedCases);
-    component.caaCasesPageType = CaaCasesPageType.UnassignedCases;
-    expect(component.getNoCasesFoundMessage(cases)).toEqual(CaaCasesNoDataMessage.NoUnassignedCases);
-    component.totalCases = 1;
-    component.caaCasesPageType = CaaCasesPageType.AssignedCases;
-    expect(component.getNoCasesFoundMessage(cases)).toEqual(CaaCasesNoDataMessage.AssignedCasesFilterMessage);
-    component.caaCasesPageType = CaaCasesPageType.UnassignedCases;
-    expect(component.getNoCasesFoundMessage(cases)).toEqual(CaaCasesNoDataMessage.UnassignedCasesFilterMessage);
-    cases = null;
-    expect(component.getNoCasesFoundMessage(cases)).toEqual('');
-  });
-
   it('should set current page type', () => {
-    let routerUrlPropertySpy = spyOnProperty(router, 'url', 'get').and.returnValue('/unassigned-cases');
+    const routerUrlPropertySpy = spyOnProperty(router, 'url', 'get').and.returnValue('/unassigned-cases');
     component.setCurrentPageType();
     expect(component.caaCasesPageType).toEqual(CaaCasesPageType.UnassignedCases);
     routerUrlPropertySpy.and.returnValue('/assigned-cases');
@@ -116,12 +86,40 @@ describe('CaaCasesComponent', () => {
 
   it('should load cases and set table config', () => {
     spyOn(component, 'setTableConfig');
+    const storePipeMock = spyOn(store, 'pipe');
+    const storeSelectMock = spyOn(store, 'select');
+    storePipeMock.and.returnValue(of({}));
+    storeSelectMock.and.returnValue(of({}));
     component.caaCasesPageType = CaaCasesPageType.UnassignedCases;
     component.loadCasesAndSetTableConfig();
-    store.pipe
     component.caaCasesPageType = CaaCasesPageType.AssignedCases;
     component.loadCasesAndSetTableConfig();
     expect(component.setTableConfig).toHaveBeenCalledTimes(2);
+  });
+
+  it('should set table config', () => {
+    let config = null;
+    component.setTableConfig(config);
+    expect(component.tableConfig).toBeUndefined();
+    config = new TableConfig();
+    component.setTableConfig(config);
+    expect(component.tableConfig).not.toBeNull();
+  });
+
+  it('should return correct no cases found message', () => {
+    let cases = [];
+    component.totalCases = 0;
+    component.caaCasesPageType = CaaCasesPageType.AssignedCases;
+    expect(component.getNoCasesFoundMessage(cases)).toEqual(CaaCasesNoDataMessage.NoAssignedCases);
+    component.caaCasesPageType = CaaCasesPageType.UnassignedCases;
+    expect(component.getNoCasesFoundMessage(cases)).toEqual(CaaCasesNoDataMessage.NoUnassignedCases);
+    component.totalCases = 1;
+    component.caaCasesPageType = CaaCasesPageType.AssignedCases;
+    expect(component.getNoCasesFoundMessage(cases)).toEqual(CaaCasesNoDataMessage.AssignedCasesFilterMessage);
+    component.caaCasesPageType = CaaCasesPageType.UnassignedCases;
+    expect(component.getNoCasesFoundMessage(cases)).toEqual(CaaCasesNoDataMessage.UnassignedCasesFilterMessage);
+    cases = null;
+    expect(component.getNoCasesFoundMessage(cases)).toEqual('');
   });
 
   // TODO: Need to revisit this test, as it doesn't seem possible to spy on the ngrx select operator directly

c22e8176a37e37861d0b7b2da3e535b1a97ddfd0
Phillip Whitaker <phillip.whitaker1@hmcts.net>
2022-11-23 13:07:03 +0000

    added service to unassigned cases

--- src/unassigned-cases/unassigned-cases.module.ts
+++ src/unassigned-cases/unassigned-cases.module.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
-import { CaseListModule, PaletteModule } from '@hmcts/ccd-case-ui-toolkit';
+import { CaseListModule } from '@hmcts/ccd-case-ui-toolkit';
 import { ExuiCommonLibModule } from '@hmcts/rpx-xui-common-lib';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { PBAService } from 'src/organisation/services/pba.service';
 import { OrganisationService } from '../organisation/services';
-import { SharedModule } from '../shared/shared.module';
 import { effects as orgEffects, reducers as orgReducers } from '../organisation/store';
+import { SharedModule } from '../shared/shared.module';
 import * as fromContainers from './containers';
 import { FeatureToggleAccountGuard } from './guards/feature-toggle.guard';
 import { RoleGuard } from './guards/user-role.guard';
@@ -27,12 +28,11 @@ import { unassignedCasesRouting } from './unassigned-cases.routing';
       EffectsModule.forFeature(orgEffects),
       StoreModule.forFeature('unassignedCases', reducers),
       EffectsModule.forFeature(effects),
-      PaletteModule,
       MatTabsModule,
       CaseListModule
     ],
     declarations: [...fromContainers.containers],
-    providers: [...fromServices.services, OrganisationService, FeatureToggleAccountGuard, RoleGuard],
+    providers: [...fromServices.services, OrganisationService, PBAService, FeatureToggleAccountGuard, RoleGuard],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
   })
 export class UnassignedCasesModule {}

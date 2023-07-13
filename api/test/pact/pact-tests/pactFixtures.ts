3d298f919dd9cd64cd5da973c4bb088eb90cd0bc
DavidJayakumar <David.Jayakumar@hmcts.net>
2023-07-13 10:47:33 +0100

    Add test for getUsers

--- api/test/pact/pact-tests/pactFixtures.ts
+++ api/test/pact/pact-tests/pactFixtures.ts
@@ -122,3 +122,21 @@ export interface InviteUserResponse {
 export interface OrganisationCreatedResponse {
   organisationIdentifier: string;
 }
+
+export interface UserResponse {
+  organisationIdentifier: string;
+  users: User[]
+}
+
+export interface User {
+  userIdentifier: string,
+  firstName: string,
+  lastName: string,
+  email: string,
+  idamStatus: string,
+  roles: [
+    string
+  ],
+  idamStatusCode: string,
+  idamMessage: string
+}

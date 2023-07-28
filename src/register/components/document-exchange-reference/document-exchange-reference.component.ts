161fb6c67cce6dc385a11201288a6201bb276654
John Benjamin <john.benjamin@hmcts.net>
2023-07-28 09:59:10 +0100

    EUI-8670 Register other org dx reference input

---
+++ src/register/components/document-exchange-reference/document-exchange-reference.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-exchange-reference',
+  templateUrl: './document-exchange-reference.component.html'
+})
+export class DocumentExchangeReferenceComponent {
+}

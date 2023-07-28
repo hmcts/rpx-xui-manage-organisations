e3749b5786853b711283b670c6aa4c7adecd7b51
John Benjamin <john.benjamin@hmcts.net>
2023-07-28 11:27:36 +0100

    EUI-8670 Register other org dx reference input

--- src/register/components/index.ts
+++ src/register/components/index.ts
@@ -1,4 +1,5 @@
 import { CheckYourAnswersComponent } from './check-your-answers/check-your-answers.component';
+import { DocumentExchangeReferenceComponent } from './document-exchange-reference/document-exchange-reference.component';
 import { GovukDlListItemComponent } from './govuk-dl-list-item/govuk-dl-list-item.component';
 import { NotificationBannerComponent } from './notification-banner/notification-banner.component';
 import { SubmittedConfirmationComponent } from './submitted-confirmation/submitted-confirmation.component';
@@ -7,10 +8,12 @@ export const components: any[] = [
   CheckYourAnswersComponent,
   GovukDlListItemComponent,
   SubmittedConfirmationComponent,
-  NotificationBannerComponent
+  NotificationBannerComponent,
+  DocumentExchangeReferenceComponent
 ];
 
 export * from './check-your-answers/check-your-answers.component';
 export * from './govuk-dl-list-item/govuk-dl-list-item.component';
-export * from './submitted-confirmation/submitted-confirmation.component';
 export * from './notification-banner/notification-banner.component';
+export * from './submitted-confirmation/submitted-confirmation.component';
+

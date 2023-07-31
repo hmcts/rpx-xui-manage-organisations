bf5b25077d69786792d5a3c390a1038c87bd097d
Uday Denduluri <uday.denduluri1@hmcts.net>
2023-07-31 11:31:50 +0100

    Init and Destroy code

--- src/register-org/containers/register/register-org.component.ts
+++ src/register-org/containers/register/register-org.component.ts
@@ -1,23 +1,24 @@
-import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { RegisterOrgService } from '../../services/index';
+import { RegistrationData } from '../../models/registrationdata.model';
 
 @Component({
   selector: 'app-prd-register-component',
   templateUrl: './register-org.component.html'
 })
 
-export class RegisterComponent implements OnInit, OnDestroy, AfterViewInit {
-  constructor(private readonly router: Router) {}
+export class RegisterComponent implements OnInit, OnDestroy {
+  constructor(private readonly router: Router,
+    private readonly registerOrgService: RegisterOrgService) {}
+
+  private registrationData: RegistrationData;
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.registrationData = this.registerOrgService.getRegisterData();
   }
 
   ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
-  }
-
-  ngAfterViewInit(): void {
-    throw new Error('Method not implemented.');
+    this.registerOrgService.persistRegistrationData(this.registrationData);
   }
 }
--- src/register-org/services/register-org.service.ts
+++ src/register-org/services/register-org.service.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@angular/core';
 import { SessionStorageService } from '../../../src/shared/services/session-storage.service';
+import { RegistrationData } from '../models/registrationdata.model';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class RegisterOrgService {
-  private readonly registerOrgKey = 'Register-Org';
+  private readonly registrationDataKey = 'Registeration-Data';
   constructor(private readonly sessionStorageService: SessionStorageService) {}
 
-  public getRegisterOrg(key: string) : any {
-    const registerOrgStr = this.sessionStorageService.getItem(this.registerOrgKey);
+  public getRegisterData() : RegistrationData {
+    const registerOrgStr = this.sessionStorageService.getItem(this.registrationDataKey);
     if (registerOrgStr) {
-      const registerOrganisation = JSON.parse(registerOrgStr);
-      return registerOrganisation[key];
+      const registerOrganisation = JSON.parse(registerOrgStr) as RegistrationData;
+      return registerOrganisation;
     }
-    return null;
+    return { name: '' } as RegistrationData;
+  }
+
+  public persistRegistrationData(data: RegistrationData) {
+    this.sessionStorageService.setItem(this.registrationDataKey, JSON.stringify(data));
   }
 }

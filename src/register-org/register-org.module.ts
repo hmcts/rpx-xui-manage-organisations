eb374e2ac2a77849f24045ac9dfac49be3b049b8
Uday Denduluri <uday.denduluri1@hmcts.net>
2023-07-31 14:59:53 +0100

    register lint fix

--- src/register-org/register-org.module.ts
+++ src/register-org/register-org.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { registerRouting } from './register-org.routing';
 import * as fromServices from './services';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [

cc1dad21d986d152cfdefa1afc5664405b903387
John Benjamin <john.benjamin@hmcts.net>
2023-08-01 14:54:08 +0100

    Register other org - Integrate with master page

--- src/register/components/index.ts
+++ src/register/components/index.ts
@@ -1,8 +1,4 @@
-import { BeforeYouStartComponent } from './before-you-start/before-you-start.component';
 import { CheckYourAnswersComponent } from './check-your-answers/check-your-answers.component';
-import { CompanyHouseDetailsComponent } from './company-house-details/company-house-details.component';
-import { DocumentExchangeReferenceDetailsComponent } from './document-exchange-reference-details/document-exchange-reference-details.component';
-import { DocumentExchangeReferenceComponent } from './document-exchange-reference/document-exchange-reference.component';
 import { GovukDlListItemComponent } from './govuk-dl-list-item/govuk-dl-list-item.component';
 import { NotificationBannerComponent } from './notification-banner/notification-banner.component';
 import { SubmittedConfirmationComponent } from './submitted-confirmation/submitted-confirmation.component';
@@ -11,15 +7,10 @@ export const components: any[] = [
   CheckYourAnswersComponent,
   GovukDlListItemComponent,
   SubmittedConfirmationComponent,
-  NotificationBannerComponent,
-  BeforeYouStartComponent,
-  CompanyHouseDetailsComponent,
-  DocumentExchangeReferenceComponent,
-  DocumentExchangeReferenceDetailsComponent
+  NotificationBannerComponent
 ];
 
 export * from './check-your-answers/check-your-answers.component';
 export * from './govuk-dl-list-item/govuk-dl-list-item.component';
 export * from './notification-banner/notification-banner.component';
 export * from './submitted-confirmation/submitted-confirmation.component';
-
--- src/register/register.routing.ts
+++ src/register/register.routing.ts
@@ -1,10 +1,10 @@
 import { ModuleWithProviders } from '@angular/core';
 // routes
 import { RouterModule, Routes } from '@angular/router';
-import { BeforeYouStartComponent } from './components/before-you-start/before-you-start.component';
-import { CompanyHouseDetailsComponent } from './components/company-house-details/company-house-details.component';
-import { DocumentExchangeReferenceDetailsComponent } from './components/document-exchange-reference-details/document-exchange-reference-details.component';
-import { DocumentExchangeReferenceComponent } from './components/document-exchange-reference/document-exchange-reference.component';
+import { BeforeYouStartComponent } from '../register-org/components/before-you-start/before-you-start.component';
+import { CompanyHouseDetailsComponent } from '../register-org/components/company-house-details/company-house-details.component';
+import { DocumentExchangeReferenceDetailsComponent } from '../register-org/components/document-exchange-reference-details/document-exchange-reference-details.component';
+import { DocumentExchangeReferenceComponent } from '../register-org/components/document-exchange-reference/document-exchange-reference.component';
 import { SubmittedConfirmationComponent } from './components/submitted-confirmation/submitted-confirmation.component';
 import { RegisterComponent } from './containers';
 import { RegisterModule } from './register.module';
@@ -23,22 +23,6 @@ export const ROUTES: Routes = [
     path: 'confirmation',
     component: SubmittedConfirmationComponent
     // canActivate: [HealthCheckGuard]
-  },
-  {
-    path: 'before-you-start',
-    component: BeforeYouStartComponent
-  },
-  {
-    path: 'company-house-details',
-    component: CompanyHouseDetailsComponent
-  },
-  {
-    path: 'document-exchange-reference',
-    component: DocumentExchangeReferenceComponent
-  },
-  {
-    path: 'document-exchange-reference-details',
-    component: DocumentExchangeReferenceDetailsComponent
   }
 ];
 

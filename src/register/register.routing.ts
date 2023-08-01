1e7f5eb60b06ef78f44b3db52dd840530867353b
John Benjamin <john.benjamin@hmcts.net>
2023-08-01 16:44:18 +0100

    EUI-8662 Register other org dx number function

--- src/register/register.routing.ts
+++ src/register/register.routing.ts
@@ -1,10 +1,6 @@
 import { ModuleWithProviders } from '@angular/core';
 // routes
 import { RouterModule, Routes } from '@angular/router';
-import { BeforeYouStartComponent } from '../register-org/components/before-you-start/before-you-start.component';
-import { CompanyHouseDetailsComponent } from '../register-org/components/company-house-details/company-house-details.component';
-import { DocumentExchangeReferenceDetailsComponent } from '../register-org/components/document-exchange-reference-details/document-exchange-reference-details.component';
-import { DocumentExchangeReferenceComponent } from '../register-org/components/document-exchange-reference/document-exchange-reference.component';
 import { SubmittedConfirmationComponent } from './components/submitted-confirmation/submitted-confirmation.component';
 import { RegisterComponent } from './containers';
 import { RegisterModule } from './register.module';

d976fa9c23d6088c061c9cc74c0fd800da853a0f
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2023-08-16 15:49:30 +0100

    Add code to display and use registered address component

--- src/register-org/containers/registered-address/registered-address.component.ts
+++ src/register-org/containers/registered-address/registered-address.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RegisterComponent } from '../../containers/register/register-org.component';
 import { RegisterOrgService } from '../../services/register-org.service';
 
 @Component({
-  selector: 'app-company-house-details',
-  templateUrl: './company-house-details.component.html'
+  selector: 'app-registered-address',
+  templateUrl: './registered-address.component.html'
 })
-export class CompanyHouseDetailsComponent extends RegisterComponent implements OnInit, OnDestroy {
+export class RegisteredAddressComponent extends RegisterComponent implements OnInit, OnDestroy {
+
+  public formGroup = new FormGroup({});
+
+  public startedInternational = false;
+  public headingText = 'What is the registered address of your organisation?';
+
   constructor(public readonly router: Router,
     public readonly registerOrgService: RegisterOrgService,
   ) {
@@ -24,5 +31,10 @@ export class CompanyHouseDetailsComponent extends RegisterComponent implements O
 
   public onContinue(): void {
     this.router.navigate([this.registerOrgService.REGISTER_ORG_NEW_ROUTE, 'registered-address']);
+  }
+
+  public onInternationalModeStart(): void {
+    this.startedInternational = true;
+    this.headingText = 'Is this a UK address?';
   }
 }

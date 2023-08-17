0b30f04c8433653097c71cbe479459e1b92627de
Uday Denduluri <uday.denduluri1@hmcts.net>
2023-08-17 17:10:57 +0100

    8733

--- src/register-org/components/payment-by-account-details/payment-by-account-details.component.ts
+++ src/register-org/components/payment-by-account-details/payment-by-account-details.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RegisterComponent } from '../../containers/register/register-org.component';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-payment-by-account-details',
   templateUrl: './payment-by-account-details.component.html'
 })
-export class PaymentByAccountDetailsComponent {
+export class PaymentByAccountDetailsComponent extends RegisterComponent implements OnInit {
+  public contactDetailsFormGroup: FormGroup;
+  ngOnInit(): void {
+    super.ngOnInit();
+    this.contactDetailsFormGroup = new FormGroup({
+      pbaNumber: new FormControl(null),
+    });
+  }
 }

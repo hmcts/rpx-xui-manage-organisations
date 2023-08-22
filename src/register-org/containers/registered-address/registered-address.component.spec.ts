c0137f9f1ada8aec6742759218b3bfdec780ad30
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2023-08-22 13:47:19 +0100

    Finalise all display changes

---
+++ src/register-org/containers/registered-address/registered-address.component.spec.ts
@@ -0,0 +1,43 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { INTERNATIONAL_HEADING, POSTCODE_HEADING } from '../../constants/register-org-constants';
+import { RegisteredAddressComponent } from './registered-address.component';
+
+describe('RegisteredAddressComponent', () => {
+  let component: RegisteredAddressComponent;
+  let fixture: ComponentFixture<RegisteredAddressComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [RegisteredAddressComponent],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      providers: []
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegisteredAddressComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should start an international mode (radio buttons)', () => {
+    component.startedInternational = false;
+    component.headingText = POSTCODE_HEADING;
+
+    component.onInternationalModeStart();
+    fixture.detectChanges();
+
+    component.startedInternational = true;
+    component.headingText = INTERNATIONAL_HEADING;
+  });
+});
--- src/register-org/containers/registered-address/registered-address.component.ts
+++ src/register-org/containers/registered-address/registered-address.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { INTERNATIONAL_HEADING, POSTCODE_HEADING } from '../../constants/register-org-constants';
 import { RegisterComponent } from '../../containers/register/register-org.component';
 import { RegisterOrgService } from '../../services/register-org.service';
 
@@ -9,11 +10,9 @@ import { RegisterOrgService } from '../../services/register-org.service';
   templateUrl: './registered-address.component.html'
 })
 export class RegisteredAddressComponent extends RegisterComponent implements OnInit, OnDestroy {
-
   public formGroup = new FormGroup({});
-
   public startedInternational = false;
-  public headingText = 'What is the registered address of your organisation?';
+  public headingText = POSTCODE_HEADING;
 
   constructor(public readonly router: Router,
     public readonly registerOrgService: RegisterOrgService,
@@ -33,8 +32,12 @@ export class RegisteredAddressComponent extends RegisterComponent implements OnI
     this.router.navigate([this.registerOrgService.REGISTER_ORG_NEW_ROUTE, 'registered-address']);
   }
 
+  public onBack(): void {
+    this.router.navigate([this.registerOrgService.REGISTER_ORG_NEW_ROUTE, 'registered-address']);
+  }
+
   public onInternationalModeStart(): void {
     this.startedInternational = true;
-    this.headingText = 'Is this a UK address?';
+    this.headingText = INTERNATIONAL_HEADING;
   }
 }

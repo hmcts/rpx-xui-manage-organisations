923647219a651020e6b4926e981707317463a9db
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2023-08-30 14:59:07 +0100

    Update error-message.model.ts

--- src/shared/models/error-message.model.ts
+++ src/shared/models/error-message.model.ts
@@ -2,11 +2,4 @@ export interface ErrorMessage {
   title: string;
   description: string;
   fieldId?: string;
-  multiple?: boolean;
-  errors?: MultipleErrorMessage[];
-}
-
-export interface MultipleErrorMessage {
-  error: string;
-  name?: string;
 }

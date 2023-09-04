7b0efa9ed69a834b9620bb5b5950a7755926ef62
John Benjamin <john.benjamin@hmcts.net>
2023-09-04 17:36:49 +0100

    EUI-8728 Register-other-org - personal regulator

--- src/register-org/components/individual-registered-with-regulator-details/individual-registered-with-regulator-details.component.ts
+++ src/register-org/components/individual-registered-with-regulator-details/individual-registered-with-regulator-details.component.ts
@@ -1,58 +1,169 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { RegulatoryType } from '../../../register-org/models';
-import { RegulatoryOrganisationType } from '../../../shared/models/lovRefData.model';
+import { ErrorMessage } from '../../../shared/models/error-message.model';
 import { LovRefDataService } from '../../../shared/services/lov-ref-data.service';
 import { RegisterComponent } from '../../containers/register/register-org.component';
+import {
+  Regulator,
+  RegulatoryOrganisationType,
+  RegulatoryOrganisationTypeMessage,
+  RegulatoryType
+} from '../../models';
 import { RegisterOrgService } from '../../services/index';
 
 @Component({
   selector: 'app-individual-registered-with-regulator-details',
   templateUrl: './individual-registered-with-regulator-details.component.html'
 })
-export class IndividualRegisteredWithRegulatorDetailsComponent extends RegisterComponent implements OnInit {
-  public regulatorFormGroup: FormGroup;
-  public organisationTypes$: Observable<RegulatoryOrganisationType[]>;
-  public showRegistrationNumber: boolean;
-  public showRegulatorName: boolean;
-  public showAddRegulatorButton: boolean;
+export class IndividualRegisteredWithRegulatorDetailsComponent extends RegisterComponent implements OnInit, OnDestroy {
+  @ViewChild('mainContent') public mainContentElement: ElementRef;
 
-  constructor(private readonly lovRefDataService: LovRefDataService,
-      public readonly router: Router,
-      public readonly registerOrgService: RegisterOrgService) {
+  public readonly SELECT_A_VALUE = 'none';
+
+  public regulatorDetailsFormGroup: FormGroup;
+  public regulatorTypes$: Observable<RegulatoryOrganisationType[]>;
+  public validationErrors: ErrorMessage[] = [];
+
+  constructor(
+    private readonly lovRefDataService: LovRefDataService,
+    public readonly router: Router,
+    public readonly registerOrgService: RegisterOrgService
+  ) {
     super(router, registerOrgService);
   }
 
-  ngOnInit(): void {
-    this.regulatorFormGroup = new FormGroup({
-      organisationType: new FormControl(null),
-      otherOrganisationType: new FormControl(null)
-    });
-    this.regulatorFormGroup.get('otherOrganisationType').setValue('none');
-    this.organisationTypes$ = this.lovRefDataService.getRegulatoryOrganisationType();
+  public ngOnInit(): void {
+    super.ngOnInit();
+    this.regulatorTypes$ = this.lovRefDataService.getRegulatoryOrganisationTypes();
+    this.setFormControlValues();
   }
 
-  public onOptionSelected(value: string): void {
-    this.showRegulatorName = false;
-    this.showRegistrationNumber = false;
-    this.showAddRegulatorButton = false;
+  public ngOnDestroy(): void {
+    super.ngOnDestroy();
+  }
+
+  public onOptionSelected(value: string, formGroupIndex: number): void {
+    // Use the selection of regulator type to control which additional fields (regulatorName and
+    // organisationRegistrationNumber) are added to or removed from the FormGroup
+    const formGroup = this.regulators.controls[formGroupIndex] as FormGroup;
     switch (value) {
       case (RegulatoryType.Other): {
-        this.showRegulatorName = true;
-        this.showRegistrationNumber = true;
-        this.showAddRegulatorButton = true;
+        formGroup.addControl('regulatorName', new FormControl(formGroup.value.regulatorName, Validators.required));
+        formGroup.addControl('organisationRegistrationNumber', new FormControl(formGroup.value.organisationRegistrationNumber));
         break;
       }
       case (RegulatoryType.NotApplicable): {
-        this.showAddRegulatorButton = true;
+        formGroup.removeControl('regulatorName');
+        formGroup.removeControl('organisationRegistrationNumber');
         break;
       }
       default: {
-        this.showRegistrationNumber = true;
-        this.showAddRegulatorButton = true;
+        formGroup.removeControl('regulatorName');
+        formGroup.addControl('organisationRegistrationNumber', new FormControl(formGroup.value.organisationRegistrationNumber));
       }
     }
+  }
+
+  public setFormControlValues(): void {
+    if (this.registrationData.regulators?.length) {
+      this.regulatorDetailsFormGroup = new FormGroup({
+        regulators: new FormArray([])
+      });
+      this.registrationData.regulators.forEach((regulatoryOrganisationType) => {
+        switch (regulatoryOrganisationType.regulatorType) {
+          case (RegulatoryType.Other): {
+            this.regulators.push(new FormGroup({
+              regulatorType: new FormControl(regulatoryOrganisationType.regulatorType, Validators.required),
+              regulatorName: new FormControl(regulatoryOrganisationType.regulatorName, Validators.required),
+              organisationRegistrationNumber: new FormControl(regulatoryOrganisationType.organisationRegistrationNumber)
+            }));
+            break;
+          }
+          case (RegulatoryType.NotApplicable): {
+            this.regulators.push(new FormGroup({
+              regulatorType: new FormControl(regulatoryOrganisationType.regulatorType, Validators.required)
+            }));
+            break;
+          }
+          default: {
+            this.regulators.push(new FormGroup({
+              regulatorType: new FormControl(regulatoryOrganisationType.regulatorType, Validators.required),
+              organisationRegistrationNumber: new FormControl(regulatoryOrganisationType.organisationRegistrationNumber)
+            }));
+          }
+        }
+      });
+    } else {
+      this.regulatorDetailsFormGroup = new FormGroup({
+        regulators: new FormArray([
+          new FormGroup({
+            regulatorType: new FormControl(this.SELECT_A_VALUE, Validators.required)
+          })
+        ])
+      });
+    }
+  }
+
+  public get regulators(): FormArray {
+    return this.regulatorDetailsFormGroup.get('regulators') as FormArray;
+  }
+
+  public get regulatoryOrganisationTypeMessage(): typeof RegulatoryOrganisationTypeMessage {
+    return RegulatoryOrganisationTypeMessage;
+  }
+
+  public onAddNewBtnClicked(): void {
+    this.regulators.push(new FormGroup({
+      regulatorType: new FormControl(this.SELECT_A_VALUE, Validators.required)
+    }));
+  }
+
+  public showAddRegulatorButton(): boolean {
+    // Return true if an organisation type has been selected for every regulator entry displayed; this prevents the
+    // user from adding a new regulator before selecting an organisation type for the previous entry
+    return this.regulators.controls.every((formGroup) => !!formGroup.get('regulatorType').value && formGroup.get('regulatorType').value !== this.SELECT_A_VALUE);
+  }
+
+  public onContinueClicked(): void {
+    if (this.validateForm()) {
+      // Set corresponding registration data
+      this.registrationData.regulators = this.regulators.value as Regulator[];
+      this.router.navigate([this.registerOrgService.REGISTER_ORG_NEW_ROUTE, 'organisation-services-access']);
+    }
+  }
+
+  public fieldHasErrorMessage(fieldId: string): boolean {
+    return this.validationErrors.some((errorMessage) => errorMessage.fieldId === fieldId);
+  }
+
+  public onRemoveBtnClicked(index: number): void {
+    // TODO To be implemented in a future ticket
+  }
+
+  private validateForm(): boolean {
+    this.validationErrors = [];
+    this.regulators.controls.forEach((formGroup, index) => {
+      if (formGroup.get('regulatorType').errors) {
+        this.validationErrors.push({
+          description: RegulatoryOrganisationTypeMessage.NO_REGULATORY_ORG_SELECTED,
+          title: '',
+          fieldId: `regulator-type${index}`
+        });
+      }
+      if (formGroup.get('regulatorName') && formGroup.get('regulatorName').errors) {
+        this.validationErrors.push({
+          description: RegulatoryOrganisationTypeMessage.NO_REGULATOR_NAME,
+          title: '',
+          fieldId: `regulator-name${index}`
+        });
+      }
+    });
+    // Scroll to the error banner at the top of the screen if there are validation failures
+    if (this.validationErrors.length > 0) {
+      this.mainContentElement.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }
+    return this.validationErrors.length === 0;
   }
 }

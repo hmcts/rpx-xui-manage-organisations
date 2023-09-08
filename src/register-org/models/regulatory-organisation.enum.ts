e9518639b05888d78231044ecd664d65f7c4d6ed
John Benjamin <john.benjamin@hmcts.net>
2023-09-08 15:29:53 +0100

    EUI-8728 Register other org - personal regulator

--- src/register-org/models/regulatory-organisation.enum.ts
+++ src/register-org/models/regulatory-organisation.enum.ts
@@ -6,5 +6,6 @@ export enum RegulatoryType {
 export enum RegulatoryOrganisationTypeMessage {
   NO_REGULATORY_ORG_SELECTED = 'Please select a regulatory organisation',
   NO_REGULATOR_NAME = 'Please enter details for the regulator you have selected',
-  DUPLICATE_REGULATOR = 'You have already entered the details of this regulator'
+  NO_REGISTRATION_NUMBER = 'Enter a registration number',
+  DUPLICATE_REGULATOR = 'You have entered these registration details more than once'
 }

0bfd0a16d8f1c9c67ef25737f10b961adf0e4083
John Benjamin <john.benjamin@hmcts.net>
2023-09-11 11:41:17 +0100

    EUI-8784 Register other org - PBA number functionality

--- src/register-org/components/payment-by-account/payment-by-account.component.spec.ts
+++ src/register-org/components/payment-by-account/payment-by-account.component.spec.ts
@@ -1,7 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ErrorMessage } from '../../../shared/models/error-message.model';
 import { EnvironmentService } from '../../../shared/services/environment.service';
 import { RegistrationData } from '../../models/registrationdata.model';
 import { PaymentByAccountComponent } from './payment-by-account.component';
@@ -9,6 +9,10 @@ import { PaymentByAccountComponent } from './payment-by-account.component';
 describe('PaymentByAccountComponent', () => {
   let component: PaymentByAccountComponent;
   let fixture: ComponentFixture<PaymentByAccountComponent>;
+
+  const mockRouter = {
+    navigate: jasmine.createSpy('navigate')
+  };
 
   const registrationData: RegistrationData = {
     name: '',
@@ -32,7 +36,10 @@ describe('PaymentByAccountComponent', () => {
         HttpClientTestingModule,
         RouterTestingModule
       ],
-      providers: [EnvironmentService]
+      providers: [
+        EnvironmentService,
+        { provide: Router, useValue: mockRouter }
+      ]
     })
       .compileComponents();
   });
@@ -68,14 +75,32 @@ describe('PaymentByAccountComponent', () => {
         scrollIntoView: scrollIntoViewSpy
       }
     };
-    const errorMessage: ErrorMessage = {
-      description: 'Please select at least one option',
-      title: '',
-      fieldId: 'pba-yes'
+    const errorMessage = {
+      id: 'pba-yes',
+      message: 'Please select an option'
     };
     component.pbaFormGroup.get('pba').setValue(null);
     component.onContinue();
-    expect(component.pbaError).toEqual(errorMessage);
+    expect(component.validationErrors[0]).toEqual(errorMessage);
     expect(scrollIntoViewSpy).toHaveBeenCalled();
+  });
+
+  it('should continue button navigate to payment by account details page', () => {
+    component.pbaFormGroup.get('pba').setValue('yes');
+    component.onContinue();
+    expect(component.validationErrors.length).toEqual(0);
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['register-org-new', 'payment-by-account-details']);
+  });
+
+  it('should continue button navigate to contact details page', () => {
+    component.pbaFormGroup.get('pba').setValue('no');
+    component.onContinue();
+    expect(component.validationErrors.length).toEqual(0);
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['register-org-new', 'contact-details']);
+  });
+
+  it('should back button navigate to organisation services access page', () => {
+    component.onBack();
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['register-org-new', 'organisation-services-access']);
   });
 });

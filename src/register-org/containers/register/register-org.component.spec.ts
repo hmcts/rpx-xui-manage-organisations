c5cd500a58ddc790f78bd51c92374f5c97968f56
John Benjamin <john.benjamin@hmcts.net>
2023-09-12 11:25:53 +0100

    EUI-8768 Register other org - Cancel link functionality

--- src/register-org/containers/register/register-org.component.spec.ts
+++ src/register-org/containers/register/register-org.component.spec.ts
@@ -1,29 +1,56 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { RegistrationData } from '../../models/registrationdata.model';
 import { RegisterOrgService } from '../../services';
 import { RegisterComponent } from './register-org.component';
 
 fdescribe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
-  let mockRegisterOrgService: any;
+
+  const mockRegisterOrgService = jasmine.createSpyObj('RegisterOrgService', [
+    'getRegisterData',
+    'persistRegistrationData',
+    'removeRegistrationData'
+  ]);
+  mockRegisterOrgService.REGISTER_ORG_NEW_ROUTE = 'register-org-new';
 
   const mockRouter = {
     navigate: jasmine.createSpy('navigate')
   };
 
+  const registrationData = {
+    name: '',
+    hasDxReference: null,
+    dxNumber: null,
+    dxExchange: null,
+    services: [],
+    hasPBA: null,
+    contactDetails: null,
+    hasRegisteredWithRegulator: null,
+    companyHouseNumber: null,
+    address: null,
+    organisationType: null,
+    organisationNumber: null,
+    regulators: [],
+    regulatorRegisteredWith: null
+  } as RegistrationData;
+
   beforeEach(async() => {
-    mockRegisterOrgService = jasmine.createSpyObj('registerOrgService', ['removeRegistrationData']);
     mockRegisterOrgService.removeRegistrationData.and.callThrough();
+    mockRegisterOrgService.persistRegistrationData.and.callThrough();
+    mockRegisterOrgService.getRegisterData.and.returnValue(registrationData);
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
       imports: [
+        HttpClientTestingModule,
         RouterTestingModule
       ],
       providers: [
-        { provider: Router, useValue: mockRouter },
-        { provider: RegisterOrgService, useValue: mockRegisterOrgService }
+        { provide: Router, useValue: mockRouter },
+        { provide: RegisterOrgService, useValue: mockRegisterOrgService }
       ]
     })
       .compileComponents();
@@ -37,13 +64,17 @@ fdescribe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(mockRegisterOrgService.getRegisterData).toHaveBeenCalled();
+    expect(component.registrationData).toEqual(registrationData);
+  });
+
+  it('should invoke persist registration data when component is unloaded', () => {
+    component.ngOnDestroy();
+    expect(mockRegisterOrgService.persistRegistrationData).toHaveBeenCalled();
   });
 
   it('should cancelRegistrationJourney confirmed by the user', () => {
-    spyOn(window, 'confirm').and.callFake(() => {
-      return true;
-    });
-    spyOn(component.registerOrgService, 'removeRegistrationData').and.callThrough();
+    spyOn(window, 'confirm').and.returnValue(true);
     component.cancelRegistrationJourney();
     expect(component.registerOrgService.removeRegistrationData).toHaveBeenCalled();
     expect(mockRouter.navigate).toHaveBeenCalledWith(['register-org-new', 'register']);

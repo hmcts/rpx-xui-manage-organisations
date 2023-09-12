9aecb0da8c5684fca94b5be5bc60672ad25cb101
John Benjamin <john.benjamin@hmcts.net>
2023-09-12 16:31:05 +0100

    EUI-8768 Register other org - Cancel link functionality

--- src/register-org/containers/register/register-org.component.ts
+++ src/register-org/containers/register/register-org.component.ts
@@ -12,21 +12,35 @@ export class RegisterComponent implements OnInit, OnDestroy {
   constructor(public readonly router: Router,
     public readonly registerOrgService: RegisterOrgService) {}
 
+  private isRegistrationJourneyCancelled = false;
+
   public registrationData: RegistrationData;
 
   public ngOnInit(): void {
-    this.registrationData = this.registerOrgService.getRegisterData();
+    this.initialiseRegistrationJourney();
   }
 
   public ngOnDestroy(): void {
-    this.registerOrgService.persistRegistrationData(this.registrationData);
+    // Registration data in the session storage must have been deleted if the user cancelled the registration journey
+    // and the property "isRegistrationJourneyCancelled" must be true.
+    // Persisting registration data is required only if the user continues with the registration journey
+    // i.e. registration data already exists in the session storage.
+    if (!this.isRegistrationJourneyCancelled) {
+      this.registerOrgService.persistRegistrationData(this.registrationData);
+    }
   }
 
   public cancelRegistrationJourney(): void {
     const confirmed = window.confirm('Cancel would erase all the data you have entered and exit from the registration journey. Are you sure you want to continue?');
     if (confirmed) {
+      this.isRegistrationJourneyCancelled = true;
       this.registerOrgService.removeRegistrationData();
       this.router.navigate([this.registerOrgService.REGISTER_ORG_NEW_ROUTE, 'register']);
     }
+  }
+
+  private initialiseRegistrationJourney(): void {
+    this.isRegistrationJourneyCancelled = false;
+    this.registrationData = this.registerOrgService.getRegisterData();
   }
 }

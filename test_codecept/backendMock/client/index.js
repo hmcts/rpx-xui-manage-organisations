914f8fd23f8e1cac4c87134fd2cc1abbaffc7c9d
sreekanthpuligadda <52752138+sreekanthpuligadda@users.noreply.github.com>
2023-09-13 10:14:24 +0100

    functional tests tests protractor to codecept migration (#1091)
    * Squashed commit of the following:
    
    commit 1b668535afa2259f6f3ef924e355b7d627876b0e
    Author: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
    Date:   Sun Sep 10 22:50:03 2023 +0100
    
        updated api tests path from codecept migration
    
    commit ff2d5fd63640be86ef0ba0eb1140a05e0410d2c4
    Author: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
    Date:   Sun Sep 10 22:20:42 2023 +0100
    
        custom log message to report update
    
    commit b4847365415f97346da6ec2597cb039a6253e35a
    Author: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
    Date:   Sun Sep 10 22:18:17 2023 +0100
    
        reportLoggger message update
    
    commit 3d268259704051db7a5a450552e08e9dbbb9219b
    Author: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
    Date:   Sun Sep 10 20:16:09 2023 +0100
    
        e2e test enahnced for stability
    
    commit 85319fd6c3bc4e64ca1aec1eaa1e5ff235f05c0f
    Author: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
    Date:   Sun Sep 10 19:21:12 2023 +0100
    
        cocedept cucumber report lib added
    
    commit 15180141af9bb3fc75c46514a505084d7c324396
    Author: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
    Date:   Sun Sep 10 19:04:17 2023 +0100
    
        added missing lib
    
    commit 3ed48ee5b62b40c15471795d0a51c5227a0e5b9e
    Author: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
    Date:   Sun Sep 10 18:50:13 2023 +0100
    
        removed unwanted imports
    
    commit 179d8dd925f4455ed0ed96ad87f3ffb07e9beeef
    Author: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
    Date:   Sun Sep 10 18:30:36 2023 +0100
    
        CVE suppress
    
    commit fe8fb14e100b70b960653fca80e70624132e6d16
    Author: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
    Date:   Sun Sep 10 18:24:07 2023 +0100
    
        codeceptjs lib in yarn.lock
    
    commit 1a785ccdf6188e735d56b2af606df7f76d28aa6f
    Author: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
    Date:   Sun Sep 10 18:18:55 2023 +0100
    
        aded missing lib
    
    commit c68e48c98d9717c2b59e27d60640227207e045bb
    Author: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
    Date:   Sun Sep 10 18:05:33 2023 +0100
    
        updated nvmrc file name
    
    commit 9efa953b221c697846f0785de5c9431ea639c7fb
    Author: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
    Date:   Sun Sep 10 17:56:34 2023 +0100
    
        fixed type errors
    
    commit f8781ee52f0878904a1d6ee1e0e123b77eb027ff
    Author: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
    Date:   Sun Sep 10 17:45:27 2023 +0100
    
        codeceptjs enabled smoke, e2e and a11y tests
    
    commit 217fd7858d8d9405daa92a50f0050d98fbbaf418
    Author: sreekanthpuligadda <sreekanth.puligadda@hmcts.net>
    Date:   Fri Sep 8 12:59:13 2023 +0100
    
        codeceptjs migration initial setup
    
    * yarn lock update on codeceptjs dependent modules
    
    * functional test step update
    
    * CVE suppress
    
    * e2e step update to improve stability
    
    * enabled codecept framework tests in nightly tests

---
+++ test_codecept/backendMock/client/index.js
@@ -0,0 +1,69 @@
+
+const axios = require('axios');
+
+const axiosInstance = axios.create({
+  
+});
+
+class BackendMockClient{
+
+    constructor(){
+        this.baseUrl = 'http://localhost:8080/client/'
+;    }
+
+    async updateAuthSessionWithRoles(auth, roles){
+        return await axiosInstance.post(`${this.baseUrl}session/user/roles`,{
+            auth: auth,
+            roles: roles
+        })
+    }
+
+
+    async updateAuthSessionWithUserInfo(auth, userInfo) {
+        return await axiosInstance.post(`${this.baseUrl}session/user/info`, {
+            auth: auth,
+            userInfo: userInfo
+        })
+    }
+
+    async updateAuthSessionWithRoleAssignments(auth, roleAssignments) {
+        await this.setUserApiData(auth, "OnUserRoleAssignments", roleAssignments )
+        return await axiosInstance.post(`${this.baseUrl}session/user/roleAssignments`, {
+            auth: auth,
+            roleAssignments: roleAssignments
+        })
+    }
+
+    async setUserApiData(auth, apiMethod, response) {
+        return await axiosInstance.post(`${this.baseUrl}session/userApiData`, {
+            auth: auth,
+            apiMethod: apiMethod,
+            apiResponse: response
+        })
+    }
+
+
+    async getSessionRolesAndRoleAssignments(auth){
+        return await axiosInstance.post(`${this.baseUrl}session/getUserRolesAndRoleAssignments`, {
+            auth: auth
+        })
+    }
+
+    async getUserSesionData(auth) {
+        return await axiosInstance.post(`${this.baseUrl}session/user/sessionData`, {
+            auth: auth
+        })
+    }
+
+
+    async logMessage(mesage){
+        return await axiosInstance.post(`${this.baseUrl}session/logMessage`, {
+            message: mesage
+        })
+    }
+
+}
+
+
+module.exports = new BackendMockClient();
+

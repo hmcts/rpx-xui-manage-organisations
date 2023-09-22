60faedab80221806a81967678661a19426eb1f52
John Benjamin <john.benjamin@hmcts.net>
2023-09-22 13:37:25 +0100

    EUI-8821 Register Other Org - Service not listed

--- src/register-org/components/organisation-services-access/organisation-services-access.component.ts
+++ src/register-org/components/organisation-services-access/organisation-services-access.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ORGANISATION_SERVICES } from 'src/register-org/constants/register-org-constants';
 import { OrganisationService, OrganisationServicesMessage } from '../../../register-org/models';
@@ -14,7 +14,7 @@ export class OrganisationServicesAccessComponent extends RegisterComponent imple
   public readonly CATEGORY_SERVICE_ACCESS = 'Service';
   public readonly SERVICE_NOT_LISTED = 'ServiceNotListed';
   public servicesFormGroup: FormGroup;
-  public services: OrganisationService[] = [];
+  public services: OrganisationService[];
   public selectedServices: string[] = [];
   public validationErrors: { id: string, message: string }[] = [];
   public noServicesError: string;
@@ -33,7 +33,7 @@ export class OrganisationServicesAccessComponent extends RegisterComponent imple
     this.services = ORGANISATION_SERVICES;
 
     this.servicesFormGroup = new FormGroup({
-      services: new FormControl(null),
+      services: new FormArray([]),
       otherServices: new FormControl(null)
     });
 
@@ -46,20 +46,21 @@ export class OrganisationServicesAccessComponent extends RegisterComponent imple
 
   public onServicesSelectionChange(event: any): void {
     if (event.target.checked) {
-      this.services.find((service) => service.key === event.target.value).selected = true;
+      this.selectedServices.push(event.target.value);
     } else {
-      this.services.find((service) => service.key === event.target.value).selected = false;
+      const serviceIndex = this.selectedServices.findIndex((service) => service === event.target.value);
+      this.selectedServices.splice(serviceIndex, 1);
     }
-    this.showOtherServicesInput = event.target.checked && event.target.value === 'NONE';
+    this.showOtherServicesInput = this.selectedServices.includes('NONE');
   }
 
   public onContinue(): void {
     if (this.isFormValid()) {
-      this.selectedServices = this.services.filter((service) => service.selected && service.key !== 'NONE').map((service) => service.key);
+      this.services = [];
       // Set corresponding registration data
-      this.registrationData.services = this.selectedServices;
+      this.registrationData.services = this.selectedServices.filter((service) => service !== 'NONE');
 
-      this.registrationData.otherServices = this.services.find((service) => service.selected && service.key === 'NONE')
+      this.registrationData.otherServices = this.showOtherServicesInput
         ? this.servicesFormGroup.get('otherServices').value
         : null;
 
@@ -73,14 +74,14 @@ export class OrganisationServicesAccessComponent extends RegisterComponent imple
   }
 
   public setFormControlValues(): void {
+    this.selectedServices = this.registrationData.services;
     this.services.forEach((service) => {
-      if (this.registrationData.services.includes(service.key)) {
-        service.selected = true;
-      }
+      service.selected = this.selectedServices.includes(service.key);
     });
     if (this.registrationData.otherServices) {
-      this.services.find((service) => service.key === 'NONE').selected = true;
       this.showOtherServicesInput = true;
+      this.selectedServices.push('NONE');
+      this.services.find((service) => service.key === 'NONE').selected = true;
       this.servicesFormGroup.get('otherServices').setValue(this.registrationData.otherServices);
     }
   }
@@ -89,7 +90,7 @@ export class OrganisationServicesAccessComponent extends RegisterComponent imple
     this.validationErrors = [];
     this.noServicesError = null;
     this.otherServicesError = null;
-    if (!this.services.some((service) => service.selected) && !this.showOtherServicesInput) {
+    if (!this.selectedServices.length && !this.showOtherServicesInput) {
       this.validationErrors.push({
         id: this.services[0].key,
         message: OrganisationServicesMessage.NO_ORG_SERVICES

2b5b59349bffec3cfdd9b733a072542b05791b71
John Benjamin <john.benjamin@hmcts.net>
2023-10-23 11:08:46 +0100

    EUI-8928 ROO CYA page corrections

--- src/register-org/components/check-your-answers/check-your-answers.component.spec.ts
+++ src/register-org/components/check-your-answers/check-your-answers.component.spec.ts
@@ -2,13 +2,16 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { throwError } from 'rxjs';
 import { RegistrationData } from '../../models/registration-data.model';
+import { RegisterOrgService } from '../../services';
 import { CheckYourAnswersComponent } from './check-your-answers.component';
 
 describe('CheckYourAnswersComponent', () => {
   let component: CheckYourAnswersComponent;
   let fixture: ComponentFixture<CheckYourAnswersComponent>;
   let router: Router;
+  let mockRegisterOrgService: any;
 
   const registrationData: RegistrationData = {
     pbaNumbers: [],
@@ -17,7 +20,11 @@ describe('CheckYourAnswersComponent', () => {
     hasDxReference: null,
     dxNumber: null,
     dxExchange: null,
-    services: [],
+    services: [
+      'AAA7',
+      'ABA3'
+    ],
+    otherServices: 'test service',
     hasPBA: null,
     contactDetails: {
       firstName: 'John',
@@ -34,11 +41,16 @@ describe('CheckYourAnswersComponent', () => {
   };
 
   beforeEach(async () => {
+    mockRegisterOrgService = jasmine.createSpyObj<RegisterOrgService>('registerOrgService', ['getRegistrationData', 'persistRegistrationData', 'postRegistration']);
+    mockRegisterOrgService.getRegistrationData.and.returnValue(registrationData);
     await TestBed.configureTestingModule({
       declarations: [CheckYourAnswersComponent],
       imports: [
         HttpClientTestingModule,
         RouterTestingModule
+      ],
+      providers: [
+        { provide: RegisterOrgService, useValue: mockRegisterOrgService }
       ]
     }).compileComponents();
   });
@@ -60,6 +72,18 @@ describe('CheckYourAnswersComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should getOrganisationType return the correct organisation type from the ref data', () => {
+    const organisationType = component.getOrganisationType('SolicitorOrganisation');
+    expect(organisationType).toEqual('Solicitor Organisation');
+  });
+
+  it('should display error message banner for api error', () => {
+    mockRegisterOrgService.postRegistration.and.returnValue(throwError('error'));
+    component.cyaFormGroup.get('confirmTermsAndConditions').setValue(true);
+    component.onSubmitData();
+    expect(component.validationErrors).toEqual([{ id: 'confirm-terms-and-conditions', message: 'Sorry, there is a problem with the service. Try again later' }]);
+  });
+
   it('should back link navigate to the individual registered with the regulator details page', () => {
     component.registrationData.hasIndividualRegisteredWithRegulator = true;
     component.onBack();
@@ -70,5 +94,11 @@ describe('CheckYourAnswersComponent', () => {
     component.registrationData.hasIndividualRegisteredWithRegulator = false;
     component.onBack();
     expect(router.navigate).toHaveBeenCalledWith(['register-org-new', 'individual-registered-with-regulator']);
+  });
+
+  it('should invoke the cancel registration journey when clicked on cancel link', () => {
+    spyOn(component, 'cancelRegistrationJourney');
+    component.onCancel();
+    expect(component.cancelRegistrationJourney).toHaveBeenCalled();
   });
 });

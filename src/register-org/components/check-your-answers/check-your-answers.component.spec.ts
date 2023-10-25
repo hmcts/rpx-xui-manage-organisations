19c7382156038e2d6df9d77cbcbe403e5045bdbe
John Benjamin <john.benjamin@hmcts.net>
2023-10-25 14:52:56 +0100

    EUI-8939 ROO PBA changes on CYA page

--- src/register-org/components/check-your-answers/check-your-answers.component.spec.ts
+++ src/register-org/components/check-your-answers/check-your-answers.component.spec.ts
@@ -3,22 +3,27 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { RegistrationData } from '../../models/registration-data.model';
+import { RegisterOrgService } from '../../services';
 import { CheckYourAnswersComponent } from './check-your-answers.component';
 
 describe('CheckYourAnswersComponent', () => {
   let component: CheckYourAnswersComponent;
   let fixture: ComponentFixture<CheckYourAnswersComponent>;
   let router: Router;
+  let registerOrgService: RegisterOrgService;
 
   const registrationData: RegistrationData = {
-    pbaNumbers: [],
     companyName: 'Minstry of Justice',
     companyHouseNumber: '11223344',
     hasDxReference: null,
     dxNumber: null,
     dxExchange: null,
     services: [],
-    hasPBA: null,
+    hasPBA: true,
+    pbaNumbers: [
+      'PBA1234567',
+      'PBA1234568'
+    ],
     contactDetails: {
       firstName: 'John',
       lastName: 'Davis',
@@ -46,9 +51,10 @@ describe('CheckYourAnswersComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CheckYourAnswersComponent);
     component = fixture.componentInstance;
-    component.registrationData = registrationData;
     router = TestBed.inject(Router);
     spyOn(router, 'navigate');
+    registerOrgService = TestBed.inject(RegisterOrgService);
+    spyOn(registerOrgService, 'getRegistrationData').and.returnValue(registrationData);
     fixture.detectChanges();
   });
 
@@ -70,5 +76,17 @@ describe('CheckYourAnswersComponent', () => {
     component.registrationData.hasIndividualRegisteredWithRegulator = false;
     component.onBack();
     expect(router.navigate).toHaveBeenCalledWith(['register-org-new', 'individual-registered-with-regulator']);
+  });
+
+  it('should set the payment by account url to be the yes or no page', () => {
+    component.registrationData.hasPBA = false;
+    component.ngOnInit();
+    expect(component.pbaUrl).toEqual('/register-org-new/payment-by-account');
+  });
+
+  it('should set the payment by account url to be the details page', () => {
+    component.registrationData.hasPBA = true;
+    component.ngOnInit();
+    expect(component.pbaUrl).toEqual('/register-org-new/payment-by-account-details');
   });
 });

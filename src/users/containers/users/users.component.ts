337a867626914b2f51c8463501db44240e8e4c5c
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-12-07 14:04:12 +0000

    avoid the load users action on the users page to avoid duplicate api calls

--- src/users/containers/users/users.component.ts
+++ src/users/containers/users/users.component.ts
@@ -3,21 +3,22 @@ import {
   EventEmitter,
   OnDestroy,
   OnInit,
-  Output,
-} from "@angular/core";
-import { User } from "@hmcts/rpx-xui-common-lib";
-import { select, Store } from "@ngrx/store";
-import { Observable, Subscription } from "rxjs";
+  Output
+} from '@angular/core';
+import { User } from '@hmcts/rpx-xui-common-lib';
+import { select, Store } from '@ngrx/store';
+import { Observable, Subscription, of } from 'rxjs';
 // TODO: The below is an odd way to import.
-import { GovukTableColumnConfig } from "../../../../projects/gov-ui/src/lib/components/govuk-table/govuk-table.component";
-import { UsersService } from "../../../users/services";
+import { GovukTableColumnConfig } from '../../../../projects/gov-ui/src/lib/components/govuk-table/govuk-table.component';
+import { UsersService } from '../../../users/services';
 
-import * as fromStore from "../../store";
+import * as fromStore from '../../store';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
-  selector: "app-prd-users-component",
-  templateUrl: "./users.component.html",
-  styleUrls: ["./users.component.scss"],
+  selector: 'app-prd-users-component',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit, OnDestroy {
   @Output() public paginationEvent = new EventEmitter<number>();
@@ -45,15 +46,36 @@ export class UsersComponent implements OnInit, OnDestroy {
   }
 
   public loadUsers(pageNumber: number): void {
-    this.store.dispatch(new fromStore.LoadUsers(pageNumber));
-    this.tableUsersData$ = this.store.pipe(select(fromStore.getGetUserList));
+    // this.store.dispatch(new fromStore.LoadUsers(pageNumber));
+    // this.tableUsersData$ = this.store.pipe(select(fromStore.getGetUserList));
     this.isLoading$ = this.store.pipe(select(fromStore.getGetUserLoading));
   }
 
   public getAllUsers(): Subscription {
-    return this.usersService.getAllUsersList().subscribe((allUserList) => {
-      this.pageTotalSize = allUserList.users.length;
-    });
+    return this.usersService
+      .getAllUsersList()
+      .pipe(
+        tap((allUserList) => {
+          this.pageTotalSize = allUserList.users.length;
+        }),
+        map((allUsersList) => {
+          this.tableUsersData$ = of(
+            allUsersList.users.map((user) => {
+              const newUser: User = {
+                email: user.email,
+                fullName: `${user.firstName} ${user.lastName}`,
+                routerLink: `user/${user.userIdentifier}`,
+                routerLinkTitle: `User details for ${user.fullName} with id ${user.userIdentifier}`,
+                status: user.idamStatus
+              };
+              user.routerLink = `user/${user.userIdentifier}`;
+              user.routerLinkTitle = `User details for ${user.fullName} with id ${user.userIdentifier}`;
+              return newUser;
+            })
+          );
+        })
+      )
+      .subscribe();
   }
 
   public pageChange(pageNumber: number): void {

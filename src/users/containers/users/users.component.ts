156bfe6aed2c3fd90f868c79d64b556d1ac684e1
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-12-08 11:25:29 +0000

    remove unused import

--- src/users/containers/users/users.component.ts
+++ src/users/containers/users/users.component.ts
@@ -1,19 +1,12 @@
-import {
-  Component,
-  EventEmitter,
-  OnDestroy,
-  OnInit,
-  Output
-} from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { User } from '@hmcts/rpx-xui-common-lib';
 import { select, Store } from '@ngrx/store';
-import { Observable, Subscription, of } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 // TODO: The below is an odd way to import.
 import { GovukTableColumnConfig } from '../../../../projects/gov-ui/src/lib/components/govuk-table/govuk-table.component';
 import { UsersService } from '../../../users/services';
 
 import * as fromStore from '../../store';
-import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-prd-users-component',

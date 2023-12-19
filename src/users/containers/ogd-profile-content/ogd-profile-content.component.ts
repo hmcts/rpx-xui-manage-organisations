c753351c0aba62ebb68f005055b1eca81f814e8f
Ben James <ben.james@hmcts.net>
2023-12-19 15:58:52 +0000

    removed scss files for new components as not required

--- src/users/containers/ogd-profile-content/ogd-profile-content.component.ts
+++ src/users/containers/ogd-profile-content/ogd-profile-content.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ogd-profile-content',
-  templateUrl: './ogd-profile-content.component.html',
-  styleUrls: ['./ogd-profile-content.component.scss']
+  templateUrl: './ogd-profile-content.component.html'
 })
 export class OgdProfileContentComponent {
 

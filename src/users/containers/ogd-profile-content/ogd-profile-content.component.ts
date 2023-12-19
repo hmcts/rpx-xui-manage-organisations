3b8af3653f84515751e1103c46f0313016dd8a22
Ben James <ben.james@hmcts.net>
2023-12-19 11:56:23 +0000

    Generated new components for profile specific content

---
+++ src/users/containers/ogd-profile-content/ogd-profile-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ogd-profile-content',
+  templateUrl: './ogd-profile-content.component.html',
+  styleUrls: ['./ogd-profile-content.component.scss']
+})
+export class OgdProfileContentComponent {
+
+}
---
+++ src/users/containers/solicitor-profile-content/solicitor-profile-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitor-profile-content',
+  templateUrl: './solicitor-profile-content.component.html',
+  styleUrls: ['./solicitor-profile-content.component.scss']
+})
+export class SolicitorProfileContentComponent {
+
+}

0e95180efe7b9b25ecdc12aac5382f127759bb71
Josh <josh.glasgow@hmcts.net>
2024-01-15 09:18:52 +0000

    Add access types to invite user post

--- src/users/containers/manage-user/manage-user.component.ts
+++ src/users/containers/manage-user/manage-user.component.ts
@@ -15,6 +15,8 @@ import { jurisdictionsExample, userAccessTypesExample } from './temp-data';
 import { Jurisdiction } from '@hmcts/ccd-case-ui-toolkit';
 import { OrganisationDetails } from 'src/models';
 import { LoggerService } from 'src/shared/services/logger.service';
+import { AppConstants } from '../../../app/app.constants';
+import { GlobalError } from '../../../app/store/reducers/app.reducer';
 
 @Component({
   selector: 'app-manage-user',
@@ -25,6 +27,7 @@ export class ManageUserComponent implements OnInit, OnDestroy {
   public userId: string;
   public summaryErrors: { isFromValid: boolean; items: { id: string; message: any; }[]; header: string };
   public user: User;
+  public resendInvite: boolean = false;
 
   // TODO: remove this when the GA-62 is complete and replace with selector
   public jurisdictions = JSON.parse(jurisdictionsExample) as Jurisdiction[];
@@ -62,6 +65,33 @@ export class ManageUserComponent implements OnInit, OnDestroy {
     this.actions$.pipe(ofType(fromStore.EDIT_USER_SUCCESS)).subscribe(() => {
       this.routerStore.dispatch(new fromRoot.Go({ path: [`users/user/${this.userId}`] }));
     });
+
+    if (!this.userId){
+      console.log('setting up invite subscriptions.');
+      this.actions$.pipe(ofType(fromStore.INVITE_USER_FAIL_WITH_400), takeUntil(this.onDestory$)).subscribe(() => {
+        this.handleError(this.userStore, 400);
+        console.log('1');
+      });
+      this.actions$.pipe(ofType(fromStore.INVITE_USER_FAIL_WITH_404), takeUntil(this.onDestory$)).subscribe(() => {
+        this.handleError(this.userStore, 404);
+        console.log('2');
+      });
+      this.actions$.pipe(ofType(fromStore.INVITE_USER_FAIL_WITH_500), takeUntil(this.onDestory$)).subscribe(() => {
+        this.handleError(this.userStore, 500);
+        console.log('3');
+      });
+      this.actions$.pipe(ofType(fromStore.INVITE_USER_FAIL_WITH_429), takeUntil(this.onDestory$)).subscribe(() => {
+      // this.showWarningMessage = true;
+        console.log('4');
+      });
+      this.actions$.pipe(ofType(fromStore.INVITE_USER_FAIL_WITH_409), takeUntil(this.onDestory$)).subscribe(() => {
+      // this.showWarningMessage = true;
+        console.log('5');
+      });
+      this.actions$.pipe(ofType(fromStore.INVITE_USER_FAIL), takeUntil(this.onDestory$)).subscribe(() => {
+        this.routerStore.dispatch(new fromRoot.Go({ path: ['service-down'] }));
+      });
+    }
   }
 
   ngOnDestroy(): void {
@@ -113,8 +143,19 @@ export class ManageUserComponent implements OnInit, OnDestroy {
     }
   }
 
-  private inviteUser() {
-    // TODO: implement
+  private inviteUser(): void {
+    let value:any = {
+      ...this.updatedUser
+    };
+    if (value.roles.includes('pui-case-manager')) {
+      value.roles = [...value.roles, ...AppConstants.CCD_ROLES];
+    }
+    value = {
+      ...value,
+      resendInvite: this.resendInvite
+    };
+    console.log(value);
+    this.userStore.dispatch(new fromStore.SendInviteUser(value));
   }
 
   private updateUser() {
@@ -123,5 +164,69 @@ export class ManageUserComponent implements OnInit, OnDestroy {
 
   private getBackurl(userId: string): string {
     return !!userId ? `/users/user/${userId}` : '/users';
+  }
+
+  public handleError(store: Store<any>, errorNumber: number): void {
+    console.log('handling error' + errorNumber);
+    const globalError = this.getGlobalError(errorNumber);
+    if (globalError) {
+      store.dispatch(new fromRoot.AddGlobalError(globalError));
+      store.dispatch(new fromRoot.Go({ path: ['service-down'] }));
+    }
+  }
+
+  public getGlobalError(error: number): GlobalError {
+    console.log(error);
+    const errorMessages = this.getErrorMessages(error);
+    const globalError = {
+      header: this.getErrorHeader(error),
+      errors: errorMessages
+    };
+    return globalError;
+  }
+
+  private getErrorMessages(error: number) {
+    switch (error) {
+      case 400:
+        return [{
+          bodyText: 'to check the status of the user',
+          urlText: 'Refresh and go back',
+          url: '/users'
+        }];
+      case 404:
+        return [{
+          bodyText: 'to reactivate this account',
+          urlText: 'Get help',
+          url: '/get-help',
+          newTab: true
+        }, {
+          bodyText: null,
+          urlText: 'Go back to manage users',
+          url: '/users'
+        }];
+      case 500:
+      default:
+        return [{
+          bodyText: 'Try again later.',
+          urlText: null,
+          url: null
+        }, {
+          bodyText: null,
+          urlText: 'Go back to manage users',
+          url: '/users'
+        }];
+    }
+  }
+
+  private getErrorHeader(error: number): string {
+    switch (error) {
+      case 400:
+        return 'Sorry, there is a problem';
+      case 404:
+        return 'Sorry, there is a problem with this account';
+      case 500:
+      default:
+        return 'Sorry, there is a problem with the service';
+    }
   }
 }

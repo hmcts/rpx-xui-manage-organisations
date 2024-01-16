1f2dd70b77ff3386ef6291e54627baee2640d22a
Ben James <ben.james@hmcts.net>
2024-01-16 15:01:08 +0000

    Added initial test for update user on manage user component

--- src/users/containers/manage-user/manage-user.component.spec.ts
+++ src/users/containers/manage-user/manage-user.component.spec.ts
@@ -9,10 +9,11 @@ import * as fromRoot from '../../../app/store';
 import * as fromStore from '../../store';
 import * as fromOrgStore from '../../../organisation/store';
 import { MemoizedSelector } from '@ngrx/store';
-import { User } from '@hmcts/rpx-xui-common-lib';
+import { User, UserAccessType } from '@hmcts/rpx-xui-common-lib';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { LoggerService } from 'src/shared/services/logger.service';
 import { OrganisationDetails } from 'src/models';
+import { EditUserModel } from 'src/user-profile/models/editUser.model';
 
 describe('ManageUserComponent', () => {
   let component: ManageUserComponent;
@@ -59,7 +60,15 @@ describe('ManageUserComponent', () => {
       },
       navigationId: 0
     };
-    defaultUser = { email: 'john@doe.com', firstName: 'John', lastName: 'Doe', idamStatus: 'Active', idamStatusCode: 'A', roles: ['pui-case-manager', 'pui-user-manager'], id: '123' };
+    defaultUser = {
+      email: 'john@doe.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      idamStatus: 'Active',
+      idamStatusCode: 'A',
+      roles: ['pui-case-manager', 'pui-user-manager'],
+      id: '123'
+    };
     defaultOrganisationState = {
       name: 'Organisation Name',
       organisationProfileIds: ['SOLICITOR_PROFILE'],
@@ -78,6 +87,7 @@ describe('ManageUserComponent', () => {
       pendingAddPaymentAccount: [],
       pendingRemovePaymentAccount: []
     };
+
     mockGetRouterState = mockRouterStore.overrideSelector(fromRoot.getRouterState, defaultRouterStateUrl);
     mockGetSingleUserSelector = mockUserStore.overrideSelector(fromStore.getGetSingleUser, of(defaultUser));
     mockOrganisationStore.overrideSelector(fromOrgStore.getOrganisationSel, defaultOrganisationState);
@@ -104,5 +114,146 @@ describe('ManageUserComponent', () => {
       flushMicrotasks();
       expect(component.backUrl).toBe('/users/user/123');
     }));
+  });
+
+  fdescribe('Update User', () => {
+    const userWithAccessTypes: User = {
+      email: 'john@doe.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      idamStatus: 'Active',
+      idamStatusCode: 'A',
+      roles: ['pui-case-manager', 'pui-user-manager', 'pui-caa'],
+      id: '123',
+      accessTypes: [{
+        accessTypeId: '101',
+        jurisdictionId: '6',
+        organisationProfileId: 'SOLICITOR_PROFILE',
+        enabled: false
+      },
+      {
+        accessTypeId: '10',
+        jurisdictionId: '6',
+        organisationProfileId: 'SOLICITOR_PROFILE',
+        enabled: true
+      }]
+    };
+    const userWithUpdatedRoles: EditUserModel = {
+      email: 'john@doe.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      idamStatus: 'Active',
+      rolesAdd: [{ name: 'pui-finance-manager' }],
+      rolesDelete: [{ name: 'pui-user-manager' }],
+      id: '123',
+      accessTypes: [{
+        accessTypeId: '101',
+        jurisdictionId: '6',
+        organisationProfileId: 'SOLICITOR_PROFILE',
+        enabled: false
+      },
+      {
+        accessTypeId: '10',
+        jurisdictionId: '6',
+        organisationProfileId: 'SOLICITOR_PROFILE',
+        enabled: true
+      }]
+    };
+    /*
+    const userWithUpdatedAccessTypes: EditUserModel = {
+      email: 'john@doe.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      idamStatus: 'Active',
+      rolesAdd: [],
+      rolesDelete: [],
+      id: '123',
+      accessTypes: [{
+        accessTypeId: '101',
+        jurisdictionId: '6',
+        organisationProfileId: 'SOLICITOR_PROFILE',
+        enabled: true
+      },
+      {
+        accessTypeId: '10',
+        jurisdictionId: '6',
+        organisationProfileId: 'SOLICITOR_PROFILE',
+        enabled: false
+      }]
+    };
+    const accessTypesUpdated: UserAccessType[] = [{
+      accessTypeId: '101',
+      jurisdictionId: '6',
+      organisationProfileId: 'SOLICITOR_PROFILE',
+      enabled: true
+    },
+    {
+      accessTypeId: '10',
+      jurisdictionId: '6',
+      organisationProfileId: 'SOLICITOR_PROFILE',
+      enabled: false
+    }];
+    */
+
+    beforeEach(() => {
+      mockGetSingleUserSelector.setResult(userWithAccessTypes);
+      component.userId = userWithAccessTypes.id;
+      component.user = userWithAccessTypes;
+    });
+    it('should save updated user details with new roles', fakeAsync(() => {
+      component.onPersonalDetailsChange({
+        email: userWithAccessTypes.email,
+        firstName: userWithAccessTypes.firstName,
+        lastName: userWithAccessTypes.lastName
+      });
+
+      component.onSelectedCaseManagamentPermissionsChange({
+        manageCases: true,
+        userAccessTypes: userWithAccessTypes.accessTypes
+      });
+
+      component.onStandardUserPermissionsChange({
+        isCaseAccessAdmin: true,
+        isPuiFinanceManager: true, // Should be added
+        isPuiOrganisationManager: false,
+        isPuiUserManager: false // Should be removed
+      });
+
+      expect(component).toBeTruthy();
+      flushMicrotasks();
+      expect(component.backUrl).toBe('/users/user/123');
+
+      const dispatchSpy = spyOn(mockUserStore, 'dispatch');
+      component.onSubmit();
+      expect(dispatchSpy).toHaveBeenCalledWith(new fromStore.EditUser(userWithUpdatedRoles));
+    }));
+    /*
+    it('should save updated user details with new access types', fakeAsync(() => {
+      component.onPersonalDetailsChange({
+        email: userWithAccessTypes.email,
+        firstName: userWithAccessTypes.firstName,
+        lastName: userWithAccessTypes.lastName
+      });
+
+      component.onSelectedCaseManagamentPermissionsChange({
+        manageCases: true,
+        userAccessTypes: accessTypesUpdated // Amended access types
+      });
+
+      component.onStandardUserPermissionsChange({
+        isCaseAccessAdmin: true,
+        isPuiFinanceManager: false,
+        isPuiOrganisationManager: false,
+        isPuiUserManager: true
+      });
+
+      expect(component).toBeTruthy();
+      flushMicrotasks();
+      expect(component.backUrl).toBe('/users/user/123');
+
+      const dispatchSpy = spyOn(mockUserStore, 'dispatch');
+      component.onSubmit();
+      expect(dispatchSpy).toHaveBeenCalledWith(new fromStore.EditUser(userWithUpdatedAccessTypes));
+    })); */
   });
 });

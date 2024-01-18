2b0a74c856d8e3fd1ca4fe933aecbf4a6a67a68f
Ben James <ben.james@hmcts.net>
2024-01-18 14:56:26 +0000

    Added EditUserModel type to the editUser param to ensure all calls to method are correctly typed

--- src/user-profile/services/user.service.ts
+++ src/user-profile/services/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserInterface } from '../models/user.model';
+import { EditUserModel } from '../models/editUser.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ import { UserInterface } from '../models/user.model';
 export class UserService {
   constructor(private readonly http: HttpClient) {}
 
-  public editUserPermissions(editUser): Observable<any> {
+  public editUserPermissions(editUser: EditUserModel): Observable<any> {
     return this.http.put(`/api/editUserPermissions/users/${editUser.id}`, editUser);
   }
 

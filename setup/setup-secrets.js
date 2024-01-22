253a68ff0c5d7b8c0e73a284aa1f0b388f2d3a8d
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-01-22 12:48:55 +0000

    update script

--- setup/setup-secrets.js
+++ setup/setup-secrets.js
@@ -1,17 +1,17 @@
-const { platform } = require("os");
-const { execSync } = require("child_process");
-const { readFileSync, writeFileSync } = require("fs");
+const { platform } = require('os');
+const { execSync } = require('child_process');
+const { readFileSync, writeFileSync } = require('fs');
 
-const env = "aat";
+const env = 'aat';
 const configEnv = process.env.NODE_CONFIG_ENV;
 const configFile = `local-${configEnv}`;
 if (!env) {
-  throw new Error("Environment variable REMOTE_ENV is required");
+  throw new Error('Environment variable REMOTE_ENV is required');
 }
 
 function getSecret(targetEnv, secret) {
   let cmd = `az keyvault secret show --vault-name "rpx-${targetEnv}" -o tsv --query value --name ${secret}`;
-  if (platform() == "win32") {
+  if (platform() == 'win32') {
     cmd = `cmd /c ${cmd}`;
   }
 
@@ -20,16 +20,21 @@ function getSecret(targetEnv, secret) {
 
 // open a file called skeleton.json and read the contents as a string
 const currentDir = __dirname;
-const template = readFileSync(`${currentDir}/skeleton.json`).toString();
+let template = '';
+if (configEnv === 'docker') {
+  template = readFileSync(`${currentDir}/skeleton-docker.json`).toString();
+} else {
+  template = readFileSync(`${currentDir}/skeleton.json`).toString();
+}
 
 // replace the env placeholder with the actual env value
 const jsonConfig = JSON.parse(template.replace(/\$\{env\}/g, env));
 
 Object.keys(jsonConfig.secrets.rpx).forEach((key) => {
-  if (key.includes("redis-connection-string")) {
+  if (key.includes('redis-connection-string')) {
     jsonConfig.secrets.rpx[key] = getSecret(
       env,
-      "webapp-redis6-connection-string"
+      'webapp-redis6-connection-string'
     );
   } else {
     jsonConfig.secrets.rpx[key] = getSecret(env, key);

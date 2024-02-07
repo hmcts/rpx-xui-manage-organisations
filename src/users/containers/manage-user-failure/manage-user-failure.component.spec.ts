7850746e9dcec8c865246c688af7286addf797b2
Ben James <ben.james@hmcts.net>
2024-02-07 09:31:13 +0000

    Additional code to fix unit tests

--- src/users/containers/manage-user-failure/manage-user-failure.component.spec.ts
+++ src/users/containers/manage-user-failure/manage-user-failure.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ManageUserFailureComponent } from './manage-user-failure.component';
+import { provideMockStore } from '@ngrx/store/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ManageUserFailureComponent', () => {
   let component: ManageUserFailureComponent;
@@ -8,6 +9,10 @@ describe('ManageUserFailureComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        provideMockStore()
+      ],
+      imports: [RouterTestingModule],
       declarations: [ManageUserFailureComponent]
     })
       .compileComponents();

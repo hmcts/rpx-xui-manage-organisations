414e2dc95599fefd784ea76a329e0e6f28b7cb0e
Josh <josh.glasgow@hmcts.net>
2024-02-07 10:38:42 +0000

    update access-type api to remove dummy data and point to backend

--- api/retrieveAccessTypes/index.ts
+++ api/retrieveAccessTypes/index.ts
@@ -1,35 +1,30 @@
 import { Request, Response, Router } from 'express';
 import { getConfigValue } from '../configuration';
-import { SERVICES_RD_PROFESSIONAL_API_PATH } from '../configuration/references';
+import { SERVICES_CCD_DATA_STORE_API_PATH } from '../configuration/references';
 import * as log4jui from '../lib/log4jui';
 import { exists, valueOrNull } from '../lib/util';
-import { getRefdataAllUserListUrl } from '../refdataAllUserListUrlUtil';
-import { jurisdictionsExample } from '../temp-data';
 
 const logger = log4jui.getLogger('retrive-access-types');
 
 export async function handleRetriveAccessTypes(req: Request, res: Response) {
   const payload = req.body;
-  //   try {
-  //     const rdProfessionalApiPath = getConfigValue(SERVICES_RD_PROFESSIONAL_API_PATH);
-  //     const url = `${rdProfessionalApiPath}/retrieve-access-types`;
-  //     logger.info('INVITE USER: request URL:: ', url);
-  //     logger.info('INVITE USER: payload:: ', payload);
-  //     const response = await req.http.post(url, payload);
-  //     logger.info('response::', response.data);
-  //     res.send(response.data);
-  //   } catch (error) {
-  //     logger.error('error', error);
-  //     const status = exists(error, 'status') ? error.status : 500;
-  //     const errReport = {
-  //       apiError: valueOrNull(error, 'data.errorMessage'),
-  //       apiStatusCode: status,
-  //       message: valueOrNull(error, 'data.errorDescription')
-  //     };
-  //     res.status(status).send(errReport);
-  //   }
-
-  res.json(JSON.parse(jurisdictionsExample));
+  try {
+    const ccdDataStore = getConfigValue(SERVICES_CCD_DATA_STORE_API_PATH);
+    const url = `${ccdDataStore}/retrieve-access-types`;
+    logger.info('RETRIEVE ACCESS TYPES: request URL:: ', url);
+    const response = await req.http.post(url, payload);
+    logger.info('response::', response.data.jurisdictions);
+    res.send(response.data.jurisdictions);
+  } catch (error) {
+    logger.error(error);
+    const status = exists(error, 'status') ? error.status : 500;
+    const errReport = {
+      apiError: valueOrNull(error, 'data.errorMessage'),
+      apiStatusCode: status,
+      message: valueOrNull(error, 'data.errorDescription')
+    };
+    res.status(status).send(errReport);
+  }
 }
 
 export const router = Router({ mergeParams: true });

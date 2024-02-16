6e89156c56e3d5340b69230f73bdecd7edd41279
OgunyemiO <ola.ogunyemi@hmcts.net>
2024-02-16 12:45:20 +0000

    buld failure integration test fix

--- test_codecept/integration/tests/get_Organisation_User_Details.ts
+++ test_codecept/integration/tests/get_Organisation_User_Details.ts
@@ -4,12 +4,13 @@ const should = require('chai').should();
 suite('Manage Org -> Get Organisation User details', function() {
   this.timeout(50000);
   test('GET Manage Organisation User details', () => generateAPIRequest('GET', '/api/allUserList')
-  // console.log('response', response.headers.get('cache-control'))
+ 
+    // console.log('response', response.headers.get('cache-control'))
     .then((response) => {
       response.status.should.be.eql(200);
-      const user = response.data.users.findIndex((idamUser) => idamUser.email ==='xuiapitestuser@mailnesia.com');
-      response.data.users[user].lastName.should.be.eql('Lee');
-      response.data.users[user].firstName.should.be.eql('Jason');
+      const user = response.data.users.findIndex((idamUser) => idamUser.email ==='probate.aat.manage.org2@gmail.com');
+      response.data.users[user].lastName.should.be.eql('Org2');
+      response.data.users[user].firstName.should.be.eql('Probate');
       response.data.users[user].idamStatus.should.be.eql('ACTIVE');
     }));
 });

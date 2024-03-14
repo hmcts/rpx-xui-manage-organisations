dcd772e322495cf9fb929707d1b522d66c0d1834
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-03-14 17:06:35 +0000

    WIP cases container component

--- src/caa-cases/containers/cases/cases.component.ts
+++ src/caa-cases/containers/cases/cases.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CaaCasesService } from 'src/caa-cases/services';
+
+import * as organisationStore from '../../../organisation/store';
+import * as userStore from '../../../users/store';
+import * as caaCasesStore from '../../store';
+import { Store, select } from '@ngrx/store';
+import { OrganisationDetails } from 'src/models/organisation.model';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { ErrorMessage } from 'src/shared/models/error-message.model';
 
 @Component({
   selector: 'app-cases',
   templateUrl: './cases.component.html',
   styleUrls: ['./cases.component.scss']
 })
-export class CasesComponent {
+export class CasesComponent implements OnInit {
+  public selectedOrganisation$: Observable<OrganisationDetails>;
 
+  public pageTitle = 'Cases';
+  public showFilterSection = false;
+  public errorMessages: ErrorMessage[];
+
+  constructor(private readonly caaCasesStore: Store<caaCasesStore.CaaCasesState>,
+    private readonly organisationStore: Store<organisationStore.OrganisationState>,
+    private readonly userStore: Store<userStore.UserState>,
+    private readonly router: Router,
+    private readonly service: CaaCasesService) {
+  }
+
+  public ngOnInit(): void {
+    // Load selected organisation details from store
+    this.organisationStore.dispatch(new organisationStore.LoadOrganisation());
+    this.selectedOrganisation$ = this.organisationStore.pipe(select(organisationStore.getOrganisationSel));
+  }
+
+  public toggleFilterSection(): void {
+    this.showFilterSection = !this.showFilterSection;
+  }
+
+  public isAnyError(): boolean {
+    return Array.isArray(this.errorMessages) && this.errorMessages.length > 0;
+  }
 }

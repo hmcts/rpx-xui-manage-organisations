253b2ae714a9b1b63fc93e9addfff19c66dd8584
TomForniCGI <Tom.Forni@HMCTS.NET>
2024-03-15 17:13:01 +0000

    Session storage changes

--- src/shared/services/session-storage.service.ts
+++ src/shared/services/session-storage.service.ts
@@ -6,9 +6,17 @@ import { Injectable } from '@angular/core';
 export class SessionStorageService {
   /**
    * Get an item from the session storage.
+   * If remove is true, the item will be removed once read
+   * @param removeAfterRead removed the key once it has been read
    */
-  public getItem(key: string): string {
-    return sessionStorage.getItem(key);
+  public getItem(key: string, removeAfterRead: boolean = false): string {
+    const item = sessionStorage.getItem(key);
+
+    if (removeAfterRead) {
+      this.removeItem(key);
+    }
+
+    return item;
   }
 
   /**
@@ -19,9 +27,16 @@ export class SessionStorageService {
   }
 
   /**
-   * Remove an item from the session storage.
+   * Remove an item in the session storage.
    */
   public removeItem(key: string): void {
     sessionStorage.removeItem(key);
+  }
+
+  /**
+   * Clear all the items held in session storage.
+   */
+  public clear(): void {
+    sessionStorage.clear();
   }
 }

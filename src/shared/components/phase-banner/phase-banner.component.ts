6be7949ab7df16d6c12810aab936d66d6ec70fac
TomForniCGI <Tom.Forni@HMCTS.NET>
2024-03-15 10:24:04 +0000

    Initial Commit

--- src/shared/components/phase-banner/phase-banner.component.ts
+++ src/shared/components/phase-banner/phase-banner.component.ts
@@ -1,9 +1,37 @@
 import { Component, Input } from '@angular/core';
+import { RpxLanguage, RpxTranslationService } from 'rpx-xui-translation';
+import { SessionStorageService } from 'src/shared/services/session-storage.service';
 
 @Component({
-  selector: 'app-phase-banner',
+  selector: 'exui-phase-banner',
   templateUrl: './phase-banner.component.html'
 })
 export class PhaseBannerComponent {
   @Input() public type: string;
+  public noBanner: boolean;
+  private readonly noBannerSessionKey = 'noBanner';
+
+  public get currentLang() {
+    return this.langService.language;
+  }
+
+  constructor(private readonly langService: RpxTranslationService,
+              private readonly sessionStorageService: SessionStorageService) { }
+
+  public ngOnInit(): void {
+    this.noBanner = (this.currentLang === 'cy' ? true : false);
+    if (!this.noBanner) {
+      this.noBanner = JSON.parse(this.sessionStorageService.getItem(this.noBannerSessionKey));
+    }
+  }
+
+  public toggleLanguage(lang: RpxLanguage): void {
+    this.noBanner = (lang === 'cy');
+    this.langService.language = lang;
+  }
+
+  public closeBanner() {
+    this.noBanner = false;
+    this.sessionStorageService.setItem(this.noBannerSessionKey, 'false');
+  }
 }

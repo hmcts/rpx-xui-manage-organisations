672d6f7ea641f1b58e0e84c2e46783d7938cfd29
Ben James <ben.james@hmcts.net>
2024-03-18 11:52:33 +0000

    Updated manage user test payload to match what is expected

--- test_codecept/integration/tests/post_Manage_User.ts
+++ test_codecept/integration/tests/post_Manage_User.ts
@@ -4,30 +4,32 @@ const should = require('chai').should();
 suite('Manage Org -> POST Manage User', function() {
   this.timeout(50000);
   const payload = {
-    email: 'john_AT@doe.com',
-    firstName: 'John',
-    lastName: 'Doe',
-    idamStatus: 'Active',
-    idamStatusCode: 'A',
-    roles: ['pui-case-manager', 'pui-user-manager', 'pui-caa'],
-    id: '123',
-    userAccessTypes: [
-      {
-        accessTypeId: '10',
-        jurisdictionId: '6',
-        organisationProfileId: 'SOLICITOR_PROFILE',
-        enabled: true
-      },
-      {
-        accessTypeId: '101',
-        jurisdictionId: '6',
-        organisationProfileId: 'SOLICITOR_PROFILE',
-        enabled: false
-      }
-    ]
+    orgIdsPayload: ['SOLICITOR_PROFILE'],
+    userPayload: {
+      email: 'xuiapiorganisation@mailnesia.com',
+      firstName: 'Jason',
+      lastName: 'Lee',
+      idamStatus: 'Active',
+      idamStatusCode: 'A',
+      roles: ['pui-case-manager', 'pui-user-manager', 'pui-caa'],
+      id: '123',
+      userAccessTypes: [
+        {
+          accessTypeId: '10',
+          jurisdictionId: '6',
+          organisationProfileId: 'SOLICITOR_PROFILE',
+          enabled: true
+        },
+        {
+          accessTypeId: '101',
+          jurisdictionId: '6',
+          organisationProfileId: 'SOLICITOR_PROFILE',
+          enabled: false
+        }
+      ]}
   };
 
-  test('POST Manage User', () => generatePOSTAPIRequest('PUT', `/api/editUserPermissions/users/${payload.id}`, payload)
+  test('POST Manage User', () => generatePOSTAPIRequest('PUT', `/api/ogd-flow/update/${payload.userPayload.id}`, payload)
   // console.log('response', response.headers.get('cache-control'))
     .then((response) => {
       response.status.should.be.eql(200);
--- test_codecept/nodeMock/reqResMapping.js
+++ test_codecept/nodeMock/reqResMapping.js
@@ -67,6 +67,16 @@ const requestMapping = {
           'idamMessage': '11 OK'
         }
       });
+    },
+    '/api/ogd-flow/update/:id': (req, res) => {
+      res.send({
+        'roleAdditionResponse': null,
+        'roleDeletionResponse': null,
+        'statusUpdateResponse': {
+          'idamStatusCode': '200',
+          'idamMessage': '11 OK'
+        }
+      });
     }
 
   },

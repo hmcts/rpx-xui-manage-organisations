c0fdf0fef94a416f33e507558c3d86a0a1b1b588
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-03-18 23:27:38 +0000

    WIP: create a results table component to display case data

--- src/caa-cases/containers/cases/cases.component.ts
+++ src/caa-cases/containers/cases/cases.component.ts
@@ -9,10 +9,12 @@ import { OrganisationDetails } from 'src/models/organisation.model';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { ErrorMessage } from 'src/shared/models/error-message.model';
-import { User } from '@hmcts/rpx-xui-common-lib';
+import { SharedCase, SubNavigation, User } from '@hmcts/rpx-xui-common-lib';
 import { SelectedCaseFilter } from 'src/caa-cases/models/selected-case-filter.model';
-import { CaaCasesSessionState, CaaCasesSessionStateValue } from 'src/caa-cases/models/caa-cases.model';
-import { CaaCasesFilterType } from 'src/caa-cases/models/caa-cases.enum';
+import { CaaCases, CaaCasesSessionState, CaaCasesSessionStateValue } from 'src/caa-cases/models/caa-cases.model';
+import { CaaCasesFilterType, CaaCasesPageType } from 'src/caa-cases/models/caa-cases.enum';
+import { HttpErrorResponse } from '@angular/common/http';
+import * as converters from '../../converters/case-converter';
 
 @Component({
   selector: 'app-cases',
@@ -30,6 +32,16 @@ export class CasesComponent implements OnInit {
   public errorMessages: ErrorMessage[] = [];
   public sessionStateValue: CaaCasesSessionStateValue;
 
+  // for the results table
+  public allCaseTypes: SubNavigation[];
+  public currentPageNo: number;
+  public paginationPageSize: number = 25;
+  public casesConfig: CaaCases;
+  public cases: any; // can we type this?
+  public casesError$: Observable<HttpErrorResponse>;
+  public shareAssignedCases$: Observable<SharedCase[]>;
+  public shareUnassignedCases$: Observable<SharedCase[]>;
+
   constructor(private readonly caaCasesStore: Store<caaCasesStore.CaaCasesState>,
     private readonly organisationStore: Store<organisationStore.OrganisationState>,
     private readonly userStore: Store<userStore.UserState>,
@@ -38,6 +50,9 @@ export class CasesComponent implements OnInit {
   }
 
   public ngOnInit(): void {
+    // this.loadCaseTypes(selectedFilterType, selectedFilterValue);
+    this.loadCaseTypes();
+
     // Load selected organisation details from store
     this.organisationStore.dispatch(new organisationStore.LoadOrganisation());
     this.selectedOrganisation$ = this.organisationStore.pipe(select(organisationStore.getOrganisationSel));
@@ -48,6 +63,61 @@ export class CasesComponent implements OnInit {
 
     // Retrieve session state to check and pre-populate the previous state if any
     this.retrieveSessionState();
+
+    // TODO: clean this up to get all cases
+    this.caaCasesStore.pipe(select(caaCasesStore.getAllUnassignedCases)).subscribe((config: CaaCases) => {
+      if (config){
+        this.casesConfig = config;
+      }
+    });
+    this.caaCasesStore.pipe(select(caaCasesStore.getAllUnassignedCaseData)).subscribe((items) => {
+      if (items){
+        this.cases = items;
+      }
+    });
+    this.casesError$ = this.caaCasesStore.pipe(select(caaCasesStore.getAllUnassignedCasesError));
+
+    this.shareAssignedCases$ = this.caaCasesStore.pipe(select(caaCasesStore.getShareAssignedCaseListState));
+    this.shareUnassignedCases$ = this.caaCasesStore.pipe(select(caaCasesStore.getShareUnassignedCaseListState));
+  }
+
+  /**
+   * This will load all case types based on the selected filter type and value
+   */
+  public loadCaseTypes() {
+    // TODO: get selected filter type and selected filter value from somewhere
+    const selectedFilterType = CaaCasesPageType.UnassignedCases; // todo: this will be all cases since it's a merge of both assigned and unassigned cases
+    const pageType = CaaCasesPageType.UnassignedCases; // todo: this will be all cases since it's a merge of both assigned and unassigned cases
+    const selectedFilterValue = null;
+    // Load case types based on current page type according to filtered value
+    this.caaCasesStore.dispatch(new caaCasesStore.LoadCaseTypes({
+      caaCasesPageType: pageType,
+      caaCasesFilterType: selectedFilterType,
+      caaCasesFilterValue: selectedFilterValue })
+    );
+    this.caaCasesStore.pipe(select(caaCasesStore.getAllCaseTypes)).subscribe((items) => {
+      this.allCaseTypes = items;
+      this.loadCases();
+    });
+  }
+
+  /**
+   * This will load cases (i.e. unassigned or assigned) from the API with the selected filter type and value
+   */
+  public loadCases(){
+    if (this.allCaseTypes && this.allCaseTypes.length > 0) {
+      const selectedFilterType = CaaCasesFilterType.None; // todo: this will be all cases since it's a merge of both assigned and unassigned cases
+      const selectedFilterValue = null;
+      const currentCaseType = this.allCaseTypes[0].text;
+
+      this.caaCasesStore.dispatch(new caaCasesStore.LoadUnassignedCases({
+        caseType: currentCaseType,
+        pageNo: this.currentPageNo,
+        pageSize: this.paginationPageSize,
+        caaCasesFilterType: selectedFilterType,
+        caaCasesFilterValue: selectedFilterValue
+      }));
+    }
   }
 
   public onSelectedFilter(selectedFilter: SelectedCaseFilter): void {
@@ -59,7 +129,7 @@ export class CasesComponent implements OnInit {
       this.storeSessionState(selectedFilter);
     }
 
-    // load cases types based on fileter and value
+    // load cases types based on filter and value
   }
 
   public onErrorMessages(errorMessages: ErrorMessage[]): void {
@@ -96,5 +166,30 @@ export class CasesComponent implements OnInit {
     };
     this.sessionStateValue = sessionStateToUpdate.value;
     this.service.storeSessionState(sessionStateToUpdate);
+  }
+
+  public onCaseSelected(selectedCases: any[]): void {
+    // if (this.caaCasesPageType === CaaCasesPageType.UnassignedCases) {
+    //   this.selectedUnassignedCases = selectedCases;
+    //   this.store.dispatch(new fromStore.SynchronizeStateToStoreUnassignedCases(
+    //     converters.toShareCaseConverter(selectedCases, this.currentCaseType)
+    //   ));
+    // } else {
+    //   this.selectedAssignedCases = selectedCases;
+    //   this.store.dispatch(new fromStore.SynchronizeStateToStoreAssignedCases(
+    //     converters.toShareCaseConverter(selectedCases, this.currentCaseType)
+    //   ));
+    // }
+
+    // do i need the line below?
+    // this.selectedUnassignedCases = selectedCases;
+    this.caaCasesStore.dispatch(new caaCasesStore.SynchronizeStateToStoreUnassignedCases(
+      converters.toShareCaseConverter(selectedCases, CaaCasesPageType.UnassignedCases)
+    ));
+  }
+
+  public onPageChanged(pageNo: number): void {
+    this.currentPageNo = pageNo;
+    this.loadCases();
   }
 }

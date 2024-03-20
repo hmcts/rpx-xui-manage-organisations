9cd6dff36db8f439a82d34b355dd2cdeda0ead4d
Ben James <ben.james@hmcts.net>
2024-03-20 10:29:36 +0000

    Reverted previous change after discussion with Josh

--- api/ogd/index.ts
+++ api/ogd/index.ts
@@ -34,8 +34,7 @@ export async function ogdUpdate(req: Request, res: Response) {
     const userId = req.params.userId;
     const compareResult = await compareAccessTypes(req);
     req.body = { ...userPayload, ...compareResult };
-    // BJ-TODO: Check if this has been tested since Josh made the change. I believe rd-prof-api only expects the userPayload.
-    const operationResult = await ogdEditUserRoute(req.body.userPayload);
+    const operationResult = await ogdEditUserRoute(req);
     req.body = { userId };
     await refreshUser(req);
     res.send(operationResult);

565d610fc181a3b6f8880737983ab070742dfa53
Ben James <ben.james@hmcts.net>
2024-03-22 16:22:56 +0000

    Reverted edit permission tests to the originals. Added @Ignore to new tests with additional comments for when they can be enabled

--- test_codecept/e2e/features/app/editPermissions.feature
+++ test_codecept/e2e/features/app/editPermissions.feature
@@ -1,29 +1,42 @@
 @fullFunctional
 Feature: edit permissions and suspend user workflow
 
+  # Each test below has a comment explaining the current situation
+  # Tests will need to be commented/uncommented in line with backend api releases
+
   Background:
     When I navigate to manage organisation Url
     Given I am logged into Townley Services Org
     Then I should be redirected to manage organisation dashboard page
 
+  # TEST 1: This test has been reverted to the original
   @Flaky
+  Scenario: Verify the Edit permission and Suspend User showing for Active Users
+    When I click on user button
+    Then I should be on display the user details
+    Then I click on First Active User
+    Then I see change link and suspend button
+
+  # TEST 2: This test has been reverted to the original
+  @Flaky
+  Scenario: Change the permissions for Active Users
+    When I click on user button
+    Then I should be on display the user details
+    Then I click on First Active User
+    Then I see change link and suspend button
+    Then I click the suspend button
+    Then I see the suspend user page
+
+  # TEST 3: This test can replace test 1 once the 'OGD Beta Users' Launch Darkly Segment is amended to serve 'Enabled'
+  @Flaky @Ignore
   Scenario: Verify the Edit permission and Suspend User showing for Active Users
     When I click on user button
     Then I should be on display the user details
     Then I click on a Active User
     Then I see change link and suspend button
 
-#  @all
-#  Scenario: Change the permissions for Active Users
-#    When I click on user button
-#    Then I should be on display the user details
-#    Then I click on a Active User
-#    Then I see change link and suspend button
-#    Then I click on change link
-#    Then I edit the Manage User checkbox and click submit
-
-
-  @Flaky
+  # TEST 4: This test can replace TEST 2 once the 'OGD Beta Users' Launch Darkly Segment is amended to serve 'Enabled'
+  @Flaky @Ignore
   Scenario: Change the permissions for Active Users
     When I click on user button
     Then I should be on display the user details
@@ -31,3 +44,13 @@ Feature: edit permissions and suspend user workflow
     Then I see change link and suspend button
     Then I click the suspend button
     Then I see the suspend user page
+
+  # TEST 5: This test has never been enabled on the pipeline
+  # @all
+  # Scenario: Change the permissions for Active Users
+  #   When I click on user button
+  #   Then I should be on display the user details
+  #   Then I click on a Active User
+  #   Then I see change link and suspend button
+  #   Then I click on change link
+  #   Then I edit the Manage User checkbox and click submit
--- test_codecept/e2e/features/step_definitions/inviteUser.steps.js
+++ test_codecept/e2e/features/step_definitions/inviteUser.steps.js
@@ -17,168 +17,170 @@ const { defineSupportCode } = require('cucumber');
 const cucumberHtmlReporter = require('cucumber-html-reporter');
 const { Error } = require('globalthis/implementation');
 
+const inviteUserPage=new InviteUserPage();
+const viewUserPage=new ViewUserPage();
+const headerPage = new HeaderPage();
 
-  const inviteUserPage=new InviteUserPage();
-  const viewUserPage=new ViewUserPage();
-  const headerPage = new HeaderPage();
+const invitedUserEmail = '';
 
-  const invitedUserEmail = '';
+When(/^I click on invite user button$/, async function () {
+  await viewUserPage.clickInviteUser();
+  // browser.sleep(LONG_DELAY);
+});
 
-  When(/^I click on invite user button$/, async function () {
-    await viewUserPage.clickInviteUser();
-    // browser.sleep(LONG_DELAY);
+When(/^I navigate to invite user page$/, async function () {
+  const inviteUserPath = config.config.baseUrl.endsWith('/') ? 'users/invite-user' : '/users/invite-user';
+  await browser.get(config.config.baseUrl + inviteUserPath);
+  await inviteUserPage.waitForPage();
+});
+
+Then(/^I should be on display invite user page$/, async function () {
+  // browser.sleep(AMAZING_DELAY);;
+  await inviteUserPage.waitForPage();
+  expect(await inviteUserPage.amOnPage(), 'Invite User page is not displayed').to.be.true;
+});
+
+When(/^I enter mandatory fields firstname,lastname,emailaddress,permissions and click on send invitation button$/, async function () {
+  await inviteUserPage.waitForPage();
+  await inviteUserPage.enterIntoTextFieldFirstName(TestData.firstName);
+  await inviteUserPage.enterIntoTextFieldLastName(TestData.lastName);
+
+  // var emailAddress =Math.random().toString(36).substring(2);
+  global.latestInvitedUser = Math.random().toString(36).substring(2)+'@mailinator.com';
+  global.latestInvitedUserPassword = 'Monday01';
+
+  await inviteUserPage.enterIntoTextFieldEmailAddress(global.latestInvitedUser);
+  await inviteUserPage.manageUserCheckbox.click();
+  browser.sleep(LONG_DELAY);
+  await inviteUserPage.clickSendInvitationButton();
+  // browser.sleep(LONG_DELAY);
+});
+Then(/^user should be created successfuly$/, async function () {
+  await browserWaits.waitForElement(inviteUserPage.userInvitaionConfirmation);
+  await browserWaits.retryWithActionCallback(async () => {
+    expect(await inviteUserPage.userInvitaionConfirmation.getText()).to.include('You\'ve invited');
   });
+});
 
-  When(/^I navigate to invite user page$/, async function () {
-    const inviteUserPath = config.config.baseUrl.endsWith('/') ? 'users/invite-user' : '/users/invite-user';
-    await browser.get(config.config.baseUrl + inviteUserPath);
-    await inviteUserPage.waitForPage();
-  });
+When(/^I not enter the mandatory fields firstname,lastname,emailaddress,permissions and click on send invitation button$/, async function () {
+  await inviteUserPage.enterIntoTextFieldFirstName('');
+  await inviteUserPage.enterIntoTextFieldLastName('');
+  await inviteUserPage.enterIntoTextFieldEmailAddress('');
+  await inviteUserPage.clickSendInvitationButton();
+});
 
-  Then(/^I should be on display invite user page$/, async function () {
-    // browser.sleep(AMAZING_DELAY);;
-    await inviteUserPage.waitForPage();
-    expect(await inviteUserPage.amOnPage(), 'Invite User page is not displayed').to.be.true;
-  });
+When('I enter mandatory fields firstname,lastname,emailaddress with permissions and click on send invitation button', async function (table) {
+  await inviteUserPage.waitForPage();
+  await inviteUserPage.enterIntoTextFieldFirstName(TestData.firstName);
+  await inviteUserPage.enterIntoTextFieldLastName(TestData.lastName);
+  global.latestInvitedUser = Math.random().toString(36).substring(2) + '@mailinator.com';
+  global.latestInvitedUserPassword = 'Monday01';
 
-  When(/^I enter mandatory fields firstname,lastname,emailaddress,permissions and click on send invitation button$/, async function () {
-    await inviteUserPage.waitForPage();
-    await inviteUserPage.enterIntoTextFieldFirstName(TestData.firstName);
-    await inviteUserPage.enterIntoTextFieldLastName(TestData.lastName);
+  await inviteUserPage.enterIntoTextFieldEmailAddress(global.latestInvitedUser);
+  const permissions = table.parse().hashes();
+  for (let permCounter = 0; permCounter < permissions.length; permCounter++){
+    await inviteUserPage.selectPermission(permissions[permCounter].Permission, true);
+  }
+  await inviteUserPage.clickSendInvitationButton();
+});
 
-    // var emailAddress =Math.random().toString(36).substring(2);
-    global.latestInvitedUser = Math.random().toString(36).substring(2)+'@mailinator.com';
-    global.latestInvitedUserPassword = 'Monday01';
+When('I edit user permissions', async function (table) {
+  const permissions = table.hashes();
+  for (let permCounter = 0; permCounter < permissions.length; permCounter++) {
+    await inviteUserPage.selectPermission(permissions[permCounter].Permission, permissions[permCounter].isSelected === 'true');
+  }
+});
 
-    await inviteUserPage.enterIntoTextFieldEmailAddress(global.latestInvitedUser);
-    await inviteUserPage.manageUserCheckbox.click();
-    browser.sleep(LONG_DELAY);
-    await inviteUserPage.clickSendInvitationButton();
-    // browser.sleep(LONG_DELAY);
-  });
-  Then(/^user should be created successfuly$/, async function () {
+Then(/^I should be display the validation error$/, async function () {
+  await expect(inviteUserPage.failure_error_heading.isDisplayed()).to.eventually.be.true;
+  await expect(inviteUserPage.failure_error_heading.getText())
+    .to
+    .eventually
+    .equal('There is a problem');
+});
 
-    await browserWaits.waitForElement(inviteUserPage.userInvitaionConfirmation)
-    await browserWaits.retryWithActionCallback(async () => {
-      expect(await inviteUserPage.userInvitaionConfirmation.getText()).to.include('You\'ve invited');
+When(/^I click on back button$/, async function () {
+  // browser.sleep(AMAZING_DELAY);
+  await inviteUserPage.clickBackButton();
+});
 
-    })
-  });
+Then('I activate invited user', async function () {
+  await mailinatorService.init();
+  try {
+    mailinatorService.setLogger((message, isScreenshot) => logger(this, message, isScreenshot));
+    await mailinatorService.openRegistrationEmailForUser(global.latestInvitedUser);
+    this.attach('Registration email received successfully.');
+    await mailinatorService.completeUserRegistrationFromEmail();
+    this.attach('Registration completed successfully.');
+    await mailinatorService.destroy();
+  } catch (err){
+    await CucumberReportLogger.AddScreenshot(mailinatorService.getScreenShotUtil());
+    await mailinatorService.destroy();
+    throw new Error('Error occured during user activation steps', err);
+  }
+});
 
-  When(/^I not enter the mandatory fields firstname,lastname,emailaddress,permissions and click on send invitation button$/, async function () {
-    await inviteUserPage.enterIntoTextFieldFirstName('');
-    await inviteUserPage.enterIntoTextFieldLastName('');
-    await inviteUserPage.enterIntoTextFieldEmailAddress('');
-    await inviteUserPage.clickSendInvitationButton();
-  });
-
-  When('I enter mandatory fields firstname,lastname,emailaddress with permissions and click on send invitation button', async function (table) {
-    await inviteUserPage.waitForPage();
-    await inviteUserPage.enterIntoTextFieldFirstName(TestData.firstName);
-    await inviteUserPage.enterIntoTextFieldLastName(TestData.lastName);
-    global.latestInvitedUser = Math.random().toString(36).substring(2) + '@mailinator.com';
-    global.latestInvitedUserPassword = 'Monday01';
-
-    await inviteUserPage.enterIntoTextFieldEmailAddress(global.latestInvitedUser);
-    const permissions = table.parse().hashes();
-    for (let permCounter = 0; permCounter < permissions.length; permCounter++){
-      await inviteUserPage.selectPermission(permissions[permCounter].Permission, true);
-    }
-    await inviteUserPage.clickSendInvitationButton();
-  });
-
-  When('I edit user permissions', async function (table) {
-    const permissions = table.hashes();
-    for (let permCounter = 0; permCounter < permissions.length; permCounter++) {
-      await inviteUserPage.selectPermission(permissions[permCounter].Permission, permissions[permCounter].isSelected === 'true');
-    }
-  });
-
-  Then(/^I should be display the validation error$/, async function () {
-    await expect(inviteUserPage.failure_error_heading.isDisplayed()).to.eventually.be.true;
-    await expect(inviteUserPage.failure_error_heading.getText())
-      .to
-      .eventually
-      .equal('There is a problem');
-  });
-
-  When(/^I click on back button$/, async function () {
-    // browser.sleep(AMAZING_DELAY);
-    await inviteUserPage.clickBackButton();
-  });
-
-  Then('I activate invited user',  async function () {
-    await mailinatorService.init();
-    try {
-      mailinatorService.setLogger((message, isScreenshot) => logger(this, message, isScreenshot));
-      await mailinatorService.openRegistrationEmailForUser(global.latestInvitedUser);
-      this.attach('Registration email received successfully.');
-      await mailinatorService.completeUserRegistrationFromEmail();
-      this.attach('Registration completed successfully.');
-      await mailinatorService.destroy();
-    } catch (err){
-      await CucumberReportLogger.AddScreenshot(mailinatorService.getScreenShotUtil());
-      await mailinatorService.destroy();
-      throw new Error('Error occured during user activation steps', err);
-    }
-  });
-
-  Then(/^I click on a Active User$/, async function () {
-    await inviteUserPage.findNextActiveUserBySearch();
-    await browserWaits.waitForElement(inviteUserPage.activeUser)
-    await expect(inviteUserPage.activeUser.isDisplayed()).to.eventually.be.true;
-    await inviteUserPage.activeUser.click();
-  });
-
-Then(/^I click on a Active User by using Active filter$/, async function () {
-  await inviteUserPage.findNextActiveUserBySearchFilter();
-  await browserWaits.waitForElement(inviteUserPage.activeUser)
+Then(/^I click on First Active User$/, async function () {
+  await inviteUserPage.findNextActiveUser();
+  await browserWaits.waitForElement(inviteUserPage.activeUser);
   await expect(inviteUserPage.activeUser.isDisplayed()).to.eventually.be.true;
   await inviteUserPage.activeUser.click();
 });
 
-  Then(/^I see change link and suspend button$/, async function () {
+Then(/^I click on a Active User$/, async function () {
+  await inviteUserPage.findNextActiveUserBySearch();
+  await browserWaits.waitForElement(inviteUserPage.activeUser);
+  await expect(inviteUserPage.activeUser.isDisplayed()).to.eventually.be.true;
+  await inviteUserPage.activeUser.click();
+});
 
-    await browserWaits.waitForElement(inviteUserPage.userDetailsComponent);
-    await browserWaits.waitForElement(inviteUserPage.changeLink);
-    await browserWaits.waitForElement(inviteUserPage.suspendButton);
+Then(/^I click on a Active User by using Active filter$/, async function () {
+  await inviteUserPage.findNextActiveUserBySearchFilter();
+  await browserWaits.waitForElement(inviteUserPage.activeUser);
+  await expect(inviteUserPage.activeUser.isDisplayed()).to.eventually.be.true;
+  await inviteUserPage.activeUser.click();
+});
 
-    await expect(inviteUserPage.changeLink.isDisplayed(), 'chnage link not displayed').to.eventually.be.true;
-    await expect(inviteUserPage.suspendButton.isDisplayed(), 'suspend button not displayed').to.eventually.be.true;
-  });
+Then(/^I see change link and suspend button$/, async function () {
+  await browserWaits.waitForElement(inviteUserPage.userDetailsComponent);
+  await browserWaits.waitForElement(inviteUserPage.changeLink);
+  await browserWaits.waitForElement(inviteUserPage.suspendButton);
 
-  Then(/^I click on change link$/, async function () {
-    browser.sleep(MID_DELAY);
-    await inviteUserPage.changeLink.click();
-    await expect(inviteUserPage.editUserText.isDisplayed()).to.eventually.be.true;
-    await expect(inviteUserPage.editUserText.getText())
-      .to
-      .eventually
-      .equal('Edit user');
-  });
+  await expect(inviteUserPage.changeLink.isDisplayed(), 'chnage link not displayed').to.eventually.be.true;
+  await expect(inviteUserPage.suspendButton.isDisplayed(), 'suspend button not displayed').to.eventually.be.true;
+});
 
-  Then(/^I edit the Manage User checkbox and click submit$/, async function () {
-    browser.sleep(MID_DELAY);
-    await inviteUserPage.manageUserCheckbox.click();
-    await inviteUserPage.clickSendInvitationButton();
-    browser.sleep(MID_DELAY);
-    await viewUserPage.waitForUserDetailsPage();
-    await expect(inviteUserPage.suspendButton.isDisplayed()).to.eventually.be.true;
-  });
+Then(/^I click on change link$/, async function () {
+  browser.sleep(MID_DELAY);
+  await inviteUserPage.changeLink.click();
+  await expect(inviteUserPage.editUserText.isDisplayed()).to.eventually.be.true;
+  await expect(inviteUserPage.editUserText.getText())
+    .to
+    .eventually
+    .equal('Edit user');
+});
 
-  Then(/^I click the suspend button$/, async function () {
-    await inviteUserPage.suspendButton.click();
-  });
+Then(/^I edit the Manage User checkbox and click submit$/, async function () {
+  browser.sleep(MID_DELAY);
+  await inviteUserPage.manageUserCheckbox.click();
+  await inviteUserPage.clickSendInvitationButton();
+  browser.sleep(MID_DELAY);
+  await viewUserPage.waitForUserDetailsPage();
+  await expect(inviteUserPage.suspendButton.isDisplayed()).to.eventually.be.true;
+});
 
-  Then(/^I see the suspend user page$/, async function () {
-    browser.sleep(MID_DELAY);
-    await expect(inviteUserPage.editUserText.isDisplayed()).to.eventually.be.true;
-    await expect(inviteUserPage.editUserText.getText())
-      .to
-      .eventually
-      .equal('Are you sure you want to suspend this account?');
-  });
+Then(/^I click the suspend button$/, async function () {
+  await inviteUserPage.suspendButton.click();
+});
 
+Then(/^I see the suspend user page$/, async function () {
+  browser.sleep(MID_DELAY);
+  await expect(inviteUserPage.editUserText.isDisplayed()).to.eventually.be.true;
+  await expect(inviteUserPage.editUserText.getText())
+    .to
+    .eventually
+    .equal('Are you sure you want to suspend this account?');
+});
 
 function logger(world, message, isScreenshot){
   if (isScreenshot){

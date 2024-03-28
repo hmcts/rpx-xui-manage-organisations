66a16635b15c9d853d6f188c057dc7b564a3dfd6
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-03-28 13:38:33 +0000

    Refactor case sharing component and filter component

--- src/cases/containers/case-share-confirm/case-share-confirm.component.ts
+++ src/cases/containers/case-share-confirm/case-share-confirm.component.ts
@@ -30,19 +30,11 @@ export class CaseShareConfirmComponent implements OnInit {
   }
 
   public ngOnInit(): void {
-    // Set fnTitle, backLink, changeLink (these two links are the same as each other) and confirmLink depending on
-    // whether navigation is via the Unassigned Cases or Assigned Cases page
-    if (this.url.startsWith('/unassigned-cases')) {
-      this.fnTitle = 'Share a case';
-      this.backLink = '/unassigned-cases/case-share';
-      this.changeLink = '/unassigned-cases/case-share';
-      this.completeLink = `/unassigned-cases/case-share-complete/${this.pageType}`;
-    } else {
-      this.fnTitle = 'Manage case sharing';
-      this.backLink = '/assigned-cases/case-share';
-      this.changeLink = '/assigned-cases/case-share';
-      this.completeLink = `/assigned-cases/case-share-complete/${this.pageType}`;
-    }
+    this.fnTitle = 'Manage case assignments';
+    this.backLink = '/cases/case-share';
+    this.changeLink = '/cases/case-share';
+    this.completeLink = `/cases/case-share-complete/${this.pageType}`;
+
     this.shareCases$ = this.pageType === CaaCasesPageType.UnassignedCases
       ? this.store.pipe(select(fromCasesFeature.getShareUnassignedCaseListState))
       : this.store.pipe(select(fromCasesFeature.getShareAssignedCaseListState));

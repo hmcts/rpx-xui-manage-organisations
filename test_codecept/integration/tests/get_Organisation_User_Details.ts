f9a0e786d70025ace7a9bdcee70f0e41d69bb368
Ritesh Dsouza <ritesh.dsouza@HMCTS.net>
2024-10-02 16:38:24 +0100

    test fix

--- test_codecept/integration/tests/get_Organisation_User_Details.ts
+++ test_codecept/integration/tests/get_Organisation_User_Details.ts
@@ -3,11 +3,11 @@ const should = require('chai').should();
 
 suite('Manage Org -> Get Organisation User details', function() {
   this.timeout(50000);
-  test('GET Manage Organisation User details', () => generateAPIRequest('GET', '/api/allUserList')
+  test('GET Manage Organisation User details', () => generateAPIRequest('GET', '/api/allUserListWithoutRoles')
  
     // console.log('response', response.headers.get('cache-control'))
     .then((response) => {
-      response.status.should.be.eql(500);
+      response.status.should.be.eql(200);
       const user = response.data.users.filter((idamUser) => idamUser.email ==='xuiapiorganisation@mailnesia.com');
       user[0].lastName.should.be.eql('Lee');
       user[0].firstName.should.be.eql('Jason');

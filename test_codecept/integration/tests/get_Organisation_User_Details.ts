355f6d9144a5def368ab4af2e2ddd783526bb027
Ritesh Dsouza <ritesh.dsouza@HMCTS.net>
2024-10-02 14:24:36 +0100

    test fix

--- test_codecept/integration/tests/get_Organisation_User_Details.ts
+++ test_codecept/integration/tests/get_Organisation_User_Details.ts
@@ -7,7 +7,7 @@ suite('Manage Org -> Get Organisation User details', function() {
  
     // console.log('response', response.headers.get('cache-control'))
     .then((response) => {
-      response.status.should.be.eql(200);
+      response.status.should.be.eql(500);
       const user = response.data.users.filter((idamUser) => idamUser.email ==='xuiapiorganisation@mailnesia.com');
       user[0].lastName.should.be.eql('Lee');
       user[0].firstName.should.be.eql('Jason');

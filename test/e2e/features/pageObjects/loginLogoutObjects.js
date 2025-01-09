1c13d2dcabae631110875b39025d13f536affca1
Anthony Dummer <anthony.dummer@HMCTS.net>
2025-01-09 16:43:39 +0000

    exui-2102 putting in logging to see what is happeing

--- test/e2e/features/pageObjects/loginLogoutObjects.js
+++ test/e2e/features/pageObjects/loginLogoutObjects.js
@@ -3,25 +3,13 @@
 const { SHORT_DELAY, MID_DELAY, LONG_DELAY } = require('../../support/constants');
 
 function loginLogoutObjects() {
-  console.log('in loginLogoutObjects');
-  console.log('emailAddress is: ' + element(by.css('input#username')));
   this.emailAddress = element(by.css('[id=\'username\']'));
-  console.log('emailAddress value is: ' + this.emailAddress.getAttribute('value'));
-  console.log('emailAddress is: ' + this.emailAddress.toString());
-  console.log('password: ' + element(by.css('[id=\'password\']')));
   this.password = element(by.css('[id=\'password\']'));
-  console.log('password: ' + this.password.toString());
-  console.log('signinTitle: ' + element(by.css('h1.heading-large')));
   this.signinTitle= element(by.xpath('//h1[@class=\'heading-large\']'));
-  console.log('singinTitle: ' + this.signinTitle.toString());
   //this.signinTitle = element(by.css("h1"));
-  console.log('signinBtn: ' + element(by.css('input.button')));
   this.signinBtn = element(by.css('input.button'));
-  console.log('signOutlink: ' + element(by.xpath('//a[@class=\'hmcts-header__navigation-link\']')));
   this.signOutlink = element(by.xpath('//a[@class=\'hmcts-header__navigation-link\']'));
-  console.log('failure_error_heading: ' + element(by.css('[id=\'validation-error-summary-heading\']')));
   this.failure_error_heading = element(by.css('[id=\'validation-error-summary-heading\']'));
-  console.log('failure_error_message: ' + element(by.css('[id=\'validation-error-summary-list\']')));
   this.dashboard_header= element(by.css('a.hmcts-header__link'));
 
   this.givenIAmLoggedIn = async function () {

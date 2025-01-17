ac613ddcb49472678b3e08b892d7560e264bee71
Josh <josh.glasgow@hmcts.net>
2025-01-17 09:38:20 +0000

    change way we check if component is visible

--- playwright_tests/login.test.ts
+++ playwright_tests/login.test.ts
@@ -5,7 +5,7 @@ test('login and log out from MC with valid user', async ({ page }) => {
   await signIn(page);
   await expect(page.getByRole('heading', { name: 'Organisation' })).toBeVisible();
   await expect(page.getByText('OrganisationNameCivil -')).toBeVisible();
-  await expect(page.locator('app-prd-organisation-component')).toBeVisible();
+  await expect(page.locator('app-prd-organisation-component')).toHaveCount(1);
   await page.getByRole('link', { name: 'Sign out' }).click();
   await expect(page.getByRole('heading', { name: 'Sign in' })).toBeVisible();
 });

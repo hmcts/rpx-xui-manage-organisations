36816757e163b977212eee4bbabf771d6253522e
RiteshHMCTS <74713687+RiteshHMCTS@users.noreply.github.com>
2025-02-26 14:36:05 +0000

    exui-2655-xss-defence (#1324)
    * xss defence changes
    
    * lint error
    
    * changed the check condition
    
    * fix unit tests failure
    
    * removed unnecessary checks
    
    ---------
    
    Co-authored-by: kasi-subramaniam <60706745+kasi-subramaniam@users.noreply.github.com>

--- api/payments/index.ts
+++ api/payments/index.ts
@@ -1,7 +1,7 @@
 import { Request, Response, Router } from 'express';
 import { getConfigValue } from '../configuration';
 import { SERVICES_FEE_AND_PAY_API_PATH } from '../configuration/references';
-import { exists, valueOrNull } from '../lib/util';
+import { exists, objectContainsOnlySafeCharacters, valueOrNull } from '../lib/util';
 
 async function handleAddressRoute(req: Request, res: Response) {
   let errReport: any;
@@ -17,6 +17,9 @@ async function handleAddressRoute(req: Request, res: Response) {
     const response = await req.http.get(
       `${getConfigValue(SERVICES_FEE_AND_PAY_API_PATH)}/pba-accounts/${req.params.account}/payments/`
     );
+    if (!objectContainsOnlySafeCharacters(response.data.payments)) {
+      return res.send('Invalid payment data').status(400);
+    }
     res.send(response.data.payments);
   } catch (error) {
     const status = exists(error, 'status') ? error.status : 500;

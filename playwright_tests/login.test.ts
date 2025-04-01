aa7aa174ee04d4556353710ad69ce642b28f2904
Josh <josh.glasgow@hmcts.net>
2025-04-01 14:48:29 +0100

    fix command

--- playwright_tests/login.test.ts
+++ playwright_tests/login.test.ts
@@ -3,9 +3,7 @@ import { signIn } from './helpers/login';
 
 test('login and log out from MC with valid user', async ({ page }) => {
   await signIn(page);
-  console.log('i am signed in to the app');
   await expect(page.getByRole('heading', { name: 'Organisation' })).toBeVisible();
-  console.log('i can see the heading');
   await expect(page.getByText('OrganisationNameCivil -')).toBeVisible();
   await expect(page.locator('app-prd-organisation-component')).toHaveCount(1);
   await page.getByRole('link', { name: 'Sign out' }).click();

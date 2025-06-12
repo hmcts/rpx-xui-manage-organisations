9837deb0cb65ec490b3265f11ab5b6f1ce4d2306
Josh <josh.glasgow@hmcts.net>
2025-06-12 16:30:05 +0100

    change idam script

--- bin/idam-user-token.sh
+++ bin/idam-user-token.sh
@@ -1,35 +1,12 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-BASEDIR=$(realpath $(dirname ${0})/../../)
-
-if [ -f $BASEDIR/.env ]
-then
-  export $(cat $BASEDIR/.env | sed 's/#.*//g' | xargs)
-fi
-
-set -e
+set -eu
 
 username=${1}
 password=${2}
+clientSecret=${CCD_API_GATEWAY_IDAM_CLIENT_SECRET:-ccd_gateway_secret}
+redirectUri=${CCD_IDAM_REDIRECT_URL:-http://localhost:3451/oauth2redirect}
 
-IDAM_API_URL=${IDAM_API_URL_BASE}
+code=$(curl --insecure --fail --show-error --silent -X POST --user "${username}:${password}" "${IDAM_API_BASE_URL:-http://localhost:5000}/oauth2/authorize?redirect_uri=${redirectUri}&response_type=code&client_id=ccd_gateway" -d "" | docker run --rm --interactive ghcr.io/jqlang/jq:latest -r .code)
 
-clientSecret=${IDAM_SECRET}
-redirectUri=http://localhost:3001/oauth2/callback
-
-echo "username: ${username}"
-echo "password: ${password}"
-echo "IDAM_API_URL: ${IDAM_API_URL}"
-echo "clientSecret: ${clientSecret}"
-
-if [ -z "$IDAM_STUB_LOCALHOST" ]; then
-  response=$(curl --insecure --fail --show-error --silent -X POST --user "${username}:${password}" "${IDAM_API_URL}/oauth2/authorize?redirect_uri=${redirectUri}&response_type=code&client_id=xuimowebapp" -d "")
-  echo "authorize response: $response"
-  code=$(echo "$response" | docker run --rm --interactive ghcr.io/jqlang/jq -r .code)
-else
-  code=stubbed-value
-fi
-
-echo "code: ${code}"
-
-curl --insecure --fail --show-error --silent -X POST -H "Content-Type: application/x-www-form-urlencoded" --user "xuimowebapp:${clientSecret}" "${IDAM_API_URL}/oauth2/token?code=${code}&redirect_uri=${redirectUri}&grant_type=authorization_code" -d "" | docker run --rm --interactive ghcr.io/jqlang/jq -r .access_token
+curl --insecure --fail --show-error --silent -X POST -H "Content-Type: application/x-www-form-urlencoded" --user "ccd_gateway:${clientSecret}" "${IDAM_API_BASE_URL:-http://localhost:5000}/oauth2/token?code=${code}&redirect_uri=${redirectUri}&grant_type=authorization_code" -d "" | docker run --rm --interactive ghcr.io/jqlang/jq:latest -r .access_token

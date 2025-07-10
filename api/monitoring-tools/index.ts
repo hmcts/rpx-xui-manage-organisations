808d2f8e6b3011090b4a1931b096f2ce2d76f78f
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-07-10 11:50:40 +0100

    CME-494 replace instrumentationkey with connection string

--- api/monitoring-tools/index.ts
+++ api/monitoring-tools/index.ts
@@ -1,18 +1,18 @@
 import { Request, Response, Router } from 'express';
 import { getConfigValue } from '../configuration';
-import { APP_INSIGHTS_KEY } from '../configuration/references';
+import { APP_INSIGHTS_CONNECTION_STRING } from '../configuration/references';
 import { exists } from '../lib/util';
 
-export async function handleInstrumentationKeyRoute(req: Request, res: Response) {
+export async function handleConnectionStringRoute(req: Request, res: Response) {
   try {
-    res.send({ key: getConfigValue(APP_INSIGHTS_KEY) });
+    res.send({ key: getConfigValue(APP_INSIGHTS_CONNECTION_STRING) });
   } catch (error) {
     const status = exists(error, 'statusCode') ? error.statusCode : 500;
 
     const errReport = {
       apiError: { ...error },
       apiStatusCode: status,
-      message: 'Instrumentation key route error'
+      message: 'AppInsights connection string route error'
     };
     res.status(status).send(errReport);
   }
@@ -20,6 +20,6 @@ export async function handleInstrumentationKeyRoute(req: Request, res: Response)
 
 export const router = Router({ mergeParams: true });
 
-router.get('/', handleInstrumentationKeyRoute);
+router.get('/', handleConnectionStringRoute);
 
 export default router;

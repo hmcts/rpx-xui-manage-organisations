ccc1c3d197cd77d59523daa58e13313382831a36
Josh <josh.glasgow@hmcts.net>
2025-11-06 13:24:58 +0000

    Remove service override env var, make sure logout works with recent NL changes

--- api/auth/index.ts
+++ api/auth/index.ts
@@ -23,8 +23,7 @@ import {
   SERVICE_S2S_PATH,
   SESSION_SECRET,
   SYSTEM_USER_NAME,
-  SYSTEM_USER_PASSWORD,
-  SERVICES_IDAM_SERVICE_OVERRIDE
+  SYSTEM_USER_PASSWORD
 } from '../configuration/references';
 import { http } from '../lib/http';
 import * as log4jui from '../lib/log4jui';
@@ -123,7 +122,7 @@ export const getXuiNodeMiddleware = () => {
     tokenEndpointAuthMethod: 'client_secret_post',
     tokenURL: tokenUrl,
     useRoutes: true,
-    serviceOverride: getConfigValue(SERVICES_IDAM_SERVICE_OVERRIDE)
+    ssoLogoutURL: `${idamWebUrl}/o/endSession`
   };
 
   const baseStoreOptions = {

f23b2bf8d97896e1e32856d65b119137a008736b
Chris Jones <christopher.jones@hmcts.net>
2026-02-04 15:28:40 +0000

    removed /external/configuration endpoint

--- test/nodeMock/reqResMapping.js
+++ test/nodeMock/reqResMapping.js
@@ -10,9 +10,6 @@ const requestMapping = {
     '/external/configuration-ui': (req, res) => {
       res.send({ 'googleAnalyticsKey': 'UA-124734893-4', 'idamWeb': 'https://idam-web-public.aat.platform.hmcts.net', 'launchDarklyClientId': '5de6610b23ce5408280f2268', 'manageCaseLink': 'https://xui-webapp-aat.service.core-compute-aat.internal/cases', 'manageOrgLink': 'https://xui-mo-webapp-aat.service.core-compute-aat.internal', 'protocol': 'http' });
     },
-    '/external/configuration': (req, res) => {
-      res.send(''+getConfigurationValue(req.query.configurationKey));
-    },
     '/api/healthCheck': (req, res) => {
       res.send({ 'healthState': true });
     },

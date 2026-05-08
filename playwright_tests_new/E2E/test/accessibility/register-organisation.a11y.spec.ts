87086cc0b97b597defa5754526b9bec212ff7fd8
Andrew Grizhenkov <andrew.grizhenkov@hmcts.net>
2026-05-08 20:12:31 +0100

    EXUI-4365: add Playwright accessibility lane

---
+++ playwright_tests_new/E2E/test/accessibility/register-organisation.a11y.spec.ts
@@ -0,0 +1,19 @@
+import { AxeUtils } from '@hmcts/playwright-common';
+import { test, expect } from '../../fixtures';
+
+test.use({ manageOrgUserRole: 'roo' });
+
+test(
+  'register organisation start page has no automatically detectable accessibility violations @a11y',
+  { tag: ['@e2e', '@a11y', '@registration'] },
+  async ({ signedInPage, registerOrganisationPage }, testInfo) => {
+    const axeUtils = new AxeUtils(signedInPage);
+
+    await registerOrganisationPage.openStartPage();
+
+    await expect(registerOrganisationPage.startPageHeading).toBeVisible();
+    await expect(registerOrganisationPage.alreadyRegisteredHeading).toBeVisible();
+    await axeUtils.audit();
+    await axeUtils.generateReport(testInfo, 'Register organisation accessibility report');
+  }
+);

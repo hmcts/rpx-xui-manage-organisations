8721d732f0a918b8a2286e69649a5e97c2af2600
Andrew Grizhenkov <andrew.grizhenkov@hmcts.net>
2026-05-20 20:53:07 +0100

    test: add terms acceptance parity coverage

--- playwright_tests_new/integration/helpers/index.ts
+++ playwright_tests_new/integration/helpers/index.ts
@@ -3,4 +3,5 @@ export * from './caseSharingMockRoutes.helper';
 export * from './manageOrgBaseRoutes.helper';
 export * from './registerOrganisationJourney.helper';
 export * from './registerOrganisationMockRoutes.helper';
+export * from './termsAndConditionsMockRoutes.helper';
 export * from './userAdminMockRoutes.helper';

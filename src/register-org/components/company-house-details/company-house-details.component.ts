b0248f71f6c14eb60adade96196cd162665ab463
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2026-08-10 13:19:49 +0100

    Reduce complexity of the company house validator

--- src/register-org/components/company-house-details/company-house-details.component.ts
+++ src/register-org/components/company-house-details/company-house-details.component.ts
@@ -1,9 +1,44 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CompanyHouseDetailsMessage } from '../../../register-org/models';
 import { RegisterComponent } from '../../containers/register/register-org.component';
 import { RegisterOrgService } from '../../services/register-org.service';
+
+// General company house number validation prefixes
+const COMPANY_HOUSE_PREFIXES = [
+  'AC', 'ZC', 'FC', 'GE', 'LP', 'OC', 'SE', 'SA', 'SZ', 'SF', 'GS', 'SL', 'SO', 'SC', 'ES',
+  'NA', 'NZ', 'NF', 'GN', 'NL', 'NC', 'R0', 'NI', 'EN', 'SG', 'FE', 'BR', 'OE', 'RS'
+];
+
+// Special company house number validation patterns (e.g. RS12345, RS1234FI, RS123CUS, NI12345A)
+const COMPANY_HOUSE_PATTERNS = [
+  /^RS\d{5}$/,
+  /^(RS|SO)\d{6}$/,
+  /^(RS|SO)\d{5}[WSRCZF]$/,
+  /^(RS|SO)\d{4}(FI|RS|SA|IP|US|EN|AS)$/,
+  /^(RS|SO)\d{3}CUS$/,
+  /^(NI|SL)\d{5}[\dA]$/,
+  /^OC[\dP]{5}[CWERTB]$/,
+  /^OC[\dP]{4}(OC|CU)$/
+];
+
+// Sonar complained about complex validation so this was created to simplify and make it more readable
+const companyHouseNumberValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const companyHouseNumber = control.value;
+
+  if (!companyHouseNumber) {
+    return null;
+  }
+
+  const hasKnownPrefix = COMPANY_HOUSE_PREFIXES.includes(companyHouseNumber.slice(0, 2));
+  const hasNumberPrefix = /^\d{2}$/.test(companyHouseNumber.slice(0, 2));
+  const hasValidStandardFormat = (hasKnownPrefix || hasNumberPrefix) && /^\d{5}[\dCR]$/.test(companyHouseNumber.slice(2));
+  const hasValidSpecialFormat = COMPANY_HOUSE_PATTERNS.some((pattern) => pattern.test(companyHouseNumber));
+
+  // Has to match the standard format or one of the special formats to be valid
+  return hasValidStandardFormat || hasValidSpecialFormat ? null : { pattern: true };
+};
 
 @Component({
   selector: 'app-company-house-details',
@@ -27,7 +62,7 @@ export class CompanyHouseDetailsComponent extends RegisterComponent implements O
     this.companyHouseFormGroup = new FormGroup({
       companyName: new FormControl(this.registrationData.companyName, Validators.required),
       companyHouseNumber: new FormControl(this.registrationData.companyHouseNumber,
-        Validators.pattern(/^((RS\d{5})|((AC|ZC|FC|GE|LP|OC|SE|SA|SZ|SF|GS|SL|SO|SC|ES|NA|NZ|NF|GN|NL|NC|R0|NI|EN|\d{2}|SG|FE|BR|OE|RS)\d{5}(\d|C|R))|((RS|SO)\d{3}(\d{3}|\d{2}[WSRCZF]|\d(FI|RS|SA|IP|US|EN|AS)|CUS))|((NI|SL)\d{5}[\dA])|(OC(([\dP]{5}[CWERTB])|([\dP]{4}(OC|CU)))))$/)
+        companyHouseNumberValidator
       )
     });
   }

702344b59701c6d4f8617f31e9489d393c4b856a
anthony borisade <jideboris@yahoo.com>
2026-08-19 17:34:32 +0100

    feat(EXUI-5093):Remove redundant components and upgrade other to use govuk
    update to bump compatible toolkit
    
    update toolkit

--- src/register/components/govuk-dl-list-item/govuk-dl-list-item.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'div[app-govuk-dl-list-item]',
-  templateUrl: './govuk-dl-list-item.component.html',
-  standalone: false
-})
-export class GovukDlListItemComponent {}
--- src/shared/components/success-notification/success-notification.component.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SuccessNotificationComponent } from './success-notification.component';
-
-describe('Invite User Success Component', () => {
-  it('should have a component', () => {
-    const component = new SuccessNotificationComponent();
-    expect(component).toBeTruthy();
-  });
-});

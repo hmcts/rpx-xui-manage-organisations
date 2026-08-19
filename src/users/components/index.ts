ce91f3a4cfb11f48130f00d0050fd0ed58fac21f
anthony borisade <jideboris@yahoo.com>
2026-08-19 17:34:32 +0100

    feat(EXUI-5093):Remove redundant components and upgrade other to use govuk

--- src/users/components/index.ts
+++ src/users/components/index.ts
@@ -5,12 +5,6 @@ import { UserTableComponent } from './user-table/user-table.component';
 import { UserPersonalDetailsComponent } from './user-personal-details/user-personal-details.component';
 import { OrganisationAccessPermissionsComponent } from './organisation-access-permissions/organisation-access-permissions.component';
 import { StandardUserPermissionsComponent } from './standard-user-permissions/standard-user-permissions.component';
-import { OgdCafcassCyProfileContentComponent } from './ogd-cafcass-cy-profile-content/ogd-cafcass-cy-profile-content.component';
-import { OgdCafcassEnProfileContentComponent } from './ogd-cafcass-en-profile-content/ogd-cafcass-en-profile-content.component';
-import { OgdCicaProfileContentComponent } from './ogd-cica-profile-content/ogd-cica-profile-content.component';
-import { OgdDwpProfileContentComponent } from './ogd-dwp-profile-content/ogd-dwp-profile-content.component';
-import { OgdHmrcProfileContentComponent } from './ogd-hmrc-profile-content/ogd-hmrc-profile-content.component';
-import { OgdHoProfileContentComponent } from './ogd-ho-profile-content/ogd-ho-profile-content.component';
 import { SolicitorProfileContentComponent } from './solicitor-profile-content/solicitor-profile-content.component';
 import { JurisdictionAccessOptionsComponent } from './jurisdictions-access-options/jurisdiction-access-options.component';
 
@@ -23,12 +17,6 @@ export const components: any[] = [
   OrganisationAccessPermissionsComponent,
   StandardUserPermissionsComponent,
   SolicitorProfileContentComponent,
-  OgdDwpProfileContentComponent,
-  OgdHoProfileContentComponent,
-  OgdHmrcProfileContentComponent,
-  OgdCicaProfileContentComponent,
-  OgdCafcassEnProfileContentComponent,
-  OgdCafcassCyProfileContentComponent,
   JurisdictionAccessOptionsComponent
 ];
 
@@ -40,11 +28,4 @@ export * from './user-personal-details/user-personal-details.component';
 export * from './organisation-access-permissions/organisation-access-permissions.component';
 export * from './standard-user-permissions/standard-user-permissions.component';
 export * from './solicitor-profile-content/solicitor-profile-content.component';
-export * from './ogd-cafcass-cy-profile-content/ogd-cafcass-cy-profile-content.component';
-export * from './ogd-cafcass-en-profile-content/ogd-cafcass-en-profile-content.component';
-export * from './ogd-cica-profile-content/ogd-cica-profile-content.component';
-export * from './ogd-dwp-profile-content/ogd-dwp-profile-content.component';
-export * from './ogd-hmrc-profile-content/ogd-hmrc-profile-content.component';
-export * from './ogd-ho-profile-content/ogd-ho-profile-content.component';
 export * from './jurisdictions-access-options/jurisdiction-access-options.component';
-

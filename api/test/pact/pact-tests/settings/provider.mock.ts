856904b1b57b73f698a3edc96b8dc6f5b897fd70
Josh <josh.glasgow@hmcts.net>
2026-09-25 12:51:19 +0100

    resolve CVEs, update docker build to only include production dependencies

--- api/test/pact/pact-tests/settings/provider.mock.ts
+++ api/test/pact/pact-tests/settings/provider.mock.ts
@@ -1,4 +1,4 @@
-import { Pact } from '@pact-foundation/pact';
+import { PactV2 as Pact, SpecificationVersion } from '@pact-foundation/pact';
 import * as path from 'path';
 
 export interface PactTestSetupConfig {
@@ -15,7 +15,7 @@ export class PactTestSetup {
       port: this.port,
       log: path.resolve(process.cwd(), 'api/test/pact/logs', 'mockserver-integration.log'),
       dir: path.resolve(process.cwd(), 'api/test/pact/pacts'),
-      spec: 2,
+      spec: SpecificationVersion.SPECIFICATION_VERSION_V2,
       consumer: 'xui_manageOrg',
       provider: config.provider,
       pactfileWriteMode: 'merge'
--- api/test/pact/publish/publish.ts
+++ api/test/pact/publish/publish.ts
@@ -1,6 +1,7 @@
-import { Publisher } from '@pact-foundation/pact';
+import { execFile } from 'child_process';
 import * as git from 'git-rev-sync';
 import * as path from 'path';
+import { promisify } from 'util';
 
 import { getConfigValue } from '../../../configuration';
 import {
@@ -23,19 +24,17 @@ const publish = async (): Promise<void> => {
       // @ts-ignore
       getConfigValue(PACT_CONSUMER_VERSION) : git.short();
 
-    const opts = {
-      consumerVersion,
-      pactBroker,
-      pactBrokerPassword: getConfigValue(PACT_BROKER_PASSWORD),
-      pactBrokerUsername: getConfigValue(PACT_BROKER_USERNAME),
-      pactFilesOrDirs: [
-        path.resolve(__dirname, '../pacts/')
-      ],
-      tags: [pactTag]
-    };
+    const args = [
+      'publish',
+      path.resolve(__dirname, '../pacts/'),
+      '--broker-base-url', pactBroker,
+      '--broker-username', getConfigValue(PACT_BROKER_USERNAME),
+      '--broker-password', getConfigValue(PACT_BROKER_PASSWORD),
+      '--consumer-app-version', consumerVersion,
+      '--tag', pactTag
+    ];
 
-    const publisher = new Publisher(opts);
-    await publisher.publishPacts();
+    await promisify(execFile)('pact-broker', args);
 
     console.log('Pact contract publishing complete!');
     console.log('');
